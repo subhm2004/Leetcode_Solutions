4a41d0e885be4b404a90fd8d134f446e0d66df27
subhm2004 <146654596+subhm2004@users.noreply.github.com>
2025-02-28 23:26:32 +0530

    Time: 372 ms (17.19%) | Memory: 291.7 MB (12.96%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -16,12 +16,34 @@ public:
         return false;
     }
     
+   bool solveME(string& s, int i, int j, vector<vector<int>>& dp) {
+        if (i >= j)
+            return true;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        bool flag = 0;
+        if (s[i] == s[j]) {
+            flag = solveME(s, i + 1, j - 1, dp);
+        }
+
+        if (flag) {
+            int currlen = j - i + 1;
+            if (currlen > maxlen) {
+                maxlen = currlen;
+                start = i;
+            }
+        }
+        return dp[i][j] = flag;
+    }
+    
     string longestPalindrome(string s) {
         int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                solveRE(s, i, j);
+                solveME(s, i, j, dp);
             }
         }
         
