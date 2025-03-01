909ab45b551db3db4360f6f414131b842d001786
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-01 12:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n = nums.size();
         // Step 1: Merge adjacent equal numbers
-        for (int i = 0; i < n - 1; ++i) {
-            if (nums[i] == nums[i + 1]) {
+
+        for (int i = 0, j = 0; i < nums.size(); ++i) {
+            if (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
-        }
-        // Step 2: Move non-zero numbers to the front
-        int j = 0;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                ++j;
-            }
+            // Step 2: Move non-zero numbers to the front
+
+            if (nums[i] > 0)
+                swap(nums[i], nums[j++]);
         }
         return nums;
     }
