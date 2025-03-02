051e3557f9eb81451336cb5612d47a7dafc72031
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-03 00:28:30 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (44.20%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,31 @@
+struct Result {
+    TreeNode* lca;
+    int depth;
+};
+
+class LCAFinder {
+public:
+    Result findLCA(TreeNode* root) {
+        if (!root)
+            return {nullptr, 0}; // Base Case
+
+        Result left = findLCA(root->left);
+        Result right = findLCA(root->right);
+
+        if (left.depth > right.depth)
+            return {left.lca, left.depth + 1}; // Left subtree is deeper
+
+        if (right.depth > left.depth)
+            return {right.lca, right.depth + 1}; // Right subtree is deeper
+
+        return {root, left.depth + 1}; // Both sides equal, root is LCA
+    }
+};
+
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        LCAFinder solver;
+        return solver.findLCA(root).lca;
+    }
+};
