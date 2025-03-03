46b4f30c28fcca4e183a7809792de780bb2c19c5
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-03 13:23:32 +0530

    Time: 4 ms (2.94%) | Memory: 13.7 MB (57.71%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,48 @@
+class Solution {
+ public:
+  bool isCompleteTree(TreeNode* root) {
+    if (!root) return true;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    bool seenNull = false;
+
+    while (!q.empty()) {
+      TreeNode* node = q.front();
+      q.pop();
+
+      if (!node) {
+        seenNull = true;
+      } else {
+        if (seenNull) return false; // If a non-null node appears after a null, it's not complete
+        q.push(node->left);
+        q.push(node->right);
+      }
+    }
+
+    return true;
+  }
+};
+
+
+// class Solution {
+// public:
+//     int countNodes(TreeNode* root) {
+//         if (!root)
+//             return 0;
+//         return 1 + countNodes(root->left) + countNodes(root->right);
+//     }
+
+//     bool isValid(TreeNode* root, int index, int totalNodes) {
+//         if (!root)
+//             return true;
+//         if (index > totalNodes)
+//             return false;
+//         return isValid(root->left, index * 2, totalNodes) &&
+//                isValid(root->right, index * 2 + 1, totalNodes);
+//     }
+//     bool isCompleteTree(TreeNode* root) {
+//         int totalNodes = countNodes(root);
+//         return isValid(root, 1, totalNodes);
+//     }
+// };
