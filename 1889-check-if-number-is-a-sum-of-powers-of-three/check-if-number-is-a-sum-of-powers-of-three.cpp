949a2577484cd681791c9e15a09fc02a2704f1e1
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-04 09:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (41.55%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,20 +1,30 @@
+// class Solution {
+// public:
+//     bool checkPowersOfThree(int n) {
+//         long power = 1;
+        
+//         // Find the smallest power of 3 that is ≥ n
+//         for (int i = 0; power < n; i++) {
+//             power = pow(3, i);
+//         }
+
+//         // Try subtracting distinct powers of 3
+//         for (; power > 0; power /= 3) {
+//             if (n >= power) {
+//                 n -= power;
+//             }
+//         }
+
+//         return n == 0;
+//     }
+// };
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        long power = 1;
-        
-        // Find the smallest power of 3 that is ≥ n
-        for (int i = 0; power < n; i++) {
-            power = pow(3, i);
+        while (n > 0) {
+            if (n % 3 == 2) return false; // If a digit 2 appears, return false
+            n /= 3;
         }
-
-        // Try subtracting distinct powers of 3
-        for (; power > 0; power /= 3) {
-            if (n >= power) {
-                n -= power;
-            }
-        }
-
-        return n == 0;
+        return true;
     }
 };
