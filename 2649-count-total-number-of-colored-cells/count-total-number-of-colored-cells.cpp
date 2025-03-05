12996e4be1c0a27ccde59564b9e001f85c2ea42e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-05 12:59:02 +0530

    Time: 5 ms (6.17%) | Memory: 10.9 MB (5.25%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,9 +1,10 @@
-class Solution {
- public:
-  long long coloredCells(int n) {
-    return (long long )(n) * n + (long long)(n - 1) * (n - 1);
-  }
-};
+// class Solution {
+//  public:
+//   long long coloredCells(int n) {
+//     return (long long )(n) * n + (long long)(n - 1) * (n - 1);
+//   }
+// };
+
 // class Solution {
 // public:
 //     long long coloredCells(int n) {
@@ -18,15 +19,15 @@ class Solution {
 //    return {}; }
 // };
 
-// class Solution {
-// public:
-//     long long solve(int n){
-//         if(n==1){
-//             return 1;
-//         }
-//         return (n-1)*4 + solve(n-1);
-//     }
-//     long long coloredCells(int n) {
-//         return solve(n);
-//     }
-// };
+class Solution {
+public:
+    long long solve(int n){
+        if(n==1){
+            return 1;
+        }
+        return (n-1)*4 + solve(n-1);
+    }
+    long long coloredCells(int n) {
+        return solve(n);
+    }
+};
