4bc42a56fba45a2dd76c91488f556f7609952c3a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-09 09:23:42 +0530

    Time: 3 ms (88.44%) | Memory: 27 MB (56.49%) - LeetSync

---
+++ 3463-alternating-groups-i/alternating-groups-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int n = colors.size();  
+        int k = 3, ans = 0, count = 0;  
+
+        // Circular array ka 2x traverse kar rahe hain taaki pura cycle cover ho
+        for (int i = 0; i < 2 * n; ++i) {  
+            // Agar pichla color same hai, to alternating group yahan break ho raha hai
+            if (i > 0 && colors[i % n] == colors[(i - 1) % n]) {  
+                count = 1; // Count ko reset kar diya
+            } else {  
+                ++count; // Alternating sequence continue ho rahi hai, to count badha diya
+            }
+
+            // Jab `i` n se bada ho jaye aur current sequence ka length `k` ya usse zyada ho
+            if (i >= n && count >= k) {  
+                ++ans; // Valid alternating group mil gaya, answer badha diya
+            }
+        }
+        return ans;  
+    }
+};
