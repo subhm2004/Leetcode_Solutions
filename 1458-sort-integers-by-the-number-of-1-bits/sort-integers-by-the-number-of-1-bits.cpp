a2c18d15086236873f4eb8c058aaf82cc1338016
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-12 15:07:20 +0530

    Time: 10 ms (11.42%) | Memory: 14.2 MB (50.25%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,11 +1,34 @@
+// class Solution {
+//  public:
+//   vector<int> sortByBits(vector<int>& arr) {
+//     sort(arr.begin(), arr.end(), [](int a, int b) {
+//       int bitsA = __builtin_popcount(a); // Count 1s in binary representation of 'a'
+//       int bitsB = __builtin_popcount(b); // Count 1s in binary representation of 'b'
+//       return bitsA == bitsB ? a < b : bitsA < bitsB;
+//     });
+//     return arr;
+//   }
+// };
+
 class Solution {
  public:
   vector<int> sortByBits(vector<int>& arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
-      int bitsA = __builtin_popcount(a); // Count 1s in binary representation of 'a'
-      int bitsB = __builtin_popcount(b); // Count 1s in binary representation of 'b'
+    auto countBits = [](int n) {
+      int count = 0;
+      while (n) {
+        count += (n & 1); // Check if the last bit is 1
+        n >>= 1;          // Right shift to check the next bit
+      }
+      return count;
+    };
+
+    sort(arr.begin(), arr.end(), [&](int a, int b) {
+      int bitsA = countBits(a);
+      int bitsB = countBits(b);
       return bitsA == bitsB ? a < b : bitsA < bitsB;
     });
+
     return arr;
   }
 };
+
