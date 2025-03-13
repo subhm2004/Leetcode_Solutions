1247e187ebc65fe0f0bb35f7f66cee1d7475d132
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-13 11:25:26 +0530

    Time: 11 ms (77.74%) | Memory: 322.9 MB (94.01%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.cpp
@@ -1,46 +1,75 @@
 class Solution {
 public:
-    bool isZeroArray(vector<int>& nums) {
-        return all_of(nums.begin(), nums.end(), [](int x) { return x == 0; });
-    }
-
-    bool canMakeZeroRecursive(vector<int>& nums, 
-                            vector<vector<int>>& queries, 
-                              int k, vector<int>& diff, int i) {
-        if (i == k) {
-            int total = 0;
-            for (int j = 0; j < nums.size(); ++j) {
-                total += diff[j];
-                if (total < nums[j]) return false;
-            }
-            return true;
-        }
-
-        int l = queries[i][0], r = queries[i][1], val = queries[i][2];
-        diff[l] += val;
-        if (r + 1 < nums.size()) diff[r + 1] -= val;
-
-        return canMakeZeroRecursive(nums, queries, k, diff, i + 1);
-    }
-
-    bool canMakeZero(vector<int>& nums, vector<vector<int>>& queries, int k) {
-        vector<int> diff(nums.size() + 1, 0);
-        return canMakeZeroRecursive(nums, queries, k, diff, 0);
-    }
-
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        if (isZeroArray(nums)) return 0;
+        int n = nums.size();
+        vector<int> cnt(n + 1, 0);  // Difference array to manage range updates
+        int sum = 0, k = 0;
 
-        int left = 1, right = queries.size(), ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (canMakeZero(nums, queries, mid)) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        for (int i = 0; i < n; i++) {
+            // Ensure sum at index `i` is at least `nums[i]`
+            while (sum + cnt[i] < nums[i]) {
+                if (k >= queries.size()) return -1;  // Not enough queries to satisfy condition
+
+                // Extract query details
+                int l = queries[k][0], r = queries[k][1], val = queries[k][2];
+                k++;  // Move to the next query
+
+                // Apply the query only if it affects index `i`
+                if (r >= i) {
+                    cnt[max(l, i)] += val;
+                    cnt[r + 1] -= val;
+                }
             }
+            sum += cnt[i];  // Update sum for the next iteration
         }
-        return ans;
+        return k;  // Return the number of queries used
     }
 };
+
+
+// class Solution {
+// public:
+//     bool isZeroArray(vector<int>& nums) {
+//         return all_of(nums.begin(), nums.end(), [](int x) { return x == 0; });
+//     }
+
+//     bool canMakeZeroRecursive(vector<int>& nums, 
+//                             vector<vector<int>>& queries, 
+//                               int k, vector<int>& diff, int i) {
+//         if (i == k) {
+//             int total = 0;
+//             for (int j = 0; j < nums.size(); ++j) {
+//                 total += diff[j];
+//                 if (total < nums[j]) return false;
+//             }
+//             return true;
+//         }
+
+//         int l = queries[i][0], r = queries[i][1], val = queries[i][2];
+//         diff[l] += val;
+//         if (r + 1 < nums.size()) diff[r + 1] -= val;
+
+//         return canMakeZeroRecursive(nums, queries, k, diff, i + 1);
+//     }
+
+//     bool canMakeZero(vector<int>& nums, vector<vector<int>>& queries, int k) {
+//         vector<int> diff(nums.size() + 1, 0);
+//         return canMakeZeroRecursive(nums, queries, k, diff, 0);
+//     }
+
+//     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+//         if (isZeroArray(nums)) return 0;
+
+//         int left = 1, right = queries.size(), ans = -1;
+//         while (left <= right) {
+//             int mid = (left + right) / 2;
+//             if (canMakeZero(nums, queries, mid)) {
+//                 ans = mid;
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
