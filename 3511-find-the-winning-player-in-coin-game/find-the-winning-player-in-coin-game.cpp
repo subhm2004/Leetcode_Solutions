d9cf4b55e9a45eabafa6a93955b9ad066752ba2b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-14 11:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (63.57%) - LeetSync

--- 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.cpp
@@ -2,6 +2,6 @@ class Solution {
 public:
     string winningPlayer(int x, int y) {
         int moves = min(x, y / 4); // Maximum valid moves possible
-        return ((moves & 1) == 0) ? "Bob" : "Alice"; 
+        return (moves % 2 == 0) ? "Bob" : "Alice";
     }
 };
