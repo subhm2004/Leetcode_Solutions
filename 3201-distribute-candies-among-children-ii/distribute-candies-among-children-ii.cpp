8b44e284418d15c00412ad566c2668eafffb0d0c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-14 12:34:40 +0530

    Time: 2 ms (86.41%) | Memory: 9 MB (17.93%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -10,16 +10,21 @@ public:
     long ways(int n) {
         if (n < 0)
             return 0;
-        return nCk(n + 2,
-                   2); // Using stars and bars method (Beggar's coin method )
+        return nCk(n + 2, 2); // Using stars and bars method (Beggar's coin method )
     }
 
-    long long distributeCandies(int n, int limit) {
-        int limitPlusOne = limit + 1;
-        return ways(n) - 3 * ways(n - limitPlusOne) +
-               3 * ways(n - 2 * limitPlusOne) - ways(n - 3 * limitPlusOne);
-    }
+  long long distributeCandies(int n, int limit) {
+    int limitPlusOne = limit + 1;
+    long oneChildExceedsLimit = ways(n - limitPlusOne);
+    long twoChildrenExceedLimit = ways(n - 2 * limitPlusOne);
+    long threeChildrenExceedLimit = ways(n - 3 * limitPlusOne);
+    // Principle of Inclusion-Exclusion  
+    return ways(n) - 3 * oneChildExceedsLimit + 3 * twoChildrenExceedLimit -
+           threeChildrenExceedLimit;
+  }
 };
+
+
 
 // class Solution {
 // public:
