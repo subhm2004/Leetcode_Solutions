ba1fd8f166065df2f604900e4559800dbbd13d79
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-16 18:12:24 +0530

    Time: 21 ms (83.62%) | Memory: 59.4 MB (45.76%) - LeetSync

--- 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -1,10 +1,11 @@
+typedef long long ll;
 class Solution {
 public:
     // yeh function check karta hai ki given time me required cars repair ho
     // sakti hai ya nahi
 
-    bool canFixEnoughCars(const vector<int>& ranks, int cars, long time) {
-        long totalCars = 0;
+    bool is_Possible(vector<int>& ranks, ll cars, ll time) {
+        ll totalCars = 0;
         for (int rank : ranks) {
             totalCars += sqrt(time / rank); // har mechanic ka max possible
                                             // repaired cars count kar rahe hain
@@ -15,13 +16,13 @@ public:
         return false;
     }
 
-    long long repairCars(vector<int>& ranks, int cars) {
-        long left = 0;
-        long right = *max_element(ranks.begin(), ranks.end()) * (long)cars * cars;
+   ll repairCars(vector<int>& ranks, int cars) {
+        ll left = 1;
+        ll right = *max_element(ranks.begin(), ranks.end()) * (1LL * cars * cars);
 
         while (left < right) {
-            long mid = (left + right) >> 1;
-            if (canFixEnoughCars(ranks, cars, mid))
+            ll mid = (left + right) >> 1;
+            if (is_Possible(ranks, cars, mid))
                 right = mid; // agar cars repair ho sakti hai toh aur kam time
                              // check karenge
             else
