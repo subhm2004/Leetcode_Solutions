dd59f97731e940ce7bfc0cdc7d686681dec49029
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-16 23:42:36 +0530

    Time: 2 ms (38.16%) | Memory: 8 MB (27.63%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,7 +1,7 @@
 class Solution {
- public:
-  bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-    int minStep = max(abs(sx - fx), abs(sy - fy));  // Chebyshev distance
-    return minStep == 0 ? t != 1 : minStep <= t;
-  }
+public:
+    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int minStep = max(abs(sx - fx), abs(sy - fy)); // Chebyshev distance
+        return minStep == 0 ? t != 1 : minStep <= t;
+    }
 };
