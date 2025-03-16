e9e86d0e8f74ec34760ad82bc2f40e881a6bfcc1
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-16 14:17:26 +0530

    Time: 72 ms (39.59%) | Memory: 319.2 MB (35.97%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -1,26 +1,40 @@
-class Solution {
+class Solution {          // DIFFERENCE ARRAY TECHNIQUE SE HUA HAI 
 public:
-   bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+    bool checkWithDifferenceArrayTeq(vector<int>& nums, vector<vector<int>>& queries, int k) {
         int n = nums.size();
-        vector<int> diff(n + 1, 0);  // Difference array for range updates
-        int sum = 0, k = 0;  
+        vector<int> diff(n, 0);
 
-        for (int i = 0; i < n; i++) {
-            while (sum + diff[i] < nums[i]) {
-                if (k >= queries.size()) return false;  // Not enough queries to make nums[i] zero
-
-                // Extract query details
-                int l = queries[k][0], r = queries[k][1], val = 1;  // Decrease by `1`
-                k++;  // Move to the next query
-
-                // Apply the query only if it affects index `i`
-                if (r >= i) {
-                    diff[max(l, i)] += val;
-                    diff[r + 1] -= val;
-                }
-            }
-            sum += diff[i];  // Apply cumulative effect for `i`
+        // Apply queries up to indeval k
+        for (int i = 0; i <= k; i++) {
+            int l = queries[i][0], r = queries[i][1], val = 1;
+            diff[l] += val;
+            if (r + 1 < n) diff[r + 1] -= val;
         }
-        return true;  // If all elements become zero, return true
+
+        int cumSum = 0;
+        for (int i = 0; i < n; i++) {
+            cumSum += diff[i];
+            if (nums[i] - cumSum > 0) return false; // nums[i] couldn't be reduced to zero
+        }
+
+        return true;
+    }
+
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        if (all_of(nums.begin(), nums.end(), [](int val) { return val == 0; })) {
+            return true; // Return true instead of 0
+        }
+
+        int left = 0, right = queries.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (checkWithDifferenceArrayTeq(nums, queries, mid)) {
+                return true;  // If it works, the array can be reduced to zero
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false; 
     }
 };
