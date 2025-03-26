2815197dccbe9af6bd27cad2777974baf3a73908
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-03-26 11:59:44 +0530

    Time: 36 ms (72.95%) | Memory: 87 MB (79.03%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -19,7 +19,7 @@ class Solution {
     }
 
     // Ab elements ko sort kar lo
-    sort(nums.rbegin(), nums.rend());
+    sort(nums.begin(), nums.end());
 
     // Median nikal lo, kyunki minimum operations ke liye median best hota hai
     int median = nums[total / 2];
