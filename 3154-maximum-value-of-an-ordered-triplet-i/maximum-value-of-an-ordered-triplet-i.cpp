53443964c02082cf032143ebf151e8f439e43f95
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-02 14:57:15 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (88.55%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,17 +1,30 @@
+// class Solution {
+// public:
+//     long long maximumTripletValue(vector<int>& nums) {
+//         long long maxTriplet = 0;
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 for (int k = j + 1; k < n; k++) {
+//                     long long value = (nums[i] - nums[j]) * 1LL * nums[k];
+//                     maxTriplet = max(maxTriplet, value);
+//                 }
+//             }
+//         }
+//         return maxTriplet;
+//     }
+// };
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        long long maxTriplet = 0;
         int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    long long value = (nums[i] - nums[j]) * 1LL * nums[k];
-                    maxTriplet = max(maxTriplet, value);
-                }
-            }
+        long long ans = 0, imax = 0, dmax = 0;
+        for (int k = 0; k < n; k++) {
+            ans = max(ans, dmax * nums[k]);
+            dmax = max(dmax, imax - nums[k]);
+            imax = max(imax, 1LL * nums[k]);
         }
-        return maxTriplet;
+        return ans;
     }
 };
