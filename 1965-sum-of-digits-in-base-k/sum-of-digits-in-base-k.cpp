31f2551f0796133ba2ba1271538b4b6fbbc56ebf
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-11 12:59:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (98.48%) - LeetSync

--- 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.cpp
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.cpp
@@ -1,43 +1,59 @@
 class Solution {
 public:
-    string convertToBaseN(int num, int base) {
-        if (base < 2) {
-            throw invalid_argument("Base must be at least 2.");
-        }
-
-        if (num == 0) {
-            return "0";
-        }
-
-        const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        string result;
-
-        bool isNegative = num < 0;
-        num = abs(num);
-
-        while (num > 0) {
-            result = digits[num % base] + result;
-            num /= base;
-        }
-
-        if (isNegative) {
-            result = "-" + result;
-        }
-
-        return result;
-    }
-
-    int sumOfDigits(int num) {
+    int sumBase(int n, int k) {
         int sum = 0;
-        while (num != 0) {
-            sum += num % 10; // get the last digit
-            num /= 10;       // remove the last digit
+
+        while (n) {
+            sum += n % k;
+            n /= k;
         }
+
         return sum;
     }
-
-    int sumBase(int n, int k) {
-        int num = stoi(convertToBaseN(n, k));
-        return sumOfDigits(num);
-    }
 };
+
+
+// class Solution {
+// public:
+//     string convertToBaseN(int num, int base) {
+//         if (base < 2) {
+//             throw invalid_argument("Base must be at least 2.");
+//         }
+
+//         if (num == 0) {
+//             return "0";
+//         }
+
+//         const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//         string result;
+
+//         bool isNegative = num < 0;
+//         num = abs(num);
+
+//         while (num > 0) {
+//             result = digits[num % base] + result;
+//             num /= base;
+//         }
+
+//         if (isNegative) {
+//             result = "-" + result;
+//         }
+
+//         return result;
+//     }
+
+//     int sumOfDigits(int num) {
+//         int sum = 0;
+//         while (num != 0) {
+//             sum += num % 10; // get the last digit
+//             num /= 10;       // remove the last digit
+//         }
+//         return sum;
+//     }
+
+//     int sumBase(int n, int k) {
+//         int num = stoi(convertToBaseN(n, k));
+//         return sumOfDigits(num);
+//     }
+// };
+
