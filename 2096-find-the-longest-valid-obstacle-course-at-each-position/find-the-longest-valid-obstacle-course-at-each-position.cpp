778b0e5b8ac1122f68c67d174094c909df95a8ee
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-11 19:10:25 +0530

    Time: 38 ms (62.08%) | Memory: 127 MB (46.88%) - LeetSync

---
+++ 2096-find-the-longest-valid-obstacle-course-at-each-position/find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+
+    // Ye function wo position return karta hai jahan par
+    // current obstacle (target) ko lis me daalna chahiye
+    // Aisi position jahan pehle element usse chhota ya barabar ho
+    int findPosition(vector<int>& lis, int target) {
+        int low = 0, high = lis.size();
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            // Agar mid waala element <= target hai to
+            // iska matlab target right me jayega
+            if (lis[mid] <= target) {
+                low = mid + 1;
+            } else {
+                // Agar bada mila, to search left me
+                high = mid;
+            }
+        }
+
+        // yahi wo position hai jahan target ko insert/update karna chahiye
+        return low;
+    }
+    
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+        vector<int> ans;  // Final answer store karega
+        vector<int> lis;  // Ye non-decreasing subsequence ka tail store karta hai
+
+        for (int i = 0; i < obstacles.size(); ++i) {
+            int h = obstacles[i];  // Current obstacle height
+
+            // findPosition se wo index milega jahan h ko daal sakte ho
+            int index = findPosition(lis, h);
+
+            // Agar h sabse bada hai to lis me add kar do
+            if (index == lis.size()) {
+                lis.push_back(h);
+            } else {
+                // Warna us position par update kar do (smaller height maintain rakhne ke liye)
+                lis[index] = h;
+            }
+
+            // index + 1 is the length of longest obstacle course ending at i
+            ans.push_back(index + 1);
+        }
+
+        return ans;
+    }
+};
