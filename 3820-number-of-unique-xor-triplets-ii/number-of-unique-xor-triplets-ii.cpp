8c1024b4995e849a6fdea5d726ed8a7a07230964
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-13 11:46:46 +0530

    Time: 2180 ms (54.29%) | Memory: 100.4 MB (71.43%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,3 +1,31 @@
+class Solution { // PRE COMPUTATON OF XOR VALUES SE HUA HAI 
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        unordered_set<int> set_1;  
+        int n = nums.size();
+        if (n < 3) return n;  
+
+        // Sabhi pairs ke XOR ko 'set_1' mein store karte hain
+        for (int i = 0; i < n; i++) {
+            for (int j = i ; j < n; j++) {  // j ko i ke baad se start karenge, taaki duplicates na ho
+                set_1.insert(nums[i] ^ nums[j]);
+            }
+        }
+
+        unordered_set<int>set_2;
+        // 'set_1' ke XOR values ko nums ke har element ke saath XOR karenge aur triplet XOR find karenge
+        for (int num : set_1) {
+            for (int x : nums) {
+                set_2.insert(num ^ x);
+            }
+        }
+
+        // 'set_2' ka size unique triplet XORs ka count dega
+        return set_2.size();  
+    }
+};
+
+
 // class Solution {
 // public:
 //     int uniqueXorTriplets(vector<int>& nums) {
@@ -18,31 +46,3 @@
 //     }
 // };
 
-
-
-class Solution {
-public:
-    int uniqueXorTriplets(vector<int>& nums) {
-        unordered_set<int> set_1;  
-        int n = nums.size();
-        if (n < 3) return n;  
-
-        // Sabhi pairs ke XOR ko 'set_1' mein store karte hain
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {  // j ko i ke baad se start karenge, taaki duplicates na ho
-                set_1.insert(nums[i] ^ nums[j]);
-            }
-        }
-
-        unordered_set<int>set_2;
-        // 'set_1' ke XOR values ko nums ke har element ke saath XOR karenge aur triplet XOR find karenge
-        for (int num : set_1) {
-            for (int x : nums) {
-                set_2.insert(num ^ x);
-            }
-        }
-
-        // 'set_2' ka size unique triplet XORs ka count dega
-        return set_2.size();  
-    }
-};
