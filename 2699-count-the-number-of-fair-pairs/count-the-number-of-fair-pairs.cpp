e6528d62be0dd76c164ff3884ba962e5fa016d5c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-19 12:11:42 +0530

    Time: 56 ms (55.54%) | Memory: 60.4 MB (73.14%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,37 +1,45 @@
+// typedef long long ll;
+// class Solution {
+// public:
+//     ll countFairPairs(vector<int>& nums, int lower, int upper) {
+//         ll count = 0;
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = i + 1; j < n; ++j) {
+//                 int sum = nums[i] + nums[j];
+//                 if (sum >= lower && sum <= upper) {
+//                     ++count;
+//                 }
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
 typedef long long ll;
+
 class Solution {
 public:
-    int lowerBound(const vector<int>& nums, int start, int end, int target) {
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        return start;
-    }
-
-    int upperBound(const vector<int>& nums, int start, int end, int target) {
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] <= target) {
-                start = mid + 1;
-            } else {
-                end = mid;
-            }
-        }
-        return start;
-    }
     ll countFairPairs(vector<int>& nums, int lower, int upper) {
-        ll ans = 0;
+        ll count = 0;
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < nums.size(); ++i) {
-            int j = lowerBound(nums, i + 1, nums.size(), lower - nums[i]);
-            int k = upperBound(nums, i + 1, nums.size(), upper - nums[i]);
-            ans += k - j;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            int left = lower - nums[i];
+            int right = upper - nums[i];
+
+            // j > i only, so we search in nums[i+1 ... n-1]
+            auto low = lower_bound(nums.begin() + i + 1, nums.end(), left);
+            auto high = upper_bound(nums.begin() + i + 1, nums.end(), right);
+
+            count += high - low;
         }
-        return ans;
+
+        return count;
     }
 };
+
