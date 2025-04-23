05805cd7cb3307341ac35735f01f03525fb945b6
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-23 18:48:21 +0530

    Time: 4 ms (13.22%) | Memory: 8.1 MB (44.07%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int sum_of_digits(int num) {
+        int sum = 0;
+        while (num) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
+    int countLargestGroup(int n) {
+        unordered_map<int, int> mp; 
+        // key   = digit sum of a number
+        // value = how many numbers had that digit sum
+
+        int maxSize = 0;
+        int count = 0;
+
+        for (int num = 1; num <= n; num++) {
+            int digitsSum = sum_of_digits(num);
+
+            mp[digitsSum]++;
+            if (mp[digitsSum] == maxSize) {
+                count++;
+            } else if (mp[digitsSum] > maxSize) {
+                maxSize = mp[digitsSum];
+                count = 1;
+            }
+        }
+
+        return count;
+    }
+};
