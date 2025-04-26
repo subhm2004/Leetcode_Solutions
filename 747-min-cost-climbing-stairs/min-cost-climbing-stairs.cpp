73a2c88416fa85afa4c4db0475b90f3a4f6f9013
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-26 16:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (12.01%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int solveRE(vector<int>& cost, int i) {
-        if (i < 0)
-            return 0;
-        if (i < 2)
-            return cost[i];
-        return cost[i] + min(solveRE(cost, i - 1), solveRE(cost, i - 2));
+        if (i >= cost.size()) return 0; // base case: if we cross stairs
+        return cost[i] + min(solveRE(cost, i + 1), solveRE(cost, i + 2));
     }
 
     int solveME(vector<int>& cost, vector<int>& dp, int i) {
-        if (i < 0)
-            return 0;
-        if (i < 2)
-            return cost[i];
-        if (dp[i] != -1)
-            return dp[i];
+        if (i >= cost.size()) return 0;
+        if (dp[i] != -1) return dp[i];
 
-       return dp[i] = cost[i] +
-                min(solveME(cost, dp, i - 1), solveME(cost, dp, i - 2));
+        return dp[i] = cost[i] + min(solveME(cost, dp, i + 1), solveME(cost, dp, i + 2));
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(), -1);
+        int n = cost.size();
+        vector<int> dp(n, -1);
 
-        return min(solveME(cost, dp, cost.size() - 1),
-                   solveME(cost, dp, cost.size() - 2));
+        // return min(solveRE(cost, 0), solveRE(cost, 1));
+
+        return min(solveME(cost, dp, 0), solveME(cost, dp, 1));
     }
 };
