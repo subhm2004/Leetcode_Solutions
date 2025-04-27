a477ec0b6045ba869c5708d767bd24e88b9d85d9
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-27 07:53:48 +0530

    Time: 0 ms (100.00%) | Memory: 48.5 MB (11.51%) - LeetSync

--- 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
+++ 3685-count-subarrays-of-length-three-with-a-condition/count-subarrays-of-length-three-with-a-condition.cpp
@@ -4,21 +4,16 @@ public:
         int count = 0;
         int n = nums.size();
 
-        for (int i = 0; i < n; ++i) {                  // First element
-            for (int j = i + 2; j < n; ++j) {           // Third element (minimum gap of 2)
-                if (j - i == 2) {                       // Only subarrays of length 3
-                    int a = nums[i];
-                    int b = nums[i + 1];
-                    int c = nums[j];
+        for (int i = 0; i <= n - 3; ++i) {
+            int a = nums[i];
+            int b = nums[i + 1];
+            int c = nums[i + 2];
 
-                    if (b % 2 == 0 && (a + c) == (b / 2)) {
-                        ++count;
-                    }
-                }
+            if (b % 2 == 0 && (a + c) == (b / 2)) {
+                ++count;
             }
         }
 
         return count;
     }
 };
-
