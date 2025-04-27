69e7c5fac8f1b55fb0b23b5f1b04a28f0511b06e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-28 01:35:25 +0530

    Time: 211 ms (21.18%) | Memory: 47.1 MB (35.96%) - LeetSync

---
+++ 3414-find-number-of-ways-to-reach-the-k-th-stair/find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -0,0 +1,66 @@
+typedef unsigned long long ull;
+class Solution {
+public:
+    ull solveRE(ull i, ull jump, bool canGoBack, ull K, vector<long long>& power) {
+        // Agar i K+1 se zyada ho gaya, toh valid move nahi hai
+        if (i > K + 1)
+            return 0;
+
+        ull ans_ways = 0;
+
+        // Agar hum K-th stair pe pahuch gaye hain, toh ans_ways ko increment kar lo
+        if (i == K) {
+            ans_ways++;  // Hum yahan ruk bhi sakte hain, ya agle moves bhi try kar sakte hain
+        }
+
+        // Agar hum back jaa sakte hain (canGoBack = true ), toh ek step neeche chalne ki koshish karo
+        if (canGoBack == true) {
+            ans_ways += solveRE(i - 1, jump, false , K, power);  // Neeche jao aur canGoBack ko 0 kar do
+        }
+
+        // Jump up karte hain 2^jump distance ke hisaab se, aur phir back jaane ki permission de dete hain
+        ans_ways += solveRE(i + power[jump], jump + 1, true , K, power);
+
+        return ans_ways;  // Total possible ways return karo
+    }
+
+
+    ull solveME(ull i, ull jump, bool canGoBack, ull K, vector<long long>& power, unordered_map<string, ull>& dp) {
+        if (i > K + 1)
+            return 0;
+
+        auto key = to_string(i) + "_" + to_string(jump) + "_" + to_string(canGoBack);
+
+        if (dp.find(key) != dp.end())
+            return dp[key];
+
+        ull ans_ways = 0;
+
+        if (i == K) {
+            ans_ways++;
+        }
+
+        if (canGoBack == true ) {
+            ans_ways += solveME(i - 1, jump, false , K, power, dp);
+        }
+
+        ans_ways += solveME(i + power[jump], jump + 1, true , K, power, dp);
+
+        return dp[key] = ans_ways;
+    }
+
+    ull waysToReachStair(ull k) {
+        unordered_map<string, ull> dp;
+
+        vector<long long> power(64);  
+        
+        for (ull i = 0; i < 63; ++i) {
+            // power[i] = pow(2, i);
+            power[i] = (1LL << i);  // 2^i
+        }
+
+        // return solveRE(1, 0, 1, k, power); 
+        return solveME(1, 0, 1, k, power, dp);
+
+    }
+};
