54953ad24625f133e2c5571c3404711993ec8b13
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-04-28 11:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (59.25%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,22 +1,50 @@
-typedef unsigned long long ull;
+// typedef unsigned long long ull;
+// class Solution {
+// public:
+//     ull numSubarraysWithSum(vector<int>& nums, ull goal) {
+//         unordered_map<ull, ull> prefixCount;
+//         prefixCount[0] = 1; // base case: empty prefix
+        
+//         ull ans = 0, currSum = 0;
+        
+//         for (auto num : nums) {
+//             currSum += num;
+            
+//             if (prefixCount.find(currSum - goal) != prefixCount.end()) {
+//                 ans += prefixCount[currSum - goal];
+//             }
+            
+//             prefixCount[currSum]++;
+//         }
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    ull numSubarraysWithSum(vector<int>& nums, ull goal) {
-        unordered_map<ull, ull> prefixCount;
-        prefixCount[0] = 1; // base case: empty prefix
+    // ye function find krega number od subarray with sum<= goal
+    int find_Total_sub_array(vector<int>& nums, int goal) {
+        if (goal < 0) return 0;
         
-        ull ans = 0, currSum = 0;
+        int left = 0, ans = 0, sum = 0;
         
-        for (auto num : nums) {
-            currSum += num;
+        for (int right = 0; right < nums.size(); ++right) {
+            sum += nums[right];
             
-            if (prefixCount.find(currSum - goal) != prefixCount.end()) {
-                ans += prefixCount[currSum - goal];
+            while (sum > goal) {
+                sum -= nums[left];
+                left++;
             }
             
-            prefixCount[currSum]++;
+            ans += (right - left + 1);
         }
         
         return ans;
+    }
+    
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        // Atmost K ka sum nikla diya hai isse
+        return find_Total_sub_array(nums, goal) - find_Total_sub_array(nums, goal - 1);
     }
 };
