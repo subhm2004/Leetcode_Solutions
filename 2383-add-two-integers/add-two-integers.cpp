6da3e6e780ab76723f71f6626e833a6298f60d93
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-03 00:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (95.07%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.cpp
@@ -0,0 +1,13 @@
+// class Solution {
+// public:
+//     int sum(int num1, int num2) {
+//         return num1 + num2;
+//     }
+// };
+
+#define Code class Solution {
+#define is public: int sum(
+#define poetry int a, int b) {
+#define in return a + b; }
+#define motion };
+Code is poetry in motion
