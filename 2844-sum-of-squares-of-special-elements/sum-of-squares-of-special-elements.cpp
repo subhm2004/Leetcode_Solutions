d17abcaf2d210c0987cbfb11e26a1290cd59b46e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-03 20:34:45 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (72.70%) - LeetSync

---
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        
+        for (int i = 1; i <= n; ++i) {
+            if (n % i == 0) {
+                sum += nums[i - 1] * nums[i - 1]; // convert 1-based to 0-based
+            }
+        }
+        
+        return sum;
+    }
+};
