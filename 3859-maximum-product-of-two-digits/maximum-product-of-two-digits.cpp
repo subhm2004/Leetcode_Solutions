15a2d31b6f09100ef2d08f4a5a65b2c041b172cd
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-04 15:09:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (89.51%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,23 +1,43 @@
+// class Solution {
+// public:
+//     int maxProduct(int n) {
+//         vector<int> digits;
+
+//         // Step 1: Extract digits from n
+//         while (n > 0) {
+//             digits.push_back(n % 10);
+//             n /= 10;
+//         }
+//         int m = digits.size();
+//         int max_Prod = 0;
+
+//         // Step 2: Try all pairs 
+//         for (int i = 0; i < m; ++i) {
+//             for (int j = i+1; j < m; ++j) {
+//                 max_Prod = max(max_Prod, digits[i] * digits[j]);
+//             }
+//         }
+
+//         return max_Prod;
+//     }
+// };
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int> digits;
+        int max1 = 0, max2 = 0;
 
-        // Step 1: Extract digits from n
+        // Go through each digit
         while (n > 0) {
-            digits.push_back(n % 10);
+            int digit = n % 10;
+            if (digit >= max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
+            }
             n /= 10;
         }
-        int m = digits.size();
-        int max_Prod = 0;
 
-        // Step 2: Try all pairs 
-        for (int i = 0; i < m; ++i) {
-            for (int j = i+1; j < m; ++j) {
-                max_Prod = max(max_Prod, digits[i] * digits[j]);
-            }
-        }
-
-        return max_Prod;
+        return max1 * max2;
     }
 };
