27aa263924eaf8a3a0c12b688a04256a08eceaf7
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-05 08:29:37 +0530

    Time: 14 ms (93.99%) | Memory: 24.2 MB (6.69%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -1,8 +1,36 @@
+// class Solution {
+// public:
+//     int maxProductDifference(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]);
+//     }
+// };
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]);
+        int lowest = INT_MAX;
+        int secondLowest = INT_MAX;
+        int secondHighest = INT_MIN;
+        int highest = INT_MIN;
+        for (int num: nums) {
+            if (num < lowest) {
+                secondLowest = lowest;
+                lowest = num;
+            }
+            else if (num < secondLowest) {
+                secondLowest = num;
+            }
+            
+            if (num > highest) {
+                secondHighest = highest;
+                highest = num;
+            }
+            else if (num > secondHighest) {
+                secondHighest = num;
+            }
+        }
+
+        return (highest * secondHighest) - (secondLowest * lowest);
     }
 };
