9ff03041cdc6d45d7cf69cd952db8d98231ee01e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-17 18:46:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (20.90%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -16,19 +16,62 @@
 //   }
 // };
 
-class Solution {
-public:
-    int solveME(int n, vector<int>& dp) {
-        if (n == 1 || n == 0)
-            return n;
-        if (dp[n] != -1)
-            return dp[n];
+// class Solution {
+// public:
+//     int solveME(int n, vector<int>& dp) {
+//         if (n == 1 || n == 0)
+//             return n;
+//         if (dp[n] != -1)
+//             return dp[n];
 
-        return dp[n] = solveME(n - 1, dp) + solveME(n - 2, dp);
-    }
-    int fib(int n) {
-        vector<int> dp(n + 1, -1);
-        int ans = solveME(n, dp);
+//         return dp[n] = solveME(n - 1, dp) + solveME(n - 2, dp);
+//     }
+//     int fib(int n) {
+//         vector<int> dp(n + 1, -1);
+//         int ans = solveME(n, dp);
+//         return ans;
+//     }
+// };
+
+class Solution {
+private:
+    const int MOD = 1e9 + 7;
+
+    // Multiply two n x n matrices
+    vector<vector<long long>> multiply(const vector<vector<long long>>& A,
+                                       const vector<vector<long long>>& B) {
+        int n = A.size();
+        vector<vector<long long>> ans(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                for (int k = 0; k < n; ++k)
+                    ans[i][j] = (ans[i][j] + A[i][k] * B[k][j]) % MOD;
         return ans;
+    }
+
+    // Exponentiate an n x n matrix
+    vector<vector<long long>> matrixExpo(vector<vector<long long>> A,
+                                         int power) {
+        int n = A.size();
+        vector<vector<long long>> ans(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; ++i)
+            ans[i][i] = 1;
+
+        while (power > 0) {
+            if (power % 2 == 1)
+                ans = multiply(ans, A);
+            A = multiply(A, A);
+            power /= 2;
+        }
+        return ans;
+    }
+
+public:
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        vector<vector<long long>> base = {{1, 1}, {1, 0}};
+        vector<vector<long long>> ansult = matrixExpo(base, n - 1);
+        return ansult[0][0];
     }
 };
