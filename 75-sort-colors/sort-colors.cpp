b545a94ba1be037126a7a8f40b90cb3ed2ac32cd
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-17 15:43:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.91%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,52 @@
-class Solution {
- public:
-  void sortColors(vector<int>& nums) {
-    int zero = -1;
-    int one = -1;
-    int two = -1;
+// class Solution {
+//  public:
+//   void sortColors(vector<int>& nums) {
+//     int zero = -1;
+//     int one = -1;
+//     int two = -1;
 
-    for (const int num : nums)
-      if (num == 0) {
-        nums[++two] = 2;
-        nums[++one] = 1;
-        nums[++zero] = 0;
-      } else if (num == 1) {
-        nums[++two] = 2;
-        nums[++one] = 1;
-      } else {
-        nums[++two] = 2;
-      }
-  }
+//     for (const int num : nums)
+//       if (num == 0) {
+//         nums[++two] = 2;
+//         nums[++one] = 1;
+//         nums[++zero] = 0;
+//       } else if (num == 1) {
+//         nums[++two] = 2;
+//         nums[++one] = 1;
+//       } else {
+//         nums[++two] = 2;
+//       }
+//   }
+// };
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        
+        int count_0 = 0;
+        int count_1 = 0;
+        int count_2 = 0;
+        
+        for(int &num : nums) {
+            if(num == 0)
+                count_0++;
+            else if(num == 1)
+                count_1++;
+            else
+                count_2++;
+        }
+        
+        for(int i = 0; i<n; i++) {
+            if(count_0 > 0) {
+                nums[i] = 0;
+                count_0--;
+            } else if(count_1 > 0) {
+                nums[i] = 1;
+                count_1--;
+            } else if(count_2 > 0) {
+                nums[i] = 2;
+                count_2--;
+            }
+        }
+    }
 };
