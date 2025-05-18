83164c867a7d12f8825d35ea5fcf95b2614880f1
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-18 15:32:08 +0530

    Time: 1067 ms (22.79%) | Memory: 151.7 MB (66.92%) - LeetSync

---
+++ 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int dijkstra(const vector<string>& matrix,
+                 unordered_map<char, vector<pair<int,int>>>& portal_Positions) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        if (matrix[m - 1][n - 1] == '#') return -1;
+
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        unordered_set<char> used_Portals;
+
+        using T = tuple<int, int, int>; // (distance, row, col)
+        priority_queue<T, vector<T>, greater<T>> pq;
+
+        pq.emplace(0, 0, 0);
+        dist[0][0] = 0;
+
+        vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
+        while (!pq.empty()) {
+            auto [curDist, r, c] = pq.top();
+            pq.pop();
+
+            if (r == m - 1 && c == n - 1) return curDist;
+
+            char ch = matrix[r][c];
+            if (ch >= 'A' && ch <= 'Z' && used_Portals.find(ch) == used_Portals.end()) {
+                used_Portals.insert(ch);
+                for (auto &[pr, pc] : portal_Positions[ch]) {
+                    if (curDist < dist[pr][pc]) {
+                        dist[pr][pc] = curDist;
+                        pq.emplace(curDist, pr, pc); // teleportation me cost nhi lagti acc to problem
+                    }
+                }
+            }
+
+            for (auto &[dr, dc] : directions) {
+                int nr = r + dr, nc = c + dc;
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] != '#') {
+                    if (curDist + 1 < dist[nr][nc]) {
+                        dist[nr][nc] = curDist + 1;
+                        pq.emplace(curDist + 1, nr, nc);
+                    }
+                }
+            }
+        }
+
+        return -1; // no path found
+    }
+
+    int minMoves(vector<string>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        unordered_map<char, vector<pair<int,int>>> portal_Positions;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char ch = matrix[i][j];
+                if (ch >= 'A' && ch <= 'Z') {
+                    portal_Positions[ch].emplace_back(i, j);
+                }
+            }
+        }
+
+        return dijkstra(matrix, portal_Positions);
+    }
+};
