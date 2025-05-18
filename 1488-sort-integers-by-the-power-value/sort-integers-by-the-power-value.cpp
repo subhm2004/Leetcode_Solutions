9e39652da6551ab19f7d87850a27ba15051d09ac
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-19 00:25:58 +0530

    Time: 143 ms (12.66%) | Memory: 47.6 MB (15.00%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.cpp
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    unordered_map<int, int> dp;
+
     int solveRE(int x) {
     if (x == 1) return 0;
     if (x % 2 == 0)
@@ -8,6 +10,15 @@ public:
         return 1 + solveRE(3 * x + 1);
     }
 
+    int solveME(int x) {
+        if (x == 1) return 0;
+        if (dp.count(x)) return dp[x];
+        if (x % 2 == 0)
+            return dp[x] = 1 + solveME(x / 2);
+        else
+            return dp[x] = 1 + solveME(3 * x + 1);
+    }
+    
     int getKth(int lo, int hi, int k) {
         vector<int> nums;
         for (int i = lo; i <= hi; ++i)
@@ -15,8 +26,8 @@ public:
         
         // Comparator inside sort using lambda and pure recursion
         sort(nums.begin(), nums.end(), [this](int a, int b) {
-            int pa = solveRE(a);
-            int pb = solveRE(b);
+            int pa = solveME(a);
+            int pb = solveME(b);
             if (pa == pb) return a < b;
             return pa < pb;
         });
