c4a5ce282175c59ace109a081f1f7c0239fcff9f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-27 12:52:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (70.35%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,27 +1,41 @@
+// class Solution {
+// public:
+//     int sum_Not_Divisible(int p, int q) {
+//         int sum = 0;
+//         for (int i = 1; i <= p; ++i) {
+//             if (i % q != 0) { // Check if number is not divisible by q
+//                 sum += i;
+//             }
+//         }
+//         return sum;
+//     }
+
+//     int sum_Divisible(int p, int q) {
+//         int sum = 0;
+//         for (int i = 1; i <= p; ++i) {
+//             if (i % q == 0) { // Check if number is divisible by q
+//                 sum += i;
+//             }
+//         }
+//         return sum;
+//     }
+//     int differenceOfSums(int n, int m) {
+//         int num1 = sum_Not_Divisible(n, m);
+//         int num2 = sum_Divisible(n, m);
+//         return num1 - num2;
+//     }
+// };
+
 class Solution {
 public:
-    int sum_Not_Divisible(int p, int q) {
-        int sum = 0;
-        for (int i = 1; i <= p; ++i) {
-            if (i % q != 0) { // Check if number is not divisible by q
-                sum += i;
-            }
-        }
-        return sum;
-    }
-
-    int sum_Divisible(int p, int q) {
-        int sum = 0;
-        for (int i = 1; i <= p; ++i) {
-            if (i % q == 0) { // Check if number is divisible by q
-                sum += i;
-            }
-        }
-        return sum;
-    }
     int differenceOfSums(int n, int m) {
-        int num1 = sum_Not_Divisible(n, m);
-        int num2 = sum_Divisible(n, m);
-        return num1 - num2;
+        int sum_Divisible = 0, sum_Not_Divisible = 0;
+        for(int i = 1; i <= n; i++) {
+            if (i % m == 0)
+                sum_Divisible += i;       // divisible by m
+            else
+                sum_Not_Divisible += i;      // not divisible by m
+        }
+        return sum_Not_Divisible - sum_Divisible;
     }
 };
