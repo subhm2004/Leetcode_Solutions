9d204bbc2a38f3af1d58e1f6ad40d636ff080def
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-05-28 14:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (20.62%) - LeetSync

--- 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.cpp
+++ 2480-find-subarrays-with-equal-sum/find-subarrays-with-equal-sum.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     bool findSubarrays(vector<int>& nums) {
-        // unordered_set<int> seen;
-        // for (int i = 0; i < nums.size() - 1; ++i) {
-        //     int sum = nums[i] + nums[i + 1];
-        //     if (seen.count(sum))
-        //         return true;
-        //     seen.insert(sum);
-        // }
-        // return false;
-        int n = nums.size();
-        for (int i = 0; i < n - 1; ++i) {
-            int sum1 = nums[i] + nums[i + 1];
-            for (int j = 0; j < i; ++j) {
-                int sum2 = nums[j] + nums[j + 1];
-                if (sum1 == sum2)
-                    return true;
-            }
+        unordered_set<int> seen;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            int sum = nums[i] + nums[i + 1];
+            if (seen.count(sum))
+                return true;
+            seen.insert(sum);
         }
         return false;
     }
