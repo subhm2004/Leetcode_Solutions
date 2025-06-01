51f3a9950e08965757e4fe1da7c5998f32ef8bfb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-01 21:12:22 +0530

    Time: 14 ms (18.47%) | Memory: 8.5 MB (59.66%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.cpp
@@ -1,44 +1,58 @@
-// class Solution {
-// public:
-//     long nCk(int n, int k) {
-//         long res = 1;
-//         for (int i = 1; i <= k; ++i)
-//             res = res * (n - i + 1) / i;
-//         return res;
-//     }
-
-//     long ways(int n) {
-//         if (n < 0)
-//             return 0;
-//         return nCk(n + 2,
-//                    2); // Using stars and bars method (Beggar's coin method )
-//     }
-
-//     int distributeCandies(int n, int limit) {
-//         int limitPlusOne = limit + 1;
-//         long oneChildExceedsLimit = ways(n - limitPlusOne);
-//         long twoChildrenExceedLimit = ways(n - 2 * limitPlusOne);
-//         long threeChildrenExceedLimit = ways(n - 3 * limitPlusOne);
-//         // Principle of Inclusion-Exclusion
-//         return ways(n) - 3 * oneChildExceedsLimit + 3 * twoChildrenExceedLimit -
-//                threeChildrenExceedLimit;
-//     }
-// };
-
 class Solution {
 public:
-    long long distributeCandies(int n, int limit) {
-        int ways = 0;
+    long long solveRE(int children_left, int candies_left, int limit) { // TLE
+        if (children_left == 0) {
+            return candies_left == 0 ? 1 : 0;
+        }
 
-        for (int x1 = 0; x1 <= min(n, limit); ++x1) {
-            for (int x2 = 0; x2 <= min(n - x1, limit); ++x2) {
-                int x3 = n - x1 - x2;
-                if (x3 >= 0 && x3 <= limit) {
-                    ++ways;
-                }
-            }
+        long long ways = 0;
+        for (int i = 0; i <= min(limit, candies_left); ++i) {
+            ways += solveRE(children_left - 1, candies_left - i, limit);
         }
 
         return ways;
     }
+
+    long nCk(int n, int k) {
+        long res = 1;
+        for (int i = 1; i <= k; ++i)
+            res = res * (n - i + 1) / i;
+        return res;
+    }
+
+    long ways(int n) {
+        if (n < 0)
+            return 0;
+        return nCk(n + 2, 2); // Using stars and bars method (Beggar's coin method )
+    }
+
+    int distributeCandies(int n, int limit) {
+        int limitPlusOne = limit + 1;
+        long oneChildExceedsLimit = ways(n - limitPlusOne);
+        long twoChildrenExceedLimit = ways(n - 2 * limitPlusOne);
+        long threeChildrenExceedLimit = ways(n - 3 * limitPlusOne);
+        // Principle of Inclusion-Exclusion
+        // return ways(n) - 3 * oneChildExceedsLimit + 3 * twoChildrenExceedLimit -
+        //        threeChildrenExceedLimit;
+        return solveRE(3, n, limit);
+
+    }
 };
+
+// class Solution {
+// public:
+//     long long distributeCandies(int n, int limit) {
+//         int ways = 0;
+
+//         for (int x1 = 0; x1 <= min(n, limit); ++x1) {
+//             for (int x2 = 0; x2 <= min(n - x1, limit); ++x2) {
+//                 int x3 = n - x1 - x2;
+//                 if (x3 >= 0 && x3 <= limit) {
+//                     ++ways;
+//                 }
+//             }
+//         }
+
+//         return ways;
+//     }
+// };
