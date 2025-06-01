01cf8e4c75770886654495a62968d71d547a516a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-01 11:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.71%) - LeetSync

--- 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-    long nCk(int n, int k) {
-        long res = 1;
-        for (int i = 1; i <= k; ++i)
-            res = res * (n - i + 1) / i;
-        return res;
+    long nCr(int n, int r) {
+        long ans = 1;
+        for (int i = 1; i <= r; ++i)
+            ans = ans * (n - i + 1) / i;
+        return ans;
     }
 
-    long ways(int n) {
+    long ways(int n) { 
         if (n < 0)
             return 0;
-        return nCk(n + 2, 2); // Using stars and bars method (Beggar's coin method )
+        return nCr(n + 3 - 1, 3 - 1); // Using stars and bars method (Beggar's coin method )
+    }
+
+    long long ways2(int n) { // n+2 C 2 hai jo ki (n+2)*(n+1)/2 likhi ja skti hai 
+        if (n < 0) return 0;
+        return 1LL * (n + 2) * (n + 1) / 2;
     }
 
   long long distributeCandies(int n, int limit) {
-    int limitPlusOne = limit + 1;
-    long oneChildExceedsLimit = ways(n - limitPlusOne);
-    long twoChildrenExceedLimit = ways(n - 2 * limitPlusOne);
-    long threeChildrenExceedLimit = ways(n - 3 * limitPlusOne);
+    int limit_Plus_One = limit + 1;
+    long one_Child_Exceeds_Limit = ways2(n - limit_Plus_One);
+    long two_Children_Exceed_Limit = ways2(n - 2 * limit_Plus_One);
+    long three_Children_Exceed_Limit = ways2(n - 3 * limit_Plus_One);
+    
     // Principle of Inclusion-Exclusion  
-    return ways(n) - 3 * oneChildExceedsLimit + 3 * twoChildrenExceedLimit -
-           threeChildrenExceedLimit;
+    return ways2(n) - 3 * one_Child_Exceeds_Limit + 3 * two_Children_Exceed_Limit -
+           three_Children_Exceed_Limit;
   }
 };
 
