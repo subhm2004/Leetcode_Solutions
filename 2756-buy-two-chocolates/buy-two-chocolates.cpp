f44b7fa5f87f042320c947320fbc7ac541517ea5
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-09 01:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 52.2 MB (30.08%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,8 +1,29 @@
+// class Solution {
+// public:
+//     int buyChoco(vector<int>& prices, int money) {
+//         sort(prices.begin(), prices.end());
+//         int sum = prices[0] + prices[1];
+//         if (sum <= money) {
+//             return money - sum;
+//         }
+//         return money;
+//     }
+// };
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-        int sum = prices[0] + prices[1];
+        int min1 = INT_MAX, min2 = INT_MAX;
+        
+        for (int price : prices) {
+            if (price < min1) {
+                min2 = min1;
+                min1 = price;
+            } else if (price < min2) {
+                min2 = price;
+            }
+        }
+        
+        int sum = min1 + min2;
         if (sum <= money) {
             return money - sum;
         }
