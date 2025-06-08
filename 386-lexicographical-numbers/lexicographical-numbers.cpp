19a579484c08e0d6fdba730bbb2a63a2a18dbf36
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-08 14:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (69.54%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,34 +1,43 @@
-class Solution {
-public:
-    vector<int> lexicalOrder(int n) {
-        vector<string> num;
-        for (int i = 1; i <= n; i++) {
-            num.push_back(to_string(i));
-        }
-        sort(num.begin(), num.end());
-        vector<int> ans;
-        for ( auto& nums : num) {
-            ans.push_back(stoi(nums));
-        }
-        return ans;
-    }
-};
 // class Solution {
 // public:
 //     vector<int> lexicalOrder(int n) {
+//         vector<string> num;
+//         for (int i = 1; i <= n; i++) {
+//             num.push_back(to_string(i));
+//         }
+//         sort(num.begin(), num.end());
 //         vector<int> ans;
-//         int curr = 1;
-//         for (int i = 0; i < n; i++) {
-//             ans.push_back(curr);
-//             if (curr * 10 <= n) {
-//                 curr *= 10;  // Move to the next depth
-//             } else {
-//                 while (curr % 10 == 9 || curr + 1 > n) {
-//                     curr /= 10;  // Backtrack if last digit is 9 or exceeds n
-//                 }
-//                 curr++;  // Move to next number
-//             }
+//         for ( auto& nums : num) {
+//             ans.push_back(stoi(nums));
 //         }
 //         return ans;
 //     }
 // };
+
+class Solution {
+public:
+    void solveRE(int curr, int n, vector<int>& ans) {
+        if (curr > n)
+            return;
+
+        ans.push_back(curr);
+
+        for (int next_Digit = 0; next_Digit <= 9; next_Digit++) {
+            int next_num = curr * 10 + next_Digit;
+
+            if (next_num > n)
+                return;
+
+            solveRE(next_num, n, ans);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+
+        for (int num = 1; num <= 9; num++)
+            solveRE(num, n, ans);
+
+        return ans;
+    }
+};
