b9e353f35c7d1653b82cd0a1963f24d690410060
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-09 00:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 52.1 MB (92.22%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        int min_sum = INT_MAX;
-        int n = prices.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int sum = prices[i] + prices[j];
-                if (sum <= money && sum < min_sum) {
-                    min_sum = sum;
-                }
-            }
+        sort(prices.begin(), prices.end());
+        int sum = prices[0] + prices[1];
+        if (sum <= money) {
+            return money - sum;
         }
-        if (min_sum == INT_MAX)
-            return money;
-        return money - min_sum;
+        return money;
     }
 };
