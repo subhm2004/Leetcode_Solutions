22345d727ed7ac1297d81c0cbdc20b288645de89
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-17 13:58:48 +0530

    Time: 225 ms (28.59%) | Memory: 128.7 MB (20.64%) - LeetSync

--- 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
+++ 2280-count-good-triplets-in-an-array/count-good-triplets-in-an-array.cpp
@@ -1,73 +1,93 @@
 class SegmentTree {
- public:
-  SegmentTree(int size) : n(size) {
-    tree.assign(4 * n, 0);
-  }
+private:
+    vector<int> segTree;
+    vector<int> arr;
+    int n;
 
-  void update(int index, int value, int node = 1, int start = 0, int end = -1) {
-    if (end == -1) end = n - 1;
-
-    if (start == end) {
-      tree[node] += value;
-      return;
+    void build(int i, int l, int r) {
+        if (l == r) {
+            segTree[i] = 0;
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        build(2 * i + 1, l, mid);
+        build(2 * i + 2, mid + 1, r);
+        segTree[i] = segTree[2 * i + 1] + segTree[2 * i + 2];
     }
 
-    int mid = (start + end) / 2;
-    if (index <= mid)
-      update(index, value, 2 * node, start, mid);
-    else
-      update(index, value, 2 * node + 1, mid + 1, end);
+    int query(int i, int l, int r, int ql, int qr) {
+        if (r < ql || l > qr) return 0;
+        if (ql <= l && r <= qr) return segTree[i];
+        int mid = l + (r - l) / 2;
+        return query(2 * i + 1, l, mid, ql, qr) +
+               query(2 * i + 2, mid + 1, r, ql, qr);
+    }
 
-    tree[node] = tree[2 * node] + tree[2 * node + 1];
-  }
+    void update(int i, int l, int r, int idx, int value) {
+        if (l == r) {
+            arr[idx] += value;
+            segTree[i] += value;
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if (idx <= mid)
+            update(2 * i + 1, l, mid, idx, value);
+        else
+            update(2 * i + 2, mid + 1, r, idx, value);
+        segTree[i] = segTree[2 * i + 1] + segTree[2 * i + 2];
+    }
 
-  int query(int l, int r, int node = 1, int start = 0, int end = -1) {
-    if (end == -1) end = n - 1;
+public:
+    SegmentTree(int size) {
+        n = size;
+        segTree.resize(4 * n);
+        arr.assign(n, 0);
+        build(0, 0, n - 1);
+    }
 
-    if (r < start || end < l)
-      return 0;
-    if (l <= start && end <= r)
-      return tree[node];
+    int query(int l, int r) {
+        if (l > r) return 0;
+        return query(0, 0, n - 1, l, r);
+    }
 
-    int mid = (start + end) / 2;
-    return query(l, r, 2 * node, start, mid) +
-           query(l, r, 2 * node + 1, mid + 1, end);
-  }
+    void update(int idx, int value) {
+        update(0, 0, n - 1, idx, value);
+    }
 
- private:
-  int n;
-  vector<int> tree;
+    int get(int idx) {
+        return arr[idx];
+    }
 };
 
 class Solution {
- public:
-  long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
-    int n = nums1.size();
-    unordered_map<int, int> numToIndex;
-    for (int i = 0; i < n; ++i)
-      numToIndex[nums1[i]] = i;
+public:
+    long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        unordered_map<int, int> posInNums1;
+        for (int i = 0; i < n; ++i)
+            posInNums1[nums1[i]] = i;
 
-    vector<int> arr(n);
-    for (int i = 0; i < n; ++i)
-      arr[i] = numToIndex[nums2[i]];
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i)
+            arr[i] = posInNums1[nums2[i]];
 
-    vector<int> leftSmaller(n), rightLarger(n);
-    SegmentTree leftTree(n), rightTree(n);
+        vector<int> leftSmaller(n), rightLarger(n);
+        SegmentTree leftTree(n), rightTree(n);
 
-    for (int i = 0; i < n; ++i) {
-      leftSmaller[i] = leftTree.query(0, arr[i] - 1);
-      leftTree.update(arr[i], 1);
+        for (int i = 0; i < n; ++i) {
+            leftSmaller[i] = leftTree.query(0, arr[i] - 1);
+            leftTree.update(arr[i], 1);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            rightLarger[i] = rightTree.query(arr[i] + 1, n - 1);
+            rightTree.update(arr[i], 1);
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < n; ++i)
+            ans += 1LL * leftSmaller[i] * rightLarger[i];
+
+        return ans;
     }
-
-    for (int i = n - 1; i >= 0; --i) {
-      rightLarger[i] = rightTree.query(arr[i] + 1, n - 1);
-      rightTree.update(arr[i], 1);
-    }
-
-    long long ans = 0;
-    for (int i = 0; i < n; ++i)
-      ans += 1LL*(leftSmaller[i]) * rightLarger[i];
-
-    return ans;
-  }
 };
