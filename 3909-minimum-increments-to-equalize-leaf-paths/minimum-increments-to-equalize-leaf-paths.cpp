1290509822643351455c1b35e90cdb751ce32fdc
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-22 13:53:17 +0530

    Time: 316 ms (43.99%) | Memory: 337.7 MB (23.86%) - LeetSync

---
+++ 3909-minimum-increments-to-equalize-leaf-paths/minimum-increments-to-equalize-leaf-paths.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    unordered_map<int, list<int>> adjList;
+    long long ans = 0;
+
+    // DFS to return max path sum from node to leaf
+    long long dfs(vector<int>& cost, int node) {
+        if (adjList[node].empty()) return cost[node];  // leaf node
+
+        vector<long long> child_Path_Sums;
+
+        for (int child : adjList[node]) {
+            child_Path_Sums.push_back(dfs(cost, child));
+        }
+
+        long long maxPath = *max_element(child_Path_Sums.begin(), child_Path_Sums.end());
+
+        // Count how many child paths are less than the max path
+        for (auto& val : child_Path_Sums) {
+            if (val != maxPath) ans++;
+        }
+
+        return cost[node] + maxPath; // max path sum
+    }
+
+    int minIncrease(int n, vector<vector<int>>& edges, vector<int>& cost) {
+        // Build the tree with directed edges: parent -> child
+        vector<bool> has_Parent(n, false);
+        for (auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            adjList[u].push_back(v);
+            has_Parent[v] = true;
+        }
+
+        // Find the root (node with no parent)
+        int root = 0;
+        while (has_Parent[root]) root++; // agar root 0 na ho to ye find kar dega root kya hai 
+
+        dfs(cost, root);
+        return ans;
+    }
+};
