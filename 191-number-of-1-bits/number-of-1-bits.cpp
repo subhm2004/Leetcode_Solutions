e40f0e0ce84153b43712a6b51e5c4d9085306324
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-24 18:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (80.64%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,7 +1,20 @@
 class Solution {
  public:
+ int count_Set_Bits_Fast(int n)
+{
+  int count = 0;
+  while (n != 0)
+  {
+    // remove last set bit
+    n = (n & (n - 1)); // last set bit hatao
+    count++;
+  }
+  return count;
+}
   int hammingWeight(int n) {
-    return __builtin_popcount(n);
+    // return __builtin_popcount(n);
+    return count_Set_Bits_Fast(n);
   }
 };
+
 
