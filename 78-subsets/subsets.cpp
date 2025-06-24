c2061c490f808a4590c0f4e29bd9f6ed0f28d94a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-24 22:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (43.55%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,46 @@
-class Solution {
+// class Solution {       // Solution using inclusion ans exclusion approach
+// public:
+//     void generate(int idx, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
+//         // Base case: if we've considered all elements
+//         if (idx == nums.size()) {
+//             ans.push_back(current);  // store the current subset
+//             return;
+//         }
+
+//         // Exclude the current element
+//         generate(idx + 1, nums, current, ans);
+
+//         // Include the current element
+//         current.push_back(nums[idx]);
+//         generate(idx + 1, nums, current, ans);
+//         current.pop_back();  // backtrack
+//     }
+
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> current;
+//         generate(0, nums, current, ans);
+//         return ans;
+//     }
+// };
+//-------------------------------------------------------------------------
+class Solution {     // Solution using backtracking 
 public:
-    void generate(int idx, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
-        // Base case: if we've considered all elements
-        if (idx == nums.size()) {
-            result.push_back(current);  // store the current subset
-            return;
+    void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
+        ans.push_back(current);  // add current subset
+
+        for (int i = start; i < nums.size(); ++i) {
+            current.push_back(nums[i]);         // choose
+            backtrack(i + 1, nums, current, ans); // explore
+            current.pop_back();                 // un-choose (backtrack)
         }
-
-        // Exclude the current element
-        generate(idx + 1, nums, current, result);
-
-        // Include the current element
-        current.push_back(nums[idx]);
-        generate(idx + 1, nums, current, result);
-        current.pop_back();  // backtrack
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
+        vector<vector<int>> ans;
         vector<int> current;
-        generate(0, nums, current, result);
-        return result;
+        backtrack(0, nums, current, ans);
+        return ans;
     }
 };
+
