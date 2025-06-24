644c823b09ac13061b93905403ca659ec578adbb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-24 19:37:12 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (4.80%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.cpp
@@ -1,14 +1,43 @@
+// class Solution {
+// public:
+//     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+//         int n = nums.size();
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = 0; j < n; ++j) {
+//                 if (abs(i - j) >= indexDifference && abs(nums[i] - nums[j]) >= valueDifference) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {-1, -1};
+//     }
+// };
+
+//------------------------------------------------------------------------------------------
 class Solution {
 public:
     vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (abs(i - j) >= indexDifference && abs(nums[i] - nums[j]) >= valueDifference) {
-                    return {i, j};
-                }
+        set<pair<int, int>> elements; // {value, index}
+
+        for (int i = indexDifference; i < n; ++i) {
+            // Insert the element at i - indexDifference into the set
+            elements.insert({nums[i - indexDifference], i - indexDifference});
+
+            // Check if there's any value >= nums[i] + valueDifference
+            auto it1 = elements.lower_bound({nums[i] + valueDifference, -1});
+            if (it1 != elements.end()) {
+                return {i, it1->second};
+            }
+
+            // Check if there's any value <= nums[i] - valueDifference
+            auto it2 = elements.lower_bound({nums[i] - valueDifference + 1, -1});
+            if (it2 != elements.begin()) {
+                --it2;
+                return {i, it2->second};
             }
         }
+
         return {-1, -1};
     }
 };
