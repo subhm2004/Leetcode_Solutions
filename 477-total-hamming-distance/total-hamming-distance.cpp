dab6871ecb4e33889ac576dddee5c8bc3f54db18
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-24 14:46:42 +0530

    Time: 15 ms (46.16%) | Memory: 23 MB (14.95%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,14 +1,33 @@
+// class Solution {
+// public:
+//     int hamming_Distance(int x, int y) { return __builtin_popcount(x ^ y); }
+
+//     int totalHammingDistance(vector<int>& nums) {
+//         int n = nums.size();
+//         int sum = 0;
+//         for (int i = 0; i < n; i++)
+//             for (int j = i + 1; j < n; j++)
+//                 sum += hamming_Distance(nums[i], nums[j]);
+
+//         return sum;
+//     }
+// };
+//-------------------------------------------------------------------------------
 class Solution {
 public:
-    int hamming_Distance(int x, int y) { return __builtin_popcount(x ^ y); }
-
     int totalHammingDistance(vector<int>& nums) {
+        int total = 0;
         int n = nums.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = i + 1; j < n; j++)
-                sum += hamming_Distance(nums[i], nums[j]);
 
-        return sum;
+        for (int bit = 0; bit < 32; ++bit) {
+            int count_ones = 0;
+            for (int num : nums) {
+                if (num & (1 << bit)) count_ones++;
+            }
+            int count_zeros = n - count_ones;
+            total += count_ones * count_zeros;
+        }
+
+        return total;
     }
 };
