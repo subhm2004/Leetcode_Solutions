4003c8b5fdc45b4bbe50b06785d2025e813b5514
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-25 00:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (94.88%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -23,24 +23,44 @@
 //         return ans;
 //     }
 // };
-//-------------------------------------------------------------------------
-class Solution {     // Solution using backtracking 
+// //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// class Solution {     // Solution using backtracking 
+// public:
+//     void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
+//         ans.push_back(current);  // add current subset
+
+//         for (int i = start; i < nums.size(); ++i) {
+//             current.push_back(nums[i]);         // choose
+//             backtrack(i + 1, nums, current, ans); // explore
+//             current.pop_back();                 // un-choose (backtrack)
+//         }
+//     }
+
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> current;
+//         backtrack(0, nums, current, ans);
+//         return ans;
+//     }
+// };
+//-------------------------------------------------------------------------------------------------------------------
+class Solution {
 public:
-    void backtrack(int start, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans) {
-        ans.push_back(current);  // add current subset
-
-        for (int i = start; i < nums.size(); ++i) {
-            current.push_back(nums[i]);         // choose
-            backtrack(i + 1, nums, current, ans); // explore
-            current.pop_back();                 // un-choose (backtrack)
-        }
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        int total_Subsets = 1 << n;  // 2^n subsets
         vector<vector<int>> ans;
-        vector<int> current;
-        backtrack(0, nums, current, ans);
+
+        for (int mask = 0; mask < total_Subsets; ++mask) {
+            vector<int> subset;
+            for (int i = 0; i < n; ++i) {
+                if (mask & (1 << i)) {  // check if i-th bit is set
+                    subset.push_back(nums[i]);
+                }
+            }
+            ans.push_back(subset);
+        }
+
         return ans;
     }
 };
-
