058974d50149814df26013aaa6bfb507a18e9b0e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-29 17:38:43 +0530

    Time: 26 ms (96.59%) | Memory: 53.3 MB (93.44%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,23 +1,24 @@
-class Solution {
+class Solution {     // partition dp + backtracking 
 public:
-    bool isPalindrome(const string& s, int l, int r) {
+     bool isPalindrome(const string& s, int l, int r) {
         while (l < r)
             if (s[l++] != s[r--])
                 return false;
         return true;
     }
-    void backtrack(const string& s, int start, vector<string>& curr,
+
+     void backtrack(const string& s, int i, vector<string>& curr,
                    vector<vector<string>>& ans) {
-        if (start == s.length()) {
-            ans.push_back(curr);
+        if (i == s.length()) {
+            ans.push_back(curr);   
             return;
         }
 
-        for (int i = start; i < s.length(); ++i)
-            if (isPalindrome(s, start, i)) {
-                curr.push_back(s.substr(start, i - start + 1));
-                backtrack(s, i + 1, curr, ans);
-                curr.pop_back();
+        for (int j = i; j < s.length(); ++j)
+            if (isPalindrome(s, i, j)) {
+                curr.push_back(s.substr(i, j - i + 1));
+                backtrack(s, j + 1, curr, ans);
+                curr.pop_back();  
             }
     }
 
