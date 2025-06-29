364f0729e668e033ecba02334d42e3ba81cb8841
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-06-29 18:38:39 +0530

    Time: 1038 ms (7.07%) | Memory: 9.4 MB (69.70%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,52 +1,29 @@
 class Solution {
 public:
-    int maxlen = 1, start = 0;
-    
-    bool solveRE(string& s, int i, int j) {
-        if (i >= j) return true;
-        
-        if (s[i] == s[j] && solveRE(s, i + 1, j - 1)) {
-            int currlen = j - i + 1;
-            if (currlen > maxlen) {
-                maxlen = currlen;
-                start = i;
+    bool isPalindrome(const string& s, int l, int r) {
+            while (l < r) {
+                if (s[l++] != s[r--])
+                    return false;
             }
             return true;
         }
-        return false;
-    }
-    
-   bool solveME(string& s, int i, int j, vector<vector<int>>& dp) {
-        if (i >= j)
-            return true;
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        bool flag = 0;
-        if (s[i] == s[j]) {
-            flag = solveME(s, i + 1, j - 1, dp);
-        }
-
-        if (flag) {
-            int currlen = j - i + 1;
-            if (currlen > maxlen) {
-                maxlen = currlen;
-                start = i;
-            }
-        }
-        return dp[i][j] = flag;
-    }
-    
     string longestPalindrome(string s) {
         int n = s.length();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                solveME(s, i, j, dp);
+        int maxlen = 1, start = 0;
+
+        // Check all substrings s[i..j]
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (isPalindrome(s, i, j)) {
+                    int len = j - i + 1;
+                    if (len > maxlen) {
+                        maxlen = len;
+                        start = i;
+                    }
+                }
             }
         }
-        
+
         return s.substr(start, maxlen);
     }
 };
