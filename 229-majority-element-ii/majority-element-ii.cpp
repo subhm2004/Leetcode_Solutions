0e398a98281371aa9687157164ec41704e4f572f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-02 23:30:16 +0530

    Time: 0 ms (100.00%) | Memory: 23.4 MB (92.85%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,80 +1,44 @@
-// class Solution {     // using unordered_map
-// public:
-//     vector<int> majorityElement(vector<int>& nums) {
-//         unordered_map<int, int> counter;
-//         for (int num : nums) {
-//             counter[num]++;
-//         }
-        
-//         vector<int> ans;
-//         int n = nums.size();
-//         for (auto& pair : counter) {
-//             if (pair.second > n / 3) {
-//                 ans.push_back(pair.first);
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-// class Solution {        // using sorting
-// public:
-//     vector<int> majorityElement(vector<int>& nums) {
-//         vector<int> ans;
-//         int n = nums.size();
-//         sort(nums.begin(), nums.end());
-
-//         for (int i = 0; i < n; ) {
-//             int j = i + 1;
-//             while (j < n && nums[j] == nums[i]) j++;
-            
-//             if (j - i > n / 3) {
-//                 ans.push_back(nums[i]);
-//             }
-
-//             i = j; // Move to the next distinct element
-//         }
-
-//         return ans;
-//     }
-// };
-
-class Solution {
+class Solution {    // Moore Voting Algorithm 
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int candidate1 = 0, candidate2 = 1; // initialize to different values
-        int count1 = 0, count2 = 0;
+        int candidate_1 = 0, candidate_2 = 1;
+        int count_1 = 0, count_2 = 0;
+        int n = nums.size();
 
         // First pass: find potential candidates
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
-            else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == candidate_1) {
+                count_1++;
+            } else if (nums[i] == candidate_2) {
+                count_2++;
+            } else if (count_1 == 0) {
+                candidate_1 = nums[i];
+                count_1 = 1;
+            } else if (count_2 == 0) {
+                candidate_2 = nums[i];
+                count_2 = 1;
             } else {
-                count1--;
-                count2--;
+                count_1--;
+                count_2--;
             }
         }
 
         // Second pass: verify candidates
-        count1 = count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
+        count_1 = 0;
+        count_2 = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == candidate_1) {
+                count_1++;
+            } else if (nums[i] == candidate_2) {
+                count_2++;
+            }
         }
 
         vector<int> ans;
-        int n = nums.size();
-        if (count1 > n / 3) ans.push_back(candidate1);
-        if (count2 > n / 3) ans.push_back(candidate2);
+        if (count_1 > n / 3) ans.push_back(candidate_1);
+        if (count_2 > n / 3) ans.push_back(candidate_2);
 
         return ans;
     }
 };
+
