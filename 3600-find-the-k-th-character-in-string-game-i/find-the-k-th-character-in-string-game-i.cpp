4d6e257a778198158d79d5029e96c566d3c78a77
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-03 19:59:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (96.34%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -1,16 +1,25 @@
+// class Solution {
+// public:
+//     char kthCharacter(int k) {
+//         string s = "a";
+//         while (s.size() < k) {
+//             string temp = "";
+//             for (int i = 0; i < s.size(); i++) {
+//                 // Get the next character cyclically (z -> a)
+//                 char next = (s[i] == 'z') ? 'a' : s[i] + 1;
+//                 temp += next;
+//             }
+//             s += temp;
+//         }
+//         return s[k - 1]; // 1-based indexing
+//     }
+// };
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class Solution {
 public:
     char kthCharacter(int k) {
-        string s = "a";
-        while (s.size() < k) {
-            string temp = "";
-            for (int i = 0; i < s.size(); i++) {
-                // Get the next character cyclically (z -> a)
-                char next = (s[i] == 'z') ? 'a' : s[i] + 1;
-                temp += next;
-            }
-            s += temp;
-        }
-        return s[k - 1];  // 1-based indexing
+        // k se p char ki value deni h lekin dekha jaye to sari values satarting me 1 (a) se hi nikli h lekin ab k pr h to shift ho gya k-1 ka lekin jo shift ho rha h wo two ki power me ho rha hai to jitni shift k andar set bits h use add krre hai a me 
+        int shifts = __builtin_popcount(k - 1); // counts no. of 1s in binary rep of k-1
+        return 'a' + shifts;
     }
 };
