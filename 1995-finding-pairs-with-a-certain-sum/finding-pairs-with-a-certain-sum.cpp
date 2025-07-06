88885f28d2ff82d9bd0d9a846c9d18d93bc778e8
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-06 15:23:33 +0530

    Time: 305 ms (26.51%) | Memory: 189.7 MB (26.51%) - LeetSync

---
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.cpp
@@ -0,0 +1,36 @@
+class FindSumPairs {
+public:
+     
+     vector<int> nums1;
+    vector<int> nums2;
+    unordered_map<int, int> mp;
+
+    FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
+        this->nums1 = nums1;
+        this->nums2 = nums2;
+        for (int x : nums2) {
+            ++mp[x];
+        }
+    }
+
+    void add(int index, int val) {
+        --mp[nums2[index]];
+        nums2[index] += val;
+        ++mp[nums2[index]];
+    }
+
+    int count(int tot) {
+        int ans = 0;
+        for (int x : nums1) {
+            ans += mp[tot - x];
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->mp(tot);
+ */
