923583166ab8eb293799d904fabcb9b46c3e1e1d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-10 21:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (17.12%) - LeetSync

---
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int maxXor = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            int x = nums[i];
+            for (int j = 0; j < n; ++j) {
+                int y = nums[j];
+                if (max(x, y) <= 2 * min(x, y)) {
+                    maxXor = max(maxXor, x ^ y);
+                }
+            }
+        }
+        return maxXor;
+    }
+};
+
+/*
+case 1 : x ≤ y
+|x - y| = y - x
+min(x, y) = x
+
+So: y - x ≤ x
+=> y ≤ 2x
+
+case 2 : y ≤ x
+|x - y| = x - y
+min(x, y) = y
+
+So: x - y ≤ y
+=> x ≤ 2y
+
+so dono ko comine krke solve hoke aata hai
+=> max(x, y) ≤ 2 * min(x, y)
+
+
+*/
