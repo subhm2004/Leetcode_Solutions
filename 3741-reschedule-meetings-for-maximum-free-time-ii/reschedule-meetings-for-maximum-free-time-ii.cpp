22109f67e651d825b4efbbd389d8e3d3423e29e5
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-12 18:35:49 +0530

    Time: 11 ms (50.43%) | Memory: 150.4 MB (31.55%) - LeetSync

--- 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,45 +1,46 @@
+//https://www.youtube.com/watch?v=nRQwEyJBW-I
 class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-        vector<int> freeArray; //store durations of free gaps
+        vector<int> free_arr; //store durations of free gaps
 
         //ith event
         //ith start - i-1th ka end = free gap duration
-        freeArray.push_back(startTime[0]);
+        free_arr.push_back(startTime[0]);
 
         for(int i = 1; i < startTime.size(); i++) {
-            freeArray.push_back(startTime[i] - endTime[i-1]);
+            free_arr.push_back(startTime[i] - endTime[i-1]);
         }
 
-        freeArray.push_back(eventTime - endTime[endTime.size()-1]);
+        free_arr.push_back(eventTime - endTime[endTime.size()-1]);
 
-        int n = freeArray.size();
-        vector<int> maxRightFree(n, 0);
-        vector<int> maxLeftFree(n, 0);
+        int n = free_arr.size();
+        vector<int> max_right_free(n, 0);
+        vector<int> max_left_free(n, 0);
         for(int i = n-2; i >= 0; i--) {
-            maxRightFree[i] = max(maxRightFree[i+1], freeArray[i+1]);
+            max_right_free[i] = max(max_right_free[i+1], free_arr[i+1]);
         }
 
         for(int i = 1; i < n; i++) {
-            maxLeftFree[i] = max(maxLeftFree[i-1], freeArray[i-1]);
+            max_left_free[i] = max(max_left_free[i-1], free_arr[i-1]);
         }
 
 
-        int result = 0;
-        //Iterating on the freeArray
+        int ans = 0;
+        //Iterating on the free_arr
         for(int i = 1; i < n; i++) {
-            int currEventTime = endTime[i-1] - startTime[i-1]; //duration of event = d
+            int curr_event_time = endTime[i-1] - startTime[i-1]; //duration of event = d
 
             //Case-1 Moving completely out
-            if(currEventTime <= max(maxLeftFree[i-1], maxRightFree[i])) {
-                result = max(result, freeArray[i-1] + currEventTime + freeArray[i]);
+            if(curr_event_time <= max(max_left_free[i-1], max_right_free[i])) {
+                ans = max(ans, free_arr[i-1] + curr_event_time + free_arr[i]);
             }
 
             //case-2 shift left or right
-            result = max(result, freeArray[i-1] + freeArray[i]);
+            ans = max(ans, free_arr[i-1] + free_arr[i]);
         }
 
-        return result;
+        return ans;
 
 
     }
