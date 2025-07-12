c874930abf340c2aab12798769cd8cdd5002a04b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-12 14:41:49 +0530

    Time: 2 ms (68.02%) | Memory: 125.8 MB (24.13%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
-        vector<int> freeArray; //store durations of free gaps
+        vector<int> free_arr; //store durations of free gaps
 
         //ith event
         //ith start - i-1th ka end = free gap duration
-        freeArray.push_back(startTime[0]);
+        free_arr.push_back(startTime[0]);
 
         for(int i = 1; i < startTime.size(); i++) {
-            freeArray.push_back(startTime[i] - endTime[i-1]);
+            free_arr.push_back(startTime[i] - endTime[i-1]);
         }
 
-        freeArray.push_back(eventTime - endTime[endTime.size()-1]);
+        free_arr.push_back(eventTime - endTime[endTime.size()-1]);
 
         //Khandani sliding window
 
+        int max_sum = INT_MIN;
+        int curr_sum = 0;
         int i = 0;
-        int j = 0;
-        int maxSum = 0;
-        int currSum = 0;
+        int n = free_arr.size();
 
-        int n = freeArray.size();
-        while(j < n) {
-            currSum += freeArray[j];
+        for (int j = 0; j < n; j++) {
+            curr_sum += free_arr[j];
 
-            if(i < n && j-i+1 > k+1) {
-                currSum -= freeArray[i];
+            // Window size control: agar window ka size k+1 se zyada ho gaya to shrink karo
+            if (j - i + 1 > k + 1) {
+                curr_sum -= free_arr[i];
                 i++;
             }
 
-            maxSum = max(maxSum, currSum);
-            j++;
+            // Har step pe max_sum update karo
+            max_sum = max(max_sum, curr_sum);
         }
 
-        return maxSum;
+        return max_sum;
+
     }
 };
