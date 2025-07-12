01498e91a2b639e7ebd5c0157624f1a0ed6449c4
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-12 14:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 91 MB (78.55%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -15,16 +15,30 @@
 //         return ans;
 //     }
 // };
+
+
+
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        int n = nums.size();
-        long long ans = 0, imax = 0, dmax = 0;
-        for (int k = 0; k < n; k++) {
-            ans = max(ans, dmax * nums[k]);
-            dmax = max(dmax, imax - nums[k]);
-            imax = max(imax, 1LL * nums[k]);
+        long long maxi = INT_MIN;     // Final answer: sabse bada (nums[i] - nums[j]) * nums[k]
+        long long max_so_far = 0;     // Ab tak ka sabse bada nums[i], jiske liye i < j
+        long long max_diff = 0;      // Ab tak ka sabse bada nums[i] - nums[j]
+
+        for (int k = 0; k < nums.size(); k++) {
+            // Step 1: Check karo ki agar current nums[k] ko 3rd number banayein (i < j < k)
+            // toh ab tak ka best (nums[i] - nums[j]) * nums[k] kya banega
+            maxi = max(maxi, max_diff * nums[k]);
+
+            // Step 2: Update karo max_diff
+            // Yaani ab tak ka sabse bada nums[i] - nums[j], jahan j < k
+            max_diff = max(max_diff, max_so_far - nums[k]);
+
+            // Step 3: Update karo max_so_far (nums[i])
+            // Taaki future me use kar sake jab j < k hoga
+            max_so_far = max(max_so_far, 1LL * nums[k]);
         }
-        return ans;
+
+        return maxi;
     }
 };
