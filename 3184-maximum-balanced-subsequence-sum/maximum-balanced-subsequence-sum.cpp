8786b9e5e4c1b50eaa406d8e3a12c2f76d03b873
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-12 21:59:32 +0530

    Time: 278 ms (69.64%) | Memory: 102.9 MB (63.37%) - LeetSync

--- 3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.cpp
+++ 3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.cpp
@@ -1,50 +1,63 @@
-typedef long long ll;
-class Solution {  
+class Solution {     // Binary search + dp se hoga ye + hash map use hora hai
 public:
-    ll solveRE(vector<int>& nums, int curr_sumr, int prev) {
-        if (curr_sumr >= nums.size()) {
+using ll = long long;
+    long long solveRE(vector<int>& nums, int curr, int prev) {
+        if (curr >= nums.size()) {
             return INT_MIN;
         }
 
-        ll include = INT_MIN;
-        if (prev == -1 || (nums[curr_sumr] - nums[prev] >= curr_sumr - prev)) {
-            include = nums[curr_sumr] + max(0LL, solveRE(nums, curr_sumr + 1, curr_sumr));
+        long long include = INT_MIN;
+        if (prev == -1 || (nums[curr] - nums[prev] >= curr - prev)) {
+            include = nums[curr] + max(0LL, solveRE(nums, curr + 1, curr));
         }
 
-        ll exclude = solveRE(nums, curr_sumr + 1, prev);
+        long long exclude = solveRE(nums, curr + 1, prev);
 
-        return max<ll>(include, exclude);
+        return max(include, exclude);
     }
 
-    ll solveME(vector<int>& nums, int curr_sumr, int prev,
-                      vector<vector<ll>>& dp) {
-        if (curr_sumr >= nums.size()) {
+    long long solveME(vector<int>& nums, int curr, int prev, vector<vector<long long>>& dp) {
+        if (curr >= nums.size()) {
             return INT_MIN;
         }
 
-        if (dp[curr_sumr][prev + 1] != LLONG_MIN) {
-            return dp[curr_sumr][prev + 1];
+        if (dp[curr][prev + 1] != LLONG_MIN) {
+            return dp[curr][prev + 1];
         }
 
-        ll include = INT_MIN;
-        if (prev == -1 || (nums[curr_sumr] - nums[prev] >= curr_sumr - prev)) {
-            include = nums[curr_sumr] + max(0LL, solveME(nums, curr_sumr + 1, curr_sumr, dp));
+        long long include = INT_MIN;
+        if (prev == -1 || (nums[curr] - nums[prev] >= curr - prev)) {
+            include = nums[curr] + max(0LL, solveME(nums, curr + 1, curr, dp));
         }
 
-        ll exclude = solveME(nums, curr_sumr + 1, prev, dp);
+        long long exclude = solveME(nums, curr + 1, prev, dp);
 
-        return dp[curr_sumr][prev + 1] = max<ll>(include, exclude);
+        return dp[curr][prev + 1] = max(include, exclude);
     }
 
-    ll solve_Bottom_Up(vector<int>& nums) {
+    long long solve_Bottom_Up_1(vector<int>& nums) { // dp wale me values sath me daal rhe hai hum dp wale me phl ebhi daal skte the values
         int n = nums.size();
-        vector<ll> dp(n, 0);
+        vector<long long> dp(n, 0);
+        dp[0] = nums[0];
+        long long maxSum = nums[0];
 
-        for (int i = 0; i < n; ++i) {
+        for (int i = 1; i < n; ++i) {
             dp[i] = nums[i];
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] - i >= nums[j] - j) {
+                    dp[i] = max(dp[i], dp[j] + nums[i]);
+                }
+            }
+            maxSum = max(maxSum, dp[i]);
         }
 
-        ll maxSum = nums[0];
+        return maxSum;
+    }
+
+        long long solve_Bottom_Up_2(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> dp(nums.begin(), nums.end()); // dp[i] = nums[i] initially
+        long long maxSum = dp[0];
 
         for (int i = 1; i < n; ++i) {
             for (int j = 0; j < i; ++j) {
@@ -52,13 +65,13 @@ public:
                     dp[i] = max(dp[i], dp[j] + nums[i]);
                 }
             }
-            maxSum = max<ll>(maxSum, dp[i]);
+            maxSum = max(maxSum, dp[i]);
         }
 
         return maxSum;
     }
-
-     ll optmized_Solution(vector<int>& nums) {
+    
+    ll optmized_Solution(vector<int>& nums) {
         int n = nums.size();
         map<int, ll> bestSum;  // (nums[i]-i , Sum) 
         ll ans = INT_MIN;
@@ -86,15 +99,14 @@ public:
         return ans;
     }
 
-    ll maxBalancedSubsequenceSum(vector<int>& nums) {
+    long long maxBalancedSubsequenceSum(vector<int>& nums) {
         int n = nums.size();
-        // return solveRE(nums, 0, -1);
-        // vector<vector<ll>> dp(n, vector<ll>(n + 1, LLONG_MIN));
+        // vector<vector<long long>> dp(n, vector<long long>(n + 1, LLONG_MIN));
 
-        // ll ans = solveME(nums, 0, -1, dp);
-        // return (ans == INT_MIN) ? 0 : ans;
+        // long long result = solveME(nums, 0, -1, dp);
+        // return (result == INT_MIN) ? 0 : result;
         // return solve_Bottom_Up(nums);
+        // return solve_Bottom_Up_2(nums);
         return optmized_Solution(nums);
-
     }
 };
