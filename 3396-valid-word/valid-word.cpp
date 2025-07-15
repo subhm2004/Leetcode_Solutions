f8032a6e12739c9180ed3c8abe137ce2a829b9a4
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-15 15:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (33.01%) - LeetSync

---
+++ 3396-valid-word/valid-word.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool is_vowel(char ch) {
+        ch = tolower(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+
+    bool isValid(string word) {
+        if (word.length() < 3)
+            return false;
+
+        bool vowel = false;
+        bool consonant = false;
+
+        for (char ch : word) {
+            if (!isalpha(ch) && !isdigit(ch))
+                return false;
+
+            else {
+                if (isalpha(ch)) {
+                    if (is_vowel(ch)) {
+                        vowel = true;
+                    } else {
+                        consonant = true;
+                    }
+                }
+            }
+        }
+
+        return vowel && consonant;
+    }
+};
