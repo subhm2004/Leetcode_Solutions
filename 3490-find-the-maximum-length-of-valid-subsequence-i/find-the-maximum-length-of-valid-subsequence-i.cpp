6ea8485112cdd3a2ea03da11656ba2d2037faa5b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-17 03:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 96.1 MB (83.19%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -34,38 +34,15 @@ public:
     }
 
 
-     int solve(vector<int>& nums) {
-        // odd number or even number ka count find krte hai 
-        int count_even = 0, count_odd = 0;
-        for (int num : nums) {
-            if (num % 2 == 0) count_even++;
-            else count_odd++;
-        }
-
-        // alternating parity subsequence banate hai 
-        int alternating_length = 1; // At ek number to hoga hi starting me
-        int prev_parity = nums[0] % 2;
-
-        for (int i = 1; i < nums.size(); ++i) {
-            int currParity = nums[i] % 2;
-            if (currParity != prev_parity) {
-                alternating_length++;
-                prev_parity = currParity;
-            }
-        }
-
-        return max({count_even, count_odd, alternating_length});
-    }
-
     // ISME HUMNE PREVIOUS ELEMENT KO NA LEKE SEEDHA PREVIOUS PARITY KA CONCEPT LE LIYA
     int solveRE_2(int i, int prev_parity, int target_parity, vector<int>& nums) {
         if (i == nums.size()) return 0;
 
         int include = 0;
-        int currParity = nums[i] % 2;
+        int curr_parity = nums[i] % 2;
 
-        if (prev_parity == -1 || (prev_parity + currParity) % 2 == target_parity) {
-            include = 1 + solveRE_2(i + 1, currParity, target_parity, nums);
+        if (prev_parity == -1 || (prev_parity + curr_parity) % 2 == target_parity) {
+            include = 1 + solveRE_2(i + 1, curr_parity, target_parity, nums);
         }
 
         int exclude = solveRE_2(i + 1, prev_parity, target_parity, nums);
@@ -79,15 +56,38 @@ public:
         if (dp[i][prev_parity + 1] != -1) return dp[i][prev_parity + 1];
 
         int include = 0;
-        int currParity = nums[i] % 2;
+        int curr_parity = nums[i] % 2;
 
-        if (prev_parity == -1 || (prev_parity + currParity) % 2 == target_parity) {
-            include = 1 + solveME_2(i + 1, currParity, target_parity, nums, dp);
+        if (prev_parity == -1 || (prev_parity + curr_parity) % 2 == target_parity) {
+            include = 1 + solveME_2(i + 1, curr_parity, target_parity, nums, dp);
         }
 
         int exclude = solveME_2(i + 1, prev_parity, target_parity, nums, dp);
 
         return dp[i][prev_parity + 1] = max(include, exclude);
+    }
+
+    int solve_optmized(vector<int>& nums) {
+        // odd number or even number ka count find krte hai 
+        int count_even = 0, count_odd = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) count_even++;
+            else count_odd++;
+        }
+
+        // alternating parity subsequence banate hai 
+        int alternating_length = 1; // At ek number to hoga hi starting me
+        int prev_parity = nums[0] % 2;
+
+        for (int i = 1; i < nums.size(); ++i) {
+            int curr_parity = nums[i] % 2;
+            if (curr_parity != prev_parity) {
+                alternating_length++;
+                prev_parity = curr_parity;
+            }
+        }
+
+        return max({count_even, count_odd, alternating_length});
     }
 
     int maximumLength(vector<int>& nums) {
@@ -99,19 +99,23 @@ public:
         // int odd = solveME_1(0, -1, 1, nums, dp);
         // return max(even, odd);
 
-        // return solve(nums);
 
         // int even = solveRE_2(0, -1, 0, nums); // target: even pair sums
         // int odd = solveRE_2(0, -1, 1, nums);  // target: odd pair sums
         // return max(even, odd);
 
-        vector<vector<int>> dp_even(n, vector<int>(3, -1));
-        vector<vector<int>> dp_odd(n, vector<int>(3, -1));
 
-        int even_ans = solveME_2(0, -1, 0, nums, dp_even);  
-        int odd_ans = solveME_2(0, -1, 1, nums, dp_odd);   
 
-        return max(even_ans, odd_ans);
+        // vector<vector<int>> dp_even(n, vector<int>(3, -1));
+        // vector<vector<int>> dp_odd(n, vector<int>(3, -1));
+
+        // int even_ans = solveME_2(0, -1, 0, nums, dp_even);  
+        // int odd_ans = solveME_2(0, -1, 1, nums, dp_odd);   
+
+        // return max(even_ans, odd_ans);
     
+
+        return solve_optmized(nums);
+
     }
 };
