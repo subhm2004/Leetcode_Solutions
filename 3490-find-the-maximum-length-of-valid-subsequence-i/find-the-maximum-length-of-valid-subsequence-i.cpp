371e08ebb3d9d764a557c58794cebe32cda845d7
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-17 03:03:00 +0530

    Time: 360 ms (5.13%) | Memory: 214.1 MB (5.13%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solveRE(int i, int prev, int parity, vector<int>& nums) {
+    int solveRE(int i, int prev, int parity, vector<int>& nums) { // ye solution ache hai lekin efficient hai honge 
         if (i == nums.size())
             return 0;
 
@@ -57,16 +57,61 @@ public:
         return max({count_even, count_odd, alternating_length});
     }
 
+    // ISME HUMNE PREVIOUS ELEMENT KO NA LEKE SEEDHA PREVIOUS PARITY KA CONCEPT LE LIYA
+    int solveRE_2(int i, int prev_parity, int target_parity, vector<int>& nums) {
+        if (i == nums.size()) return 0;
+
+        int include = 0;
+        int currParity = nums[i] % 2;
+
+        if (prev_parity == -1 || (prev_parity + currParity) % 2 == target_parity) {
+            include = 1 + solveRE_2(i + 1, currParity, target_parity, nums);
+        }
+
+        int exclude = solveRE_2(i + 1, prev_parity, target_parity, nums);
+
+        return max(include, exclude);
+    }
+
+    int solveME_2(int i, int prev_parity, int target_parity, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == nums.size()) return 0;
+
+        if (dp[i][prev_parity + 1] != -1) return dp[i][prev_parity + 1];
+
+        int include = 0;
+        int currParity = nums[i] % 2;
+
+        if (prev_parity == -1 || (prev_parity + currParity) % 2 == target_parity) {
+            include = 1 + solveME_2(i + 1, currParity, target_parity, nums, dp);
+        }
+
+        int exclude = solveME_2(i + 1, prev_parity, target_parity, nums, dp);
+
+        return dp[i][prev_parity + 1] = max(include, exclude);
+    }
+
     int maximumLength(vector<int>& nums) {
         // mod 2 krne se do values (parity) aa skti hai 0 and 1
-        // int n = nums.size();
+        int n = nums.size();
         // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(n + 1, vector<int>(2, -1)));
 
         // int even = solveME_1(0, -1, 0, nums, dp);
         // int odd = solveME_1(0, -1, 1, nums, dp);
         // return max(even, odd);
 
-        return solve(nums);
+        // return solve(nums);
+
+        // int even = solveRE_2(0, -1, 0, nums); // target: even pair sums
+        // int odd = solveRE_2(0, -1, 1, nums);  // target: odd pair sums
+        // return max(even, odd);
+
+        vector<vector<int>> dp_even(n, vector<int>(3, -1));
+        vector<vector<int>> dp_odd(n, vector<int>(3, -1));
+
+        int even_ans = solveME_2(0, -1, 0, nums, dp_even);  
+        int odd_ans = solveME_2(0, -1, 1, nums, dp_odd);   
+
+        return max(even_ans, odd_ans);
     
     }
 };
