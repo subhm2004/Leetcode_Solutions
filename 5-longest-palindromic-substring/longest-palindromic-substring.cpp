34e79752bfacc5a340cc08a6fc47c87d66d0574a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-18 20:58:15 +0530

    Time: 1007 ms (7.78%) | Memory: 9.3 MB (92.93%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -9,21 +9,21 @@ public:
         }
     string longestPalindrome(string s) {
         int n = s.length();
-        int maxlen = 1, start = 0;
+        int max_len = 1, start = 0;
 
         // Check all substrings s[i..j]
         for (int i = 0; i < n; ++i) {
             for (int j = i; j < n; ++j) {
                 if (isPalindrome(s, i, j)) {
                     int len = j - i + 1;
-                    if (len > maxlen) {
-                        maxlen = len;
+                    if (len > max_len) {
+                        max_len = len;
                         start = i;
                     }
                 }
             }
         }
 
-        return s.substr(start, maxlen);
+        return s.substr(start, max_len);
     }
 };
