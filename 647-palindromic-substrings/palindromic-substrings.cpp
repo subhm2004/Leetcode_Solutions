e23b3bc213b45dfbe4ac9710f785748fd1c87994
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-20 02:56:54 +0530

    Time: 31 ms (30.48%) | Memory: 10.9 MB (56.53%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,27 +1,65 @@
+// class Solution {    // badhiya method hai 
+// public:
+//     int count_palindrom(string s, int left, int right) {
+//         int count = 0;
+//         while (left >= 0 && right < s.size() && s[left] == s[right]) {
+//             count++;
+//             left--;
+//             right++;
+//         }
+//         return count;
+//     }
+
+//     int countSubstrings(string s) {
+//         int count = 0;
+//         int n = s.size();
+//         for (int i = 0; i < n; i++) {
+//             //  odd-length palindromes
+//             count += count_palindrom(s, i, i);
+
+//             //  even-length palindromes
+//             count += count_palindrom(s, i, i + 1);
+//         }
+
+//         return count;
+//     }
+// };
+
+
+
+//-----------------------------------------------------------------------------------------------------
+
+
 class Solution {
 public:
-
-      int count_palindrom(string s, int left, int right) {
+    int solve_bottom_up(string &s) {
+        int n = s.length();
         int count = 0;
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            count++;
-            left--;
-            right++;
+
+        // dp[i][j] = true agar s[i..j] ek palindrome hai
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        // L = length of substring
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L - 1 < n; i++) {
+                int j = i + L - 1;
+
+                if (L == 1) {
+                    dp[i][j] = true;
+                } else if (L == 2) {
+                    dp[i][j] = (s[i] == s[j]);
+                } else {
+                    dp[i][j] = (s[i] == s[j]) && dp[i + 1][j - 1];
+                }
+
+                if (dp[i][j]) count++;
+            }
         }
+
         return count;
     }
 
     int countSubstrings(string s) {
-        int count = 0;
-        int n = s.size();
-        for (int i = 0; i < n; i++) {
-            //  odd-length palindromes
-            count += count_palindrom(s, i, i);
-
-            //  even-length palindromes
-            count += count_palindrom(s, i, i + 1);
-        }
-
-        return count;
+        return solve_bottom_up(s);
     }
 };
