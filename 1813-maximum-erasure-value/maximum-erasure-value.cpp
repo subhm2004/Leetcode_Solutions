7b064be646ed05e71f147a5a74b95f7af5640637
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-22 23:22:52 +0530

    Time: 205 ms (21.80%) | Memory: 131 MB (31.90%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/maximum-erasure-value.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        unordered_set<int> seen;
-        int start = 0, curr_sum = 0, maxi = 0;
+        unordered_set<int> seen;  // unique elements ka track rakhne ke liye
+        int start = 0 ;
+        int  curr_sum = 0;
+        int  maxi = 0;
 
         for (int end = 0; end < nums.size(); ++end) {
+            // agar duplicate mila, to window ko left se chhota karo
             while (seen.count(nums[end])) {
-                seen.erase(nums[start]);
-                curr_sum -= nums[start];
-                start++;
+                seen.erase(nums[start]);        // start wala element remove karo
+                curr_sum -= nums[start];         // uska sum bhi kam karo
+                start++;                        // aur window aage badhao
             }
 
-            seen.insert(nums[end]);
-            curr_sum += nums[end];
-            maxi = max(maxi, curr_sum);
+            seen.insert(nums[end]);             // current element ko set me daalo
+            curr_sum += nums[end];               // aur sum me add karo
+            maxi = max(maxi, curr_sum);  // max score update karte hai 
         }
 
         return maxi;
