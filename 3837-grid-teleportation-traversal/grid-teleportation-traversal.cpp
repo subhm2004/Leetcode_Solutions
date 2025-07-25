8535d859fd087f617530e720bda1baff49f12e6d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-25 17:49:25 +0530

    Time: 1041 ms (38.94%) | Memory: 151.3 MB (77.86%) - LeetSync

--- 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
+++ 3837-grid-teleportation-traversal/grid-teleportation-traversal.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dijkstra(const vector<string>& matrix,
+    int dijkstra(vector<string>& matrix,
                  unordered_map<char, vector<pair<int,int>>>& portal_Positions) {
         int m = matrix.size();
         int n = matrix[0].size();
@@ -8,7 +8,7 @@ public:
         if (matrix[m - 1][n - 1] == '#') return -1;
 
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        unordered_set<char> used_Portals;
+        vector<bool> visited(26, false);   
 
         using T = tuple<int, int, int>; // (distance, row, col)
         priority_queue<T, vector<T>, greater<T>> pq;
@@ -19,19 +19,22 @@ public:
         vector<pair<int,int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
         while (!pq.empty()) {
-            auto [currDist, r, c] = pq.top();
+            auto [curr_dist, r, c] = pq.top();
             pq.pop();
 
-            if (r == m - 1 && c == n - 1) return currDist;
-            if (currDist > dist[r][c]) continue;
+            if (r == m - 1 && c == n - 1) return curr_dist;
+            if (curr_dist > dist[r][c]) continue;
 
             char ch = matrix[r][c];
-            if (ch >= 'A' && ch <= 'Z' && used_Portals.find(ch) == used_Portals.end()) {
-                used_Portals.insert(ch);
-                for (auto &[pr, pc] : portal_Positions[ch]) {
-                    if (currDist < dist[pr][pc]) {
-                        dist[pr][pc] = currDist;
-                        pq.emplace(currDist, pr, pc); // teleportation me cost nhi lagti acc to problem
+            if (ch >= 'A' && ch <= 'Z') {
+                int idx = ch - 'A';
+                if (!visited[idx]) {
+                    visited[idx] = true;
+                    for (auto &[pr, pc] : portal_Positions[ch]) {
+                        if (curr_dist < dist[pr][pc]) {
+                            dist[pr][pc] = curr_dist;
+                            pq.emplace(curr_dist, pr, pc); // teleportation is free
+                        }
                     }
                 }
             }
@@ -39,9 +42,9 @@ public:
             for (auto &[dr, dc] : directions) {
                 int nr = r + dr, nc = c + dc;
                 if (nr >= 0 && nr < m && nc >= 0 && nc < n && matrix[nr][nc] != '#') {
-                    if (currDist + 1 < dist[nr][nc]) {
-                        dist[nr][nc] = currDist + 1;
-                        pq.emplace(currDist + 1, nr, nc);
+                    if (curr_dist + 1 < dist[nr][nc]) {
+                        dist[nr][nc] = curr_dist + 1;
+                        pq.emplace(curr_dist + 1, nr, nc);
                     }
                 }
             }
