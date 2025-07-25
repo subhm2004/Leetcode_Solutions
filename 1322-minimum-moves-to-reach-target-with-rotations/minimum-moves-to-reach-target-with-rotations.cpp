39d06a5cfea9b6455c673b0291737d8cb2b5a842
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-07-25 17:46:36 +0530

    Time: 32 ms (80.73%) | Memory: 30 MB (49.54%) - LeetSync

---
+++ 1322-minimum-moves-to-reach-target-with-rotations/minimum-moves-to-reach-target-with-rotations.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int bfs(vector<vector<int>>& arr) {
+        int n = arr.size();
+        queue<tuple<int, int, int, int>> q; // (row, col, orientation, moves)
+        vector<vector<vector<bool>>> visited(n, vector<vector<bool>>(n, vector<bool>(2, false)));
+
+        q.push({0, 0, 0, 0}); // Start from top-left, horizontal, 0 moves
+        visited[0][0][0] = true;
+
+        while (!q.empty()) {
+            auto [r, c, orient, moves] = q.front();
+            q.pop();
+
+            // Reached goal
+            if (r == n - 1 && c == n - 2 && orient == 0) return moves;
+
+            // Move Right
+            if (orient == 0 && c + 2 < n &&
+                arr[r][c + 1] == 0 && arr[r][c + 2] == 0 &&
+                !visited[r][c + 1][0]) {
+                visited[r][c + 1][0] = true;
+                q.push({r, c + 1, 0, moves + 1});
+            }
+
+            // Move Down (Horizontal)
+            if (orient == 0 && r + 1 < n &&
+                arr[r + 1][c] == 0 && arr[r + 1][c + 1] == 0 &&
+                !visited[r + 1][c][0]) {
+                visited[r + 1][c][0] = true;
+                q.push({r + 1, c, 0, moves + 1});
+            }
+
+            // Rotate Clockwise
+            if (orient == 0 && r + 1 < n &&
+                arr[r + 1][c] == 0 && arr[r + 1][c + 1] == 0 &&
+                !visited[r][c][1]) {
+                visited[r][c][1] = true;
+                q.push({r, c, 1, moves + 1});
+            }
+
+            // Move Right (Vertical)
+            if (orient == 1 && c + 1 < n &&
+                arr[r][c + 1] == 0 && arr[r + 1][c + 1] == 0 &&
+                !visited[r][c + 1][1]) {
+                visited[r][c + 1][1] = true;
+                q.push({r, c + 1, 1, moves + 1});
+            }
+
+            // Move Down (Vertical)
+            if (orient == 1 && r + 2 < n &&
+                arr[r + 2][c] == 0 &&
+                !visited[r + 1][c][1]) {
+                visited[r + 1][c][1] = true;
+                q.push({r + 1, c, 1, moves + 1});
+            }
+
+            // Rotate Counter-clockwise
+            if (orient == 1 && c + 1 < n &&
+                arr[r][c + 1] == 0 && arr[r + 1][c + 1] == 0 &&
+                !visited[r][c][0]) {
+                visited[r][c][0] = true;
+                q.push({r, c, 0, moves + 1});
+            }
+        }
+
+        return -1; // Not reachable
+    }
+
+    int minimumMoves(vector<vector<int>>& arr) {
+        return bfs(arr);
+    }
+};
