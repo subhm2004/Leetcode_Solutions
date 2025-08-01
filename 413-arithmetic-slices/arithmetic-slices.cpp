a74a23e3bbd36db7de541941a642bd424e4b0308
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-01 16:58:23 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (10.78%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.cpp
+++ 413-arithmetic-slices/arithmetic-slices.cpp
@@ -1,19 +1,40 @@
+// class Solution {   // Brute force solution 
+// public:
+//     int numberOfArithmeticSlices(vector<int>& nums) {
+//         int n = nums.size(), ans = 0;
+
+//         for (int i = 0; i < n - 2; ++i) {
+//             int diff = nums[i + 1] - nums[i];
+
+//             for (int j = i + 2; j < n; ++j) {
+//                 if (nums[j] - nums[j - 1] == diff)
+//                     ans++;
+//                 else
+//                     break;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n = nums.size(), ans = 0;
+        int n = nums.size();
+        if (n < 3) return 0;
 
-        for (int i = 0; i < n - 2; ++i) {
-            int diff = nums[i + 1] - nums[i];
+        int count = 0, total = 0;
 
-            for (int j = i + 2; j < n; ++j) {
-                if (nums[j] - nums[j - 1] == diff)
-                    ans++;
-                else
-                    break;
+        for (int i = 2; i < n; ++i) {
+            if (nums[i] - nums[i-1] == nums[i-1] - nums[i-2]) {
+                count++;          
+                total += count;    
+            } else {
+                count = 0;       
             }
         }
 
-        return ans;
+        return total;
     }
 };
