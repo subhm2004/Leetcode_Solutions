5573bc39c687ffff604d974e3062f684d840957d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-06 12:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (70.53%) - LeetSync

--- 1115-valid-boomerang/valid-boomerang.cpp
+++ 1115-valid-boomerang/valid-boomerang.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     bool isBoomerang(vector<vector<int>>& points) {
-        auto& A = points[0];
-        auto& B = points[1];
-        auto& C = points[2];
+        auto& A = points[0]; // x1, y1
+        auto& B = points[1]; // x2, y2
+        auto& C = points[2]; // x3, y3
 
-         if (A == B || B == C || A == C) return false;
+         if (A == B || B == C || A == C)
+            return false;
 
-         int area = (B[0] - A[0]) * (C[1] - A[1]) - 
-                   (B[1] - A[1]) * (C[0] - A[0]);
+         // Area = 0.5 * | x1(y2 - y3) + x2(y3 - y1) + x3(y1 - y2) |
+        int x1 = A[0], y1 = A[1];
+        int x2 = B[0], y2 = B[1];
+        int x3 = C[0], y3 = C[1];
 
-        return area != 0;
+        int twice_area = x1 * (y2 - y3) + 
+                         x2 * (y3 - y1) + 
+                         x3 * (y1 - y2);
+
+        return twice_area != 0; // area ≠ 0 ⇒ not collinear ⇒ boomerang
     }
 };
