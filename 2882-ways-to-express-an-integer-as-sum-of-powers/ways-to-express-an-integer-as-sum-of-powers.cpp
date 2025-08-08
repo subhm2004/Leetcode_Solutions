a0594244f5e4f452e1fc4d2a2cb73d61991058fc
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-09 02:45:54 +0530

    Time: 432 ms (20.18%) | Memory: 274.5 MB (29.58%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -4,45 +4,44 @@ public:
 
     int solveRE(int i, int sum, int target, int x) {
         if (sum == target)
-            return 1; // Found a valid way
-        if (i <= 0 || sum > target)
-            return 0; // No valid way
+            return 1; // Valid way
+        if (i > target || sum > target)
+            return 0; // Out of range
 
-        long long power = pow(i, x);
+        long long power = (long long)pow(i, x);  
 
-        if (sum + power > target)
-            return solveRE(i - 1, sum, target, x);
+        if (power > target) return 0;  
 
-        int include = solveRE(i - 1, sum + power, target, x) % MOD;
-        int exclude = solveRE(i - 1, sum, target, x) % MOD;
+        // Include current number
+        int include = solveRE(i + 1, sum + power, target, x) % MOD;
+        // Exclude current number
+        int exclude = solveRE(i + 1, sum, target, x) % MOD;
 
         return (include + exclude) % MOD;
     }
 
     int solveME(int i, int sum, int target, int x, vector<vector<int>>& dp) {
         if (sum == target)
-            return 1;  // Found a valid way
-        if (i <= 0 || sum > target)
-            return 0;  // No valid way
+            return 1; 
+        if (i > target || sum > target)
+            return 0; 
 
         if (dp[i][sum] != -1)
             return dp[i][sum];
 
-        long long power = pow(i,x);
+        long long power = (long long)pow(i, x);
 
-        if (sum + power > target)
-            return dp[i][sum] = solveME(i - 1, sum, target, x, dp);
+        if (power > target) return dp[i][sum] = 0;
 
-        int include = solveME(i - 1, sum + power, target, x, dp) % MOD;
-        int exclude = solveME(i - 1, sum, target, x, dp) % MOD;
+        int include = solveME(i + 1, sum + power, target, x, dp) % MOD;
+        int exclude = solveME(i + 1, sum, target, x, dp) % MOD;
 
         return dp[i][sum] = (include + exclude) % MOD;
     }
 
     int numberOfWays(int n, int x) {
-        // return solveRE(n, 0, n, x);
-
+        // return solveRE(1, 0, n, x);  
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        return solveME(n, 0, n, x, dp);
+        return solveME(1, 0, n, x, dp);
     }
 };
