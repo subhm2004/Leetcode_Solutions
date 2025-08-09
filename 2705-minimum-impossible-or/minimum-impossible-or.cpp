b2babcf832618e3bf26cc2fe769986856c0d28e2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-10 01:01:07 +0530

    Time: 90 ms (27.50%) | Memory: 78.4 MB (41.00%) - LeetSync

--- 2705-minimum-impossible-or/minimum-impossible-or.cpp
+++ 2705-minimum-impossible-or/minimum-impossible-or.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int minImpossibleOR(vector<int>& nums) {
-         unordered_map<int, int> mp;
+        // Create a map to store which numbers are present
+        unordered_map<int, int> mp;
         
-         for(int i = 0; i < nums.size(); i++) {
+        // Mark all numbers as present in the map
+        for(int i = 0; i < nums.size(); i++) {
             mp[nums[i]] = 1;
         }
         
-         int ans = 1;
+        // Start with the smallest power of 2
+        int ans = 1;
         
-          while(mp.find(ans) != mp.end()) {
-            ans *= 2;   
+        // Keep checking if current power of 2 exists in the array
+        // If it exists, move to the next power of 2
+        // If it doesn't exist, that's our answer
+        while(mp.find(ans) != mp.end()) {
+            ans *= 2;  // Move to next power of 2: 1, 2, 4, 8, 16, ...
         }
         
         return ans;
