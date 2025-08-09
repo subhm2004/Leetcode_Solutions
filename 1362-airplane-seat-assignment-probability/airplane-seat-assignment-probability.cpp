8f6885c37970f2dbf5ac68d60b2aaf6aacb48336
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-09 20:05:39 +0530

    Time: 43 ms (4.90%) | Memory: 46.1 MB (5.26%) - LeetSync

--- 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
+++ 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
@@ -1,4 +1,19 @@
 class Solution {
 public:
-    double nthPersonGetsNthSeat(int n) { return (n == 1) ? 1.0 : 0.5; }
+    double solveME(int n, vector<double>& dp) {
+        if (n == 1)
+            return 1.0;
+        if (n == 2)
+            return 0.5;
+        if (dp[n] >= 0)
+            return dp[n];
+
+        double prev = solveME(n - 1, dp);
+        return dp[n] = (1.0 + (n - 2) * prev) / n;
+    }
+
+    double nthPersonGetsNthSeat(int n) {
+        vector<double> dp(n + 1, -1.0);
+        return solveME(n, dp);
+    }
 };
