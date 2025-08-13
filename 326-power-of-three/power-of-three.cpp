fca9bf7b40a5559e1ac1e4b4c66039cb70a6d384
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-13 23:20:26 +0530

    Time: 2 ms (66.52%) | Memory: 8.8 MB (97.26%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,11 +1,20 @@
+// class Solution {
+// public:
+//     bool isPowerOfThree(int n) {
+//         if (n <= 0) return false;
+//         while (n % 3 == 0) {
+//             n /= 3;
+//         }
+//         return n == 1;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n <= 0) return false;
-        while (n % 3 == 0) {
-            n /= 3;
-        }
-        return n == 1;
+        if (n == 1) return true;      // 3^0 = 1
+        if (n <= 0 || n % 3 != 0) return false;
+        return isPowerOfThree(n / 3); // har step me 3 se divide karo
     }
 };
-
