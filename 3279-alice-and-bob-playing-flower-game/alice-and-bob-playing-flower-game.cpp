d2adbbd9eb037548c9a70139eaa3f3e7d3fe0f22
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-23 13:41:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (13.27%) - LeetSync

--- 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     long long flowerGame(int n, int m) {
-        return (1LL * n * m) / 2;
+        // Step 1: Odd/Even count
+        long long odd_x = (n + 1) / 2;
+        long long even_x = n / 2;
+        long long odd_y = (m + 1) / 2;
+        long long even_y = m / 2;
+
+        // Step 2: Alice winning pairs
+        return odd_x * even_y + even_x * odd_y;
     }
 };
