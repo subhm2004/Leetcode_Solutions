5607a6e4093d35a1e6068b9069cad2df72d435bd
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-25 20:36:32 +0530

    Time: 5 ms (24.41%) | Memory: 13.7 MB (37.07%) - LeetSync

--- 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/sort-the-matrix-diagonally.cpp
@@ -1,77 +1,41 @@
 class Solution {
 public:
-    void sort_diagonal(vector<vector<int>>& mat, int row, int col) {
-        int m = mat.size(), n = mat[0].size();
+    void sort_diagonal(vector<vector<int>>& arr, int row, int col) {
+        int m = arr.size(), n = arr[0].size();
         vector<int> diagonal;
 
         // Diagonal elements ko ek vector me daalna
         int r = row, c = col;
         while (r < m && c < n) {
-            diagonal.push_back(mat[r++][c++]);
+            diagonal.push_back(arr[r++][c++]);
         }
 
         // Diagonal ko sort karna
         sort(diagonal.begin(), diagonal.end());
 
-        // Sorted values wapas matrix me daalna
+        // Sorted values wapas arrrix me daalna
         r = row, c = col;
         int i = 0;
         while (r < m && c < n) {
-            mat[r++][c++] = diagonal[i++];
+            arr[r++][c++] = diagonal[i++];
         }
     }
-    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
+
+    vector<vector<int>> diagonalSort(vector<vector<int>>& arr) {
+        int m = arr.size();
+        int n = arr[0].size();
 
         // First column ke diagonals ko process karenge
         for (int i = 0; i < m; i++) {
-            sort_diagonal(mat, i, 0);
+            sort_diagonal(arr, i, 0);
         }
 
-        // First row ke diagonals ko process karenge (except [0,0] jo pehle se
-        // ho chuka hai)
+        // First row ke diagonals ko process karenge (except [0,0] jo pehle se ho chuka hai)
         for (int j = 1; j < n; j++) {
-            sort_diagonal(mat, 0, j);
+            sort_diagonal(arr, 0, j);
         }
-        return mat;
+        return arr;
     }
 };
 
-// ( Using Priority Queue ye achi approach hai ise hi use krnege aage se )
-
-// class Solution {
-// public:
-//     void sortDiagonal(vector<vector<int>>& mat, int row, int col) {
-//         int m = mat.size(), n = mat[0].size();
-//         priority_queue<int, vector<int>, greater<int>> pq; // Min-Heap
-
-//         // Pehle diagonal elements ko collect karna hai
-//         int r = row, c = col;
-//         while (r < m && c < n) {
-//             pq.push(mat[r++][c++]);
-//         }
-
-//         // Sorted elements ko wapas matrix me daalna hai
-//         r = row, c = col;
-//         while (r < m && c < n) {
-//             mat[r++][c++] = pq.top();
-//             pq.pop();
-//         }
-//     }
-//     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-//         int m = mat.size(), n = mat[0].size();
-
-//         // Sabhi diagonals ko process karenge (leftmost column aur topmost row se start)
-//         for (int row = m - 1; row >= 0; row--) {
-//             sortDiagonal(mat, row, 0);
-//         }
-//         for (int col = 1; col < n; col++) {
-//             sortDiagonal(mat, 0, col);
-//         }
-
-//         return mat;
-//     }
-
-
-// };
-
+ 
