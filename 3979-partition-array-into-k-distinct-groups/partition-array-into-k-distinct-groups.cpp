f84d38ee6285486ea20c09306a224933a040766c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-25 20:37:56 +0530

    Time: 58 ms (12.50%) | Memory: 271.5 MB (43.53%) - LeetSync

---
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n % k != 0) return false; // total elements divide hone chahiye
+
+        unordered_map<int,int> freq;
+        for (int x : nums) freq[x]++;
+
+        int totalGroups = n / k;
+
+        for (auto [num, count] : freq) {
+            if (count > totalGroups) return false; 
+        }
+
+        return true;
+    }
+};
