1927c4a252438b41f2337156ef153f8467802340
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-26 11:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 29.2 MB (77.05%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -4,16 +4,17 @@ public:
         double max_diagonal = -1.0;   
         int max_area = -1;
 
-        for (auto& rect : dimensions) {
-            int l = rect[0], w = rect[1];
+        for (int i = 0; i < dimensions.size(); i++) {
+            int l = dimensions[i][0];
+            int w = dimensions[i][1];
+
             double diagonal = sqrt(1.0 * l * l + 1.0 * w * w);
             int area = l * w;
 
             if (diagonal > max_diagonal) {
                 max_diagonal = diagonal;
                 max_area = area;
-            } else if (diagonal == max_diagonal) {
-                // diagonals equal  
+            } else if (diagonal == max_diagonal) { // diagonal equal ho gya
                 max_area = max(max_area, area);
             }
         }
