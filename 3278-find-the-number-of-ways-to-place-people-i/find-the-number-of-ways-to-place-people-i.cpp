a09c13b6ead7d07d81587fcf358c4660ccc34bcd
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-08-28 16:55:22 +0530

    Time: 12 ms (50.77%) | Memory: 32.7 MB (40.77%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,40 +1,37 @@
-struct pair_hash {
-    size_t operator()(const pair<int,int>& p) const {
-        return hash<long long>()(((long long)p.first << 1) ^ p.second);
-    }
-};
-
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
-        
-        
+
         int ans = 0;
-        
+
         // check every pair (A,B)
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                
+                if (i == j)
+                    continue;
+
                 int Ax = points[i][0], Ay = points[i][1];
                 int Bx = points[j][0], By = points[j][1];
-                
+
                 // condition: A is upper-left of B (including same coordinates)
                 if (Ax <= Bx && Ay >= By) {
                     bool valid = true;
-                    
-                    // check if any other point lies inside rectangle (or on border)
+
+                    // check if any other point lies inside rectangle (or on
+                    // border)
                     for (int k = 0; k < n; k++) {
-                        if (k == i || k == j) continue;
+                        if (k == i || k == j)
+                            continue;
                         int Cx = points[k][0], Cy = points[k][1];
                         if (Ax <= Cx && Cx <= Bx && By <= Cy && Cy <= Ay) {
                             valid = false;
                             break;
                         }
                     }
-                    
-                    if (valid) ans++;
+
+                    if (valid)
+                        ans++;
                 }
             }
         }
