5e54cf255f10b720fa00a08e7a912bcc5cffd0c0
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-04 23:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.95%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -1,11 +1,16 @@
+typedef long long ll;
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        for (int k = 0; k <= 60; ++k) {
-            long long target = (long long)num1 - (long long)k * num2;
-            if (target < 0) continue;
-            int bits = __builtin_popcountll(target);
-            if (bits <= k && k <= target) return k;  
+        for (ll k = 1; k <= 60; k++) {
+            ll target = (ll)num1 - k * (ll)num2;
+            if (target < 0)
+                continue;
+
+            int bits = __builtin_popcountll(target); // kisi bhi number ki binary representatin me kitni set bits hai ( set bits hoti h 1 kitne present h uski binary representatin me ) okay ?
+
+            if (bits <= k && k<= target) 
+                return (int)k;
         }
         return -1;
     }
