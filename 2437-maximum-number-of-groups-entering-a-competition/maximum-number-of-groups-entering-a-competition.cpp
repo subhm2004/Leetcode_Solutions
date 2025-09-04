fa0cc58e976bbf8ef2535baa864548896ae370a7
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-05 02:17:37 +0530

    Time: 0 ms (100.00%) | Memory: 60.4 MB (27.62%) - LeetSync

--- 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.cpp
+++ 2437-maximum-number-of-groups-entering-a-competition/maximum-number-of-groups-entering-a-competition.cpp
@@ -1,6 +1,31 @@
+// class Solution {
+// public:
+//     int maximumGroups(vector<int>& grades) {
+//         int n = grades.size();
+//         int k = (-1 + sqrt(1 + 8 * n)) / 2;
+//         return k;
+//     }
+// };
+
 class Solution {
 public:
     int maximumGroups(vector<int>& grades) {
-        return (-1 + sqrt(1 + 8 * grades.size())) / 2;
+        int n = grades.size();
+        int low = 1;
+        int high = n;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            long long sum = 1LL * mid * (mid + 1) / 2;
+
+            if (sum <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
     }
 };
