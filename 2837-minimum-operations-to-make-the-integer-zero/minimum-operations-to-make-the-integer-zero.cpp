0468049362fbc27e4d21369fce58e9d9a2bed66c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-04 23:16:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.95%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -2,7 +2,7 @@ typedef long long ll;
 class Solution {
 public:
     int makeTheIntegerZero(int num1, int num2) {
-        for (ll k = 1; k <= 60; k++) {
+        for (ll k = 0; k <= 60; k++) {
             ll target = (ll)num1 - k * (ll)num2;
             if (target < 0)
                 continue;
