41da6f1915b1c0b1f70bb383a48e66b83aced754
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-05 18:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (96.83%) - LeetSync

--- 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.cpp
@@ -3,15 +3,23 @@ public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int n = nums.size();
         int ans = INT_MAX;
+        int i = 0;
+        int curr_OR = 0;
 
-        for (int i = 0; i < n; i++) {
-            int curr_OR = 0;
-            for (int j = i; j < n; j++) {
-                curr_OR |= nums[j];
-                if (curr_OR >= k) {
-                    ans = min(ans, j - i + 1);
-                    break; 
+        for (int j = 0; j < n; j++) {
+            curr_OR |= nums[j];  // window expand
+
+            // shrink jab tak OR ≥ k
+            while (i <= j && curr_OR >= k) {
+                ans = min(ans, j - i + 1);
+
+                // i ko move karo aur OR recompute
+                int temp_OR = 0;
+                for (int t = i + 1; t <= j; t++) {
+                    temp_OR |= nums[t];
                 }
+                i++;
+                curr_OR = temp_OR;
             }
         }
 
