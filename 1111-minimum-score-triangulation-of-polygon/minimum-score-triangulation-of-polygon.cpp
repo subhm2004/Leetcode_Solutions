8b35234c411b7080d7fca7c19b23fee332c70702
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-07 23:14:51 +0530

    Time: 1 ms (72.05%) | Memory: 11.3 MB (43.35%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -1,40 +1,40 @@
-class Solution {      // Partition Dp hai
+class Solution {
 public:
-    int solveRE(vector<int>& values, int i, int j) {
-        // Base case: Agar 3 se kam vertices hain, toh triangulation possible nahi hai
-        if (i+1 == j) return 0;
+    int solveRE(vector<int>& arr, int i, int j) {
+        if (i + 1 == j)
+            return 0;
 
         int ans = INT_MAX;
 
-        // Har possible partition ko try karte hain aur cost calculate karte hain
-        for (int k = i + 1; k < j; ++k) {
-            // Triangle (i, k, j) ka cost calculate karte hain
-            int cost = solveRE(values, i, k) + values[i] * values[k] * values[j] + solveRE(values, k, j);
-            // Minimum cost ko store karte hain
+        for (int k = i + 1; k < j; k++) {
+            int cost = solveRE(arr, i, k) + arr[i] * arr[k] * arr[j] +
+                       solveRE(arr, k, j);
             ans = min(ans, cost);
         }
         return ans;
     }
 
-    int solveME(vector<int>& values, int i, int j, vector<vector<int>>& dp) {
-        if (i+1 == j) return 0;
+    int solveME(vector<int>& arr, int i, int j, vector<vector<int>>& dp) {
+        if (i + 1 == j)
+            return 0;
 
-        if (dp[i][j] != -1) return dp[i][j];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         int ans = INT_MAX;
 
-        for (int k = i + 1; k < j; ++k) {
-            int cost = solveME(values, i, k, dp) + values[i] * values[k] * values[j] + solveME(values, k, j, dp);
+        for (int k = i + 1; k < j; k++) {
+            int cost = solveME(arr, i, k, dp) + arr[i] * arr[k] * arr[j] +
+                       solveME(arr, k, j, dp);
             ans = min(ans, cost);
         }
-
         return dp[i][j] = ans;
     }
 
-    int minScoreTriangulation(vector<int>& values) {
-        int n = values.size();
+    int minScoreTriangulation(vector<int>& arr) {
+        int n = arr.size();
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        
-        return solveME(values, 0, n - 1, dp);
+
+        return solveME(arr, 0, n - 1, dp);
     }
 };
