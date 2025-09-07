94992f58c758063a776badf899b5b50227351401
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-07 18:16:06 +0530

    Time: 59 ms (46.13%) | Memory: 32.5 MB (30.47%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.cpp
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -1,78 +1,78 @@
 class Solution {
 public:
-    int solveRE(int K, int N) {
-        if (N <= 1)
-            return N; // If 0 or 1 floor, we need at most N drops
-        if (K == 1)
-            return N; // With one egg, we drop from every floor
-
+    int solveRE(int k, int n) {
+        if (k == 1)
+            return n;
+        if (n <= 1)
+            return n;
         int ans = INT_MAX;
 
-        for (int i = 1; i <= N; i++) {
-            int broken = solveRE(K - 1, i - 1); // Egg breaks -> Check below
-            int unbroken = solveRE(K, N - i);   // Egg survives -> Check above
-            int worst = 1 + max(broken, unbroken); // Worst-case scenario
+        for (int i = 1; i <= n; i++) {
+            int broken = solveRE(k - 1, i - 1);
+            int unbroken = solveRE(k, n - i);
+
+            int worst = 1 + max(broken, unbroken);
 
             ans = min(ans, worst);
         }
         return ans;
     }
 
-    int solveME(vector<vector<int>>& dp, int K, int N) {
-        if (N <= 1)
-            return N;
-        if (K == 1)
-            return N;
-
-        if (dp[K][N] != -1)
-            return dp[K][N];
+    int solveME(int k, int n, vector<vector<int>>& dp) {
+        if (k == 1)
+            return n;
+        if (n <= 1)
+            return n;
+        if (dp[k][n] != -1)
+            return dp[k][n];
 
         int ans = INT_MAX;
 
-        for (int i = 1; i <= N; i++) {
-            int broken = solveME(dp, K - 1, i - 1);
-            int unbroken = solveME(dp, K, N - i);
+        for (int i = 1; i <= n; i++) {
+            int broken = solveME(k - 1, i - 1, dp);
+            int unbroken = solveME(k, n - i, dp);
+
             int worst = 1 + max(broken, unbroken);
 
             ans = min(ans, worst);
         }
-
-        return dp[K][N] = ans;
+        return dp[k][n] = ans;
     }
 
-    int solve_Binary_ME(vector<vector<int>>& dp, int K, int N) {
-        if (N <= 1)
-            return N;
-        if (K == 1)
-            return N;
-
-        if (dp[K][N] != -1)
-            return dp[K][N];
+    int solve_Binary_ME(int k, int n, vector<vector<int>>& dp) {
+        if (k == 1)
+            return n;
+        if (n <= 1)
+            return n;
+        if (dp[k][n] != -1)
+            return dp[k][n];
 
         int ans = INT_MAX;
-        int l = 1, r = N;
-
+        int l = 1, r = n;
         while (l <= r) {
-            int mid = (l+r) >> 1;
-            
-            int broken = solve_Binary_ME(dp, K - 1, mid - 1); // Egg breaks → check below
-            int unbroken = solve_Binary_ME(dp, K, N - mid);   // Egg survives → check above
+            int mid = (l + r) >> 1;
+            int broken = solve_Binary_ME(k - 1, mid - 1, dp);
+
+            int unbroken = solve_Binary_ME(k, n - mid, dp);
 
             int worst = 1 + max(broken, unbroken);
+
             ans = min(ans, worst);
 
             if (broken > unbroken) {
-                r = mid - 1; // Move downwards
+                r = mid - 1;
+
             } else {
-                l = mid + 1; // Move upwards
+                l = mid + 1;
             }
         }
-
-        return dp[K][N] = ans;
+        return dp[k][n] = ans;
     }
 
-    int superEggDrop(int K, int N) {
-        vector<vector<int>> dp(K + 1, vector<int>(N + 1, -1));
-        return solve_Binary_ME(dp, K, N);
+    int superEggDrop(int k, int n) {
+        // return solveRE(k, n);
+        vector<vector<int>> dp(k + 1, vector<int>(n + 1, -1));
+        // return solveME(k, n, dp);
+        return solve_Binary_ME(k, n, dp);
     }
 };
