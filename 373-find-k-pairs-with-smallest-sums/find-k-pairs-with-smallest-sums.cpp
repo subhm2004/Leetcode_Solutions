4d706c0d97ddf1bd92247d6ae1e944615e4dd424
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-09 00:37:34 +0530

    Time: 44 ms (76.14%) | Memory: 135.6 MB (76.57%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<vector<int>> result;
-        if (nums1.empty() || nums2.empty() || k == 0) return result;
+        vector<vector<int>> ans;
+        if (nums1.empty() || nums2.empty() || k == 0) return ans;
 
         // Min heap: {sum, i, j}
         priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> min_heap;
@@ -16,13 +16,13 @@ public:
         while (k-- > 0 && !min_heap.empty()) {
             auto [sum, i, j] = min_heap.top();
             min_heap.pop();
-            result.push_back({nums1[i], nums2[j]});
+            ans.push_back({nums1[i], nums2[j]});
 
             // Agla candidate push karo
             if (j + 1 < nums2.size()) {
                 min_heap.push({nums1[i] + nums2[j+1], i, j+1});
             }
         }
-        return result;
+        return ans;
     }
 };
