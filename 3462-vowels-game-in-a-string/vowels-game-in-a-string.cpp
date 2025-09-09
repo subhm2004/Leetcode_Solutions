71e8216571553768680f9dd7d14c327e0c77ecaf
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-09 17:21:48 +0530

    Time: 4 ms (79.28%) | Memory: 19.1 MB (54.75%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    bool isVowel(char c) {
+    bool is_vowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
+
     bool doesAliceWin(string s) {
-        int vowelCount = 0;
+        int vowel_count = 0;
 
         // Step 1: Count the number of vowels in the string
-        for (char c : s) {
-            if (isVowel(c)) {
-                vowelCount++;
+        for (char &c : s) {
+            if (is_vowel(c)) {
+                vowel_count++;
             }
         }
 
         // Step 2: Alice wins if there is at least one vowel
-        return vowelCount > 0;
+        return vowel_count > 0;
     }
 };
