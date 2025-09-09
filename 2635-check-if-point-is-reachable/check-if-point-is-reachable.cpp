2dd320f80d8b0f17162201e860d3ac0834ab558f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-10 03:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (13.01%) - LeetSync

--- 2635-check-if-point-is-reachable/check-if-point-is-reachable.cpp
+++ 2635-check-if-point-is-reachable/check-if-point-is-reachable.cpp
@@ -1,39 +1,5 @@
 // class Solution {
 // public:
-//     bool is_visited(vector<pair<int,int>>& visited, int x, int y) {
-//         for (auto &p : visited) {
-//             if (p.first == x && p.second == y) return true;
-//         }
-//         return false;
-//     }
-
-//     bool dfs(int x, int y, int targetX, int targetY, vector<pair<int,int>>& visited) {
-//         if (x == targetX && y == targetY) return true;
-//         if (x <= 0 || y <= 0 || x > targetX * 2 || y > targetY * 2) return false;
-
-//         if (is_visited(visited, x, y)) return false;
-//         visited.push_back({x, y});
-
-//         // try all 4 moves
-//         if (dfs(x, y - x, targetX, targetY, visited)) return true;
-//         if (dfs(x - y, y, targetX, targetY, visited)) return true;
-//         if (dfs(2 * x, y, targetX, targetY, visited)) return true;
-//         if (dfs(x, 2 * y, targetX, targetY, visited)) return true;
-
-//         return false;
-//     }
-
-//     bool isReachable(int targetX, int targetY) {
-//         vector<pair<int,int>> visited;
-//         return dfs(1, 1, targetX, targetY, visited);
-//     }
-// };
-
-
-
-
-// class Solution {
-// public:
 //     bool is_valid(int nx, int ny, int targetX, int targetY, vector<pair<int,int>>& visited) {
 //         if (nx <= 0 || ny <= 0) return false;                    
 //         if (nx > targetX * 2 || ny > targetY * 2) return false;  
@@ -57,14 +23,14 @@
 
 //             if (x == targetX && y == targetY) return true;
 
-//             vector<pair<int,int>> nextMoves = {
+//             vector<pair<int,int>> next_moves = {
 //                 {x, y - x},
 //                 {x - y, y},
 //                 {2 * x, y},
 //                 {x, 2 * y}
 //             };
 
-//             for (auto [nx, ny] : nextMoves) {
+//             for (auto [nx, ny] : next_moves) {
 //                 if (is_valid(nx, ny, targetX, targetY, visited)) {
 //                     visited.push_back({nx, ny});
 //                     q.push({nx, ny});
@@ -84,7 +50,9 @@ class Solution {
 public:
     bool isReachable(long long targetX, long long targetY) {
         long long g = gcd(targetX, targetY);
-        // check if gcd is power of 2
-        return (g & (g - 1)) == 0;
+        // check if gcd is power of 
+        
+        return __builtin_popcountll(g) == 1 ;
+        // return (g & (g - 1)) == 0;
     }
 };
