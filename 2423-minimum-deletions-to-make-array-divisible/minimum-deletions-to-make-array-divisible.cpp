91d9ce3c6fdd91b87c63209af4d4ee6eb8f671fa
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-10 13:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 92 MB (17.38%) - LeetSync

--- 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
+++ 2423-minimum-deletions-to-make-array-divisible/minimum-deletions-to-make-array-divisible.cpp
@@ -14,29 +14,68 @@ const auto __ = []() {
 }();
 #endif
 
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+//         // nums ko set me daalo → sorted unique elements
+//         set<int> s(nums.begin(), nums.end());
+
+//         int deletions = 0;
+//         for (int candidate : s) {
+//             bool valid = true;
+//             for (int x : numsDivide) {
+//                 if (x % candidate != 0) {
+//                     valid = false;
+//                     break;
+//                 }
+//             }
+//             if (valid) {
+//                 // jitne nums < candidate h unko delete karna padega
+//                 for (int val : nums) {
+//                     if (val < candidate) deletions++;
+//                 }
+//                 return deletions;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+// Property 
+//
+//Agar nums ka smallest element x sabhi numsDivide elements ko divide karna hai, iska matlab:
+// x / gcd(numsDivide[0],numsDivide[1],…,numsDivide[m−1]) 
+// Matlab hume numsDivide ka GCD (Greatest Common Divisor) nikalna hoga.
+// Fir nums me se smallest element jo gcd ko divide kare, wahi answer hoga.
+// Usse chhote saare elements delete karne padenge.
+
+// Agar x sabhi numsDivide ke elements ko divide karta hai, iska matlab hai:
+// To x ek common divisor hai sabhi elements ka.
+// Sabhi numbers ke common divisors hamesha unke GCD ko divide karte hain.
+// eg : x = 3 and numsDivide = [9, 6, 15] 
+// g = gcd(9, 6, 15) = 3
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-        // nums ko set me daalo → sorted unique elements
-        set<int> s(nums.begin(), nums.end());
+        // Step 1: Find GCD of numsDivide
+        int g = numsDivide[0];
+        for (int i = 1; i < numsDivide.size(); i++) {
+            g = __gcd(g, numsDivide[i]);
+        }
 
-        int deletions = 0;
-        for (int candidate : s) {
-            bool valid = true;
-            for (int x : numsDivide) {
-                if (x % candidate != 0) {
-                    valid = false;
-                    break;
-                }
-            }
-            if (valid) {
-                // jitne nums < candidate h unko delete karna padega
-                for (int val : nums) {
-                    if (val < candidate) deletions++;
-                }
-                return deletions;
+        // Step 2: Sort nums
+        sort(nums.begin(), nums.end());
+
+        // Step 3: Find first num that divides gcd
+        for (int i = 0; i < nums.size(); i++) {
+            if (g % nums[i] == 0) {
+                return i; // i = number of deletions (all smaller elements deleted)
             }
         }
         return -1;
     }
 };
+
