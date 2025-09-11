fc80486f080cdfac121599d48b660c7345b92c02
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-12 01:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.15%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,26 +1,9 @@
-// class Solution {
-// public:
-//     int missingNumber(vector<int>& nums) {
-//         int n = nums.size();
-//         int sum1 = (n * (n + 1)) / 2;
-//         int sum2 = accumulate(nums.begin(), nums.end(), 0);
-//         return sum1 - sum2;
-//     }
-// };
-
-
 class Solution {
- public:
-  int missingNumber(vector<int>& nums) {
-    int xorAll = 0;
-    int n = nums.size();
-
-    for (int i = 0; i < n; ++i) {
-      xorAll ^= i ^ nums[i];
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum1 = (n * (n + 1) / 2);
+        int sum2 = accumulate(nums.begin(), nums.end(), 0);
+        return sum1 - sum2;
     }
-
-    xorAll ^= n;
-
-    return xorAll;
-  }
 };
