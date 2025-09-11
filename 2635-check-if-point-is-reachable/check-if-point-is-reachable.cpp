9fe2708dd64f092dd62221ba9bb91cee5a59355c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-12 00:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (85.25%) - LeetSync

--- 2635-check-if-point-is-reachable/check-if-point-is-reachable.cpp
+++ 2635-check-if-point-is-reachable/check-if-point-is-reachable.cpp
@@ -82,8 +82,15 @@
 
 class Solution {
 public:
+    // a * b = lcm(a,b) * hcf(a,b)
+    // hcf or gcd dono same hota hai
+    int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
     bool isReachable(int targetX, int targetY) {
-        long long g = __gcd(targetX, targetY);
+        long long g = gcd(targetX, targetY);
         return (g & (g - 1)) == 0;
     }
 };
