94ff804bc7a7e5f1de40a375b47fe1fd6b392383
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-23 14:56:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (10.40%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -5,33 +5,34 @@ const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
 #define LC_HACK
 #ifdef LC_HACK
 const auto __ = []() {
-  struct ___ { static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; } };
-  std::atexit(&___::_);
-  return 0;
+    struct ___ {
+        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
+    };
+    std::atexit(&___::_);
+    return 0;
 }();
 #endif
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 public:
-    int compareVersion(string version1, string version2) {
-        // helper function: string ko split karke vector<int> banao
-        auto split = [](string &s) {
-            vector<int> arr;
-            string temp;
-            for (char c : s) {
-                if (c == '.') {
-                    arr.push_back(stoi(temp)); // string → int
-                    temp.clear();
-                } else {
-                    temp.push_back(c);
-                }
-            }
-            arr.push_back(stoi(temp)); // last part add karo
-            return arr;
-        };
+    // helper function: string ko split karke vector<int> banao
+    vector<int> split(const string& s) {
+        vector<int> arr;
+        string temp;
 
+        for (char c : s) {
+            if (c == '.') {
+                arr.push_back(stoi(temp)); // ek number complete ho gaya
+                temp.clear();
+            } else {
+                temp.push_back(c);
+            }
+        }
+        arr.push_back(stoi(temp)); // last number add karna mat bhoolna
+        return arr;
+    }
+
+    int compareVersion(string version1, string version2) {
         vector<int> v1 = split(version1);
         vector<int> v2 = split(version2);
 
@@ -42,8 +43,10 @@ public:
 
         // ab compare karo
         for (int i = 0; i < n; i++) {
-            if (v1[i] < v2[i]) return -1;
-            if (v1[i] > v2[i]) return 1;
+            if (v1[i] < v2[i])
+                return -1;
+            if (v1[i] > v2[i])
+                return 1;
         }
         return 0; // sab equal nikle
     }
