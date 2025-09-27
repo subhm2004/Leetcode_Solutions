8f1ca76c0ae6a617c7bfbc23d4f419bfa969e869
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-09-27 11:40:54 +0530

    Time: 3 ms (35.26%) | Memory: 10.4 MB (63.36%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -1,3 +1,53 @@
+// Shoelace Formula
+// class Solution {
+// public:
+//     double largestTriangleArea(vector<vector<int>>& points) {
+//         double ans = 0;
+
+//         for (int i = 0; i < points.size(); ++i) {
+//             for (int j = i + 1; j < points.size(); ++j) {
+//                 for (int k = j + 1; k < points.size(); ++k) {
+//                     // (1/2) |x1(y2 − y3) + x2(y3 − y1) + x3(y1 − y2)|
+//                     double area =
+//                         0.5 * abs(points[i][0] * (points[j][1] - points[k][1]) +
+//                                   points[j][0] * (points[k][1] - points[i][1]) +
+//                                   points[k][0] * (points[i][1] - points[j][1]));
+//                     ans = max(ans, area);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// Herons Formula
+// class Solution {
+// public:
+//     double largestTriangleArea(vector<vector<int>>& points) {
+//         double ans = 0;
+
+//         auto dist = [&](vector<int>& a, vector<int>& b) {
+//             return sqrt((a[0] - b[0]) * 1.0 * (a[0] - b[0]) + 
+//                         (a[1] - b[1]) * 1.0 * (a[1] - b[1]));
+//         };
+
+//         for (int i = 0; i < points.size(); ++i) {
+//             for (int j = i + 1; j < points.size(); ++j) {
+//                 for (int k = j + 1; k < points.size(); ++k) {
+//                     double a = dist(points[i], points[j]);
+//                     double b = dist(points[j], points[k]);
+//                     double c = dist(points[k], points[i]);
+//                     double s = (a + b + c) / 2.0;
+//                     double area = sqrt(max(0.0, s * (s - a) * (s - b) * (s - c)));
+//                     ans = max(ans, area);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+// Trignometric formula 
 class Solution {
 public:
     double largestTriangleArea(vector<vector<int>>& points) {
@@ -38,3 +88,5 @@ public:
         return maxArea;
     }
 };
+
+
