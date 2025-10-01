dd043e3c2c8cd48a078f79551034f38c06c0f6e4
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-01 23:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.15%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,21 +1,33 @@
-class Solution {
+// class Solution {
+// public:
+//     int findTheWinner(int n, int k) {
+//         vector<int> arr;
+//         for(int i = 1; i <= n; i++) {
+//             arr.push_back(i);
+//         }
+
+//         int i = 0; //Game starts from 1st player which is sitting at index 0 in arr
+
+//         while(arr.size() > 1) {
+//             int idx = (i + k - 1) % arr.size();
+
+//             arr.erase(arr.begin() + idx);
+
+//             i = idx;
+//         }
+
+//         return arr[0];
+//     }
+// };
+
+
+
+class Solution { //Josephus Problem
 public:
-    int findTheWinner(int n, int k) {
-        vector<int> arr;
-        for(int i = 1; i <= n; i++) {
-            arr.push_back(i);
-        }
-
-        int i = 0; //Game starts from 1st player which is sitting at index 0 in arr
-
-        while(arr.size() > 1) {
-            int idx = (i + k - 1) % arr.size();
-
-            arr.erase(arr.begin() + idx);
-
-            i = idx;
-        }
-
-        return arr[0];
+    int solveRE(int n, int k) { // ye 0 index based solve kr diya hai 
+        if (n == 1)
+            return 0;
+        return (solveRE(n - 1, k) + k) % n;
     }
+    int findTheWinner(int n, int k) { return solveRE(n, k) + 1; } // 1 baed idx me convert kr diya
 };
