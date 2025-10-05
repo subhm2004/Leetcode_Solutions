52fee87c24010be05b7a341c2587ed5454980c2a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-06 00:54:27 +0530

    Time: 7 ms (49.35%) | Memory: 8.2 MB (21.83%) - LeetSync

--- 1276-closest-divisors/closest-divisors.cpp
+++ 1276-closest-divisors/closest-divisors.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     vector<int> find_closest_divisor_pair(int n) {
-        int a = 1, b = n; // Initialize divisors with default 1 and n
-        int sqrt_n = sqrt(n); // We only need to check divisors up to sqrt(n)
-        for (int i = 1; i <= sqrt_n; i++) { // Loop from 1 to sqrt(n)
-            if (n % i == 0) { // If i divides n exactly
-                int j = n / i; // j is the corresponding paired divisor
-                if (abs((long long)i - j) < abs((long long)a - b)){ // If this pair has smaller difference than previous
-                    a = i; // Update a
-                    b = j; // Update b
+        int a = 1, b = n;
+        int sqrt_n = sqrt(n);
+        for (int i = 1; i <= sqrt_n; i++) {
+            if (n % i == 0) {
+                int j = n / i;
+                if (abs((long long)i - j) < abs((long long)a - b)){
+                    a = i;
+                    b = j;
                 }
             }
         }
-        return {a, b}; // Return the pair of divisors with minimum absolute difference
+        return {a, b};
     }
+
     vector<int> closestDivisors(int num) {
-        vector<int> pair1 = find_closest_divisor_pair(num + 1); // Find closest divisors of num + 1
-        vector<int> pair2 = find_closest_divisor_pair(num + 2); // Find closest divisors of num + 2
+        vector<int> pair1 = find_closest_divisor_pair(num + 1);
+        vector<int> pair2 = find_closest_divisor_pair(num + 2);
         
-        // Compare absolute difference of the two pairs
-        // Return the pair which has smaller difference
         if (abs(pair1[0] - pair1[1]) <= abs(pair2[0] - pair2[1]))
             return pair1;
         return pair2;
