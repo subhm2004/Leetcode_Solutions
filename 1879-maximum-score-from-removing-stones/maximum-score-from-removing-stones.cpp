b8160e13eb6f5ef1686fe6904fc47b2f87a537cb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-07 18:58:47 +0530

    Time: 55 ms (50.00%) | Memory: 7.9 MB (52.06%) - LeetSync

---
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        priority_queue<int> pq;
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        
+        int score = 0;
+        
+        while (pq.size() > 1) {
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
+            
+            x--; y--; // ek stone dono piles se hata do or store bada do 1 unit se or agar piles se stone hatane k baad bhi value > 0 hai to fir se khelte rho jab tak 2 piles ka score 0 na ho jaye
+            score++;
+            
+            if (x > 0) pq.push(x);
+            if (y > 0) pq.push(y);
+        }
+        
+        return score;
+    }
+};
