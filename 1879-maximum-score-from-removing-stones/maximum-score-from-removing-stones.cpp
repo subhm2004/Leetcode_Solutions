1e5bf1dc8d0f1630d8fece50235f8e0b3bfb7ef2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-07 19:04:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (84.02%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int maximumScore(int a, int b, int c) {
-        priority_queue<int> pq;
-        pq.push(a);
-        pq.push(b);
-        pq.push(c);
-        
-        int score = 0;
-        
-        while (pq.size() > 1) {
-            int x = pq.top(); pq.pop();
-            int y = pq.top(); pq.pop();
-            
-            x--; y--; // ek stone dono piles se hata do or store bada do 1 unit se or agar piles se stone hatane k baad bhi value > 0 hai to fir se khelte rho jab tak 2 piles ka score 0 na ho jaye
-            score++;
-            
-            if (x > 0) pq.push(x);
-            if (y > 0) pq.push(y);
-        }
-        
-        return score;
+        int sum = a + b + c;
+        int maxi = max({a, b, c});
+        return min(sum / 2, sum - maxi);
     }
 };
+
+// class Solution {
+// public:
+//     int maximumScore(int a, int b, int c) {
+//         priority_queue<int> pq;
+//         pq.push(a);
+//         pq.push(b);
+//         pq.push(c);
+
+//         int score = 0;
+
+//         while (pq.size() > 1) {
+//             int x = pq.top();
+//             pq.pop();
+//             int y = pq.top();
+//             pq.pop();
+
+//             x--;
+//             y--; // ek stone dono piles se hata do or store bada do 1 unit se or
+//                  // agar piles se stone hatane k baad bhi value > 0 hai to fir
+//                  // se khelte rho jab tak 2 piles ka score 0 na ho jaye
+//             score++;
+
+//             if (x > 0)
+//                 pq.push(x);
+//             if (y > 0)
+//                 pq.push(y);
+//         }
+
+//         return score;
+//     }
+// };
