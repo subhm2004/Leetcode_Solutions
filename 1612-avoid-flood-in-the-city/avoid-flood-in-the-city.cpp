c9dbbbd16a9c3913f497ca7d4c0e5c8fea0909c5
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-08 21:21:44 +0530

    Time: 133 ms (33.62%) | Memory: 114.8 MB (36.75%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
         int n = rains.size();
-    vector<int> ans(n);
-    unordered_map<int,int> lastFullDay; // lake -> last day it got full
-    set<int> sunnyDays; // indices of sunny days
+        vector<int> ans(n);
+        unordered_map<int, int> last_full_day; // lake -> last day it got full
+        set<int> sunny_days;                  // indices of sunny days
 
-    for (int i = 0; i < n; i++) {
-        if (rains[i] > 0) {
-            int lake = rains[i];
-            if (lastFullDay.count(lake)) {
-                // lake is full, need to find a sunny day to dry it
-                bool dried = false;
-                for (int sd : sunnyDays) {
-                    if (sd > lastFullDay[lake]) {
-                        ans[sd] = lake; // dry this lake
-                        sunnyDays.erase(sd);
-                        dried = true;
-                        break;
+        for (int i = 0; i < n; i++) {
+            if (rains[i] > 0) {
+                int lake = rains[i];
+                if (last_full_day.count(lake)) {
+                    // lake is full, need to find a sunny day to dry it
+                    bool dried = false;
+                    for (int sd : sunny_days) {
+                        if (sd > last_full_day[lake]) {
+                            ans[sd] = lake; // dry this lake
+                            sunny_days.erase(sd);
+                            dried = true;
+                            break;
+                        }
                     }
+                    if (!dried)
+                        return {}; // no sunny day to dry → flood
                 }
-                if (!dried) return {}; // no sunny day to dry → flood
+                last_full_day[lake] = i;
+                ans[i] = -1;
+            } else {
+                sunny_days.insert(i); // mark this day as sunny
             }
-            lastFullDay[lake] = i;
-            ans[i] = -1;
-        } else {
-            sunnyDays.insert(i); // mark this day as sunny
         }
-    }
 
-    // For remaining sunny days, dry any lake (let's pick 1)
-    for (int sd : sunnyDays) ans[sd] = 1;
+        // For remaining sunny days, dry any lake chzlo ki day 1 ko dry kr dete hai
+        for (int sd : sunny_days)
+            ans[sd] = 1;
 
-    return ans;
+        return ans;
     }
 };
