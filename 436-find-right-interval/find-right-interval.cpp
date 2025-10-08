0d4239881ba50197a15bb777ba44bcd474212d85
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-08 20:21:23 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (61.56%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -1,30 +1,37 @@
-class Solution {
+class Solution { // Binary Search 
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
         int n = intervals.size();
-        vector<int> ans(n, -1);  // initially sabko -1 se bhar do
-
-        // Outer loop → har interval ke liye
+        vector<pair<int,int>> arr;  // {start, original_idx}
+        
         for(int i = 0; i < n; i++) {
-            int end_val = intervals[i][1];   // current interval ka END
-            int min_start = INT_MAX;         // best candidate ka start
-            int idx = -1;                   // best candidate ka index
-
-            // Inner loop → sab intervals check karo
-            for(int j = 0; j < n; j++) {
-                // Agar start[j] ≥ end[i] hai to ye candidate ban sakta hai
-                if(intervals[j][0] >= end_val) {
-                    // Dekho kya ye ab tak ka sabse chhota start hai?
-                    if(intervals[j][0] < min_start) {
-                        min_start = intervals[j][0];
-                        idx = j;
-                    }
+            arr.push_back({intervals[i][0], i});
+        }
+        
+        // Sort by start
+        sort(arr.begin(), arr.end());
+        
+        vector<int> ans(n, -1);
+        
+        for(int i = 0; i < n; i++) {
+            int end_val = intervals[i][1];
+            
+            // Binary search in arr array
+            int l = 0, r = n - 1, idx = -1;
+            while(l <= r) {
+                int mid = (l + r) / 2;
+                if(arr[mid].first >= end_val) {
+                    idx = arr[mid].second; // candidate mila
+                    r = mid - 1;              // aur chhota dhundo
+                } else {
+                    l = mid + 1;
                 }
             }
-
-            ans[i] = idx;  // jo best mila uska index store karo
+            
+            ans[i] = idx;
         }
-
+        
         return ans;
     }
 };
+
