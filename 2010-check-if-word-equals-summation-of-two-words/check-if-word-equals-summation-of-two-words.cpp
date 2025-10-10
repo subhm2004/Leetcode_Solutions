09e51529bda5f0ff077362fb29e9f11052aa915d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-11 04:33:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (44.96%) - LeetSync

--- 2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.cpp
+++ 2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.cpp
@@ -16,6 +16,12 @@ public:
         teesra char 'b' → 1 → num = 2*10 + 1 = 21
 
     */
+    int get_value(string s) {
+        string num = "";
+        for (char c : s)
+            num += to_string(c - 'a');
+        return stoi(num);
+    }
 
     bool isSumEqual(string firstWord, string secondWord, string targetWord) {
         return get_str_value(firstWord) + get_str_value(secondWord) == get_str_value(targetWord);
