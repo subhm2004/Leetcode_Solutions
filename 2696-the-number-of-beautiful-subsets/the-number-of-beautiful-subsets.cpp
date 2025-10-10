0dbcd524da03ac90c24caa74a02b5884feb44e6b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-11 03:26:29 +0530

    Time: 103 ms (83.48%) | Memory: 37.3 MB (94.15%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int beautifulSubsets(vector<int>& nums, int k) {
-        int n = nums.size();
-        int count = 0;
+    int count = 0;  
 
-        // Iterate through all subsets using bitmask
-        for (int mask = 1; mask < (1 << n);
-             mask++) { // mask = 0 is empty subset
-            bool beautiful = true;
-            vector<int> subset;
-
-            for (int i = 0; i < n; i++) {
-                if (mask & (1 << i))
-                    subset.push_back(nums[i]);
-            }
-
-            // Check if subset is beautiful
-            for (int i = 0; i < subset.size(); i++) {
-                for (int j = i + 1; j < subset.size(); j++) {
-                    if (abs(subset[i] - subset[j]) == k) {
-                        beautiful = false;
-                        break;
-                    }
-                }
-                if (!beautiful)
-                    break;
-            }
-
-            if (beautiful)
-                count++;
+    void backtrack(vector<int>& nums, int k, int i, vector<int>& subset) {
+        if(i == nums.size()) {
+            if(!subset.empty()) count++;
+            return;
         }
 
+        // Option 1: Include nums[i] ko agar ye safe hai 
+        bool can_include = true;
+        for(int val : subset) {
+            if(abs(val - nums[i]) == k) {
+                can_include = false;
+                break;
+            }
+        }
+
+        if(can_include) {
+            subset.push_back(nums[i]); // include
+            backtrack(nums, k, i + 1, subset); // call recursive function again
+            subset.pop_back(); // backtrack
+        }
+
+        // Option 2: Exclude nums[i]
+        backtrack(nums, k, i + 1, subset);
+    }
+
+    int beautifulSubsets(vector<int>& nums, int k) {
+        vector<int> subset;
+        count = 0;
+        backtrack(nums, k, 0, subset);
         return count;
     }
 };
