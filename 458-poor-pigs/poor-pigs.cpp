b19380238a5a1714251ca163ef2a3d0d136ed71b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-10 17:57:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (46.53%) - LeetSync

--- 458-poor-pigs/poor-pigs.cpp
+++ 458-poor-pigs/poor-pigs.cpp
@@ -1,11 +1,18 @@
+//class Solution {
+// public:
+//     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+//         int tests_trials = minutesToTest / minutesToDie;
+//         int pigs = 0;
+//         while (pow(tests_trials + 1, pigs) < buckets) {
+//             pigs++;
+//         }
+//         return pigs;
+//     }
+// };
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int tests_trials = minutesToTest / minutesToDie;
-        int pigs = 0;
-        while (pow(tests_trials + 1, pigs) < buckets) {
-            pigs++;
-        }
-        return pigs;
+        int tests = minutesToTest / minutesToDie;
+        return ceil(log(buckets) / log(tests + 1)- 1e-12);
     }
 };
