79656d9ab3daab852a091b8f12e01a19200710f3
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-16 22:46:10 +0530

    Time: 250 ms (25.04%) | Memory: 181.9 MB (37.88%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
+    bool is_possible(vector<int>& nums, int k, vector<int>& incresing) {
+        int n = nums.size();
+        for (int i = 0; i + 2 * k <= n; i++) {
+            if (incresing[i + k - 1] >= k && incresing[i + 2 * k - 1] >= k)
+                return true;
+        }
+        return false;
+    }
+
     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        int curr_count = 1;
-        int prev_count = 0;
-        int ans = 0;
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > nums[i - 1]) {
-                ++curr_count;
+        vector<int> incresing(n, 1);
+
+        // compute incresing streaks
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1])
+                incresing[i] = incresing[i - 1] + 1;
+        }
+
+        int low = 1, high = n, ans = 0;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (is_possible(nums, mid, incresing)) {
+                ans = mid;
+                low = mid + 1; // try bigger k
             } else {
-                prev_count = curr_count;
-                curr_count = 1;
+                high = mid - 1; // smaller k
             }
-            ans = max(ans, min(prev_count, curr_count));
-            ans = max(ans, curr_count / 2);
         }
         return ans;
     }
