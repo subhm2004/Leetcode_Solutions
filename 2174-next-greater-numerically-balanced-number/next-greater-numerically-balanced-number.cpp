699e14db1367378510ac2c95b13f8ca75f61ded3
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-18 22:44:43 +0530

    Time: 856 ms (17.34%) | Memory: 257 MB (13.30%) - LeetSync

---
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool is_balanced(int x) {
+        string s = to_string(x);
+        vector<int> count(10, 0);
+
+        for (char c : s)
+            count[c - '0']++;
+
+        for (int i = 0; i < 10; i++)
+            if (count[i] && count[i] != i)
+                return false;
+
+        return true;
+    }
+
+    int nextBeautifulNumber(int n) {
+        for (int i = n + 1; i <= 1224444; i++) {
+            if (is_balanced(i))
+                return i;
+        }
+        return -1;
+    }
+};
