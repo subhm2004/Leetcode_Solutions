08fac0bb8462ccb4707f928d51902f110613fb6e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-20 16:42:03 +0530

    Time: 7 ms (87.25%) | Memory: 9.3 MB (80.84%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,59 @@
+// class Solution {
+// public:
+//     bool is_palindrome(const string& s, int l, int r) {
+//             while (l < r) {
+//                 if (s[l++] != s[r--])
+//                     return false;
+//             }
+//             return true;
+//         }
+//     string longestPalindrome(string s) {
+//         int n = s.length();
+//         int max_len = 1, start_idx = 0;
+
+//         // Check all substrings s[i..j]
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = i; j < n; ++j) {
+//                 if (is_palindrome(s, i, j)) {
+//                     int len = j - i + 1;
+//                     if (len > max_len) {
+//                         max_len = len;
+//                         start_idx = i;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return s.substr(start_idx, max_len);
+//     }
+// };
+
+
 class Solution {
 public:
-    bool isPalindrome(const string& s, int l, int r) {
-            while (l < r) {
-                if (s[l++] != s[r--])
-                    return false;
-            }
-            return true;
+    // Expand from the center and return length of palindrome
+    int expandAroundCenter(const string &s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
         }
+        return right - left - 1; // Length of palindrome
+    }
+
     string longestPalindrome(string s) {
-        int n = s.length();
-        int max_len = 1, start_idx = 0;
+        if (s.empty()) return "";
+        int start = 0, end = 0;
 
-        // Check all substrings s[i..j]
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                if (isPalindrome(s, i, j)) {
-                    int len = j - i + 1;
-                    if (len > max_len) {
-                        max_len = len;
-                        start_idx = i;
-                    }
-                }
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expandAroundCenter(s, i, i);     // Odd length
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length
+            int len = max(len1, len2);
+
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
-
-        return s.substr(start_idx, max_len);
+        return s.substr(start, end - start + 1);
     }
 };
