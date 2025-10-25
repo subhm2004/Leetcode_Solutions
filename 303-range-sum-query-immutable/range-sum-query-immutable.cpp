0bc25f4b63c45f2ea6c360cc5728c667abcd7f2c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-25 14:29:46 +0530

    Time: 3 ms (49.95%) | Memory: 24.3 MB (22.89%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,16 +1,92 @@
-class NumArray {
-public:
-    vector<int> nums;
+// prefixSum[i] ka matlab hota hai: nums[0] + nums[1] + ... + nums[i - 1]
 
-    NumArray(vector<int>& nums) : nums(nums) {}
+// class NumArray {
+// public:
+//     vector<int> prefixSum;  
+//      NumArray(vector<int>& nums) {
+//          prefixSum = vector<int>(nums.size() + 1, 0);
+
+//          for (int i = 0; i < nums.size(); ++i) {
+//              prefixSum[i + 1] = prefixSum[i] + nums[i];
+//         }
+//     }
+
+//      int sumRange(int left, int right) {
+//          return prefixSum[right + 1] - prefixSum[left];
+//     }
+// };
+
+
+class SegmentTree {
+private:
+    vector<int> segTree;
+    int n;
+
+    void build_Tree(vector<int>& nums, int i, int l, int r) {
+        if (l == r) {
+            segTree[i] = nums[l];
+            return;
+        }
+        int mid = (l + r) / 2;
+        build_Tree(nums, 2 * i + 1, l, mid);
+        build_Tree(nums, 2 * i + 2, mid + 1, r);
+        segTree[i] = segTree[2 * i + 1] + segTree[2 * i + 2];
+    }
+
+    void update(int i, int l, int r, int pos, int val) {
+        if (l == r) {
+            segTree[i] = val;
+            return;
+        }
+        int mid = (l + r) / 2;
+        if (pos <= mid)
+            update(2 * i + 1, l, mid, pos, val);
+        else
+            update(2 * i + 2, mid + 1, r, pos, val);
+        segTree[i] = segTree[2 * i + 1] + segTree[2 * i + 2];
+    }
+
+    int query(int i, int l, int r, int ql, int qr) {
+        if (qr < l || ql > r) return 0;
+        if (ql <= l && r <= qr) return segTree[i];
+        int mid = (l + r) / 2;
+        return query(2 * i + 1, l, mid, ql, qr) +
+               query(2 * i + 2, mid + 1, r, ql, qr);
+    }
+
+public:
+    SegmentTree(vector<int>& nums) {
+        n = nums.size();
+        segTree.resize(4 * n);
+        build_Tree(nums, 0, 0, n - 1);
+    }
+
+    void update(int pos, int val) {
+        update(0, 0, n - 1, pos, val);
+    }
+
+    int query(int l, int r) {
+        return query(0, 0, n - 1, l, r);
+    }
+};
+class NumArray {
+private:
+    SegmentTree* segTree;
+
+public:
+    NumArray(vector<int>& nums) {
+        segTree = new SegmentTree(nums);
+    }
+
+    // void update(int i, int val) {
+    //     segTree->update(i, val);
+    // }
 
     int sumRange(int left, int right) {
-        int sum = 0;
+        return segTree->query(left, right);
+    }
 
-        for (int i = left; i <= right; ++i) {
-            sum += nums[i];
-        }
-
-        return sum;
+    ~NumArray() {
+        delete segTree;
     }
 };
