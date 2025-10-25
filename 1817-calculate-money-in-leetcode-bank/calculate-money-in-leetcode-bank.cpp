60e03c5f52ef3f315af72eb5f3c799e6a30f3cb1
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-25 14:40:09 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (13.49%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int totalMoney = 0;
-        int mondayMoney = 1;  // Pehle Monday ko 1 rupay milenge
+        int total_money = 0;
+        int monday_money = 1;  // Pehle Monday ko 1 ₹ milenge
         
-        for (int day = 0; day < n; ++day) {
-            totalMoney += mondayMoney + (day % 7);  // Har din ka paisa add karna hai
+        for (int day = 0; day < n; day++) {
+            total_money += monday_money + (day % 7);  // Har din ka paisa add karna hai
             
             // Agar week ka last day (Sunday) aagaya, toh next Monday ke liye paisa badha do
             if (day % 7 == 6) {
-                ++mondayMoney;
+                monday_money++;
             }
         }
         
-        return totalMoney;  // Total jama kiya hua paisa return kar do
+        return total_money;  
     }
 };
