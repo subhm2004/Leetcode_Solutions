d6bd0d9cbeb61f3f2e99f4bc8924da384103ba71
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-10-29 14:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.95%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,10 +1,18 @@
+// class Solution {
+// public:
+//     int smallestNumber(int n) {
+//         int k = 1;
+//         while ((1 << k) - 1 < n) { // 2^k - 1
+//             k++;
+//         }
+//         return (1 << k) - 1;
+//     }
+// };
+
 class Solution {
 public:
     int smallestNumber(int n) {
-        int k = 1;
-        while ((1 << k) - 1 < n) { // 2^k - 1
-            k++;
-        }
-        return (1 << k) - 1;
+        int k = ceil(log2(n + 1));
+        return (1 << k) - 1; 
     }
 };
