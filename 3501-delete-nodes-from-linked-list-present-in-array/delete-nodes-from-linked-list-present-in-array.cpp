2fffe2b29a962251771c2d0be3a0fc3142cd18ea
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-11-01 12:52:18 +0530

    Time: 101 ms (51.85%) | Memory: 263.4 MB (41.15%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        // Set bnao jisme nums ke unique numbers honge
+        unordered_set<int> anokha(nums.begin(), nums.end());
+
+        // Dummy node bnao taaki head remove hone wali edge cases handle ho ske
+        ListNode* chutiya = new ListNode(0);
+        chutiya->next = head;
+        
+        ListNode* current = chutiya;
+
+        while (current->next) {
+            // Agar current node ka value unique_nums me hai toh usse skip karo
+            if (anokha.find(current->next->val) != anokha.end()) {
+                current->next = current->next->next;
+            } else {
+                // Agar nahi hai toh next node pe move karo
+                current = current->next;
+            }
+        }
+
+        // Modified list return karo
+        return chutiya->next;
+    }
+};
