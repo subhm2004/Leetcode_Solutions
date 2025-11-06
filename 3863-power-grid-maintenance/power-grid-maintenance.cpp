12ebf51005218fc6e05f6d1a3aabe04c8f1fd4ba
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-11-06 23:43:01 +0530

    Time: 587 ms (23.50%) | Memory: 432.7 MB (17.27%) - LeetSync

---
+++ 3863-power-grid-maintenance/power-grid-maintenance.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    //O(c + n)
+    void dfs(int node, unordered_map<int, vector<int>> &adj, int id, vector<int>& nodeId, 
+            unordered_map<int, set<int>>& mp, vector<bool>& visited) {
+        visited[node] = true;
+        mp[id].insert(node);
+        nodeId[node] = id;
+
+        for(int &ngbr : adj[node]) {
+            if(!visited[ngbr]) {
+                dfs(ngbr, adj, id, nodeId, mp, visited);
+            }
+        }
+    }
+
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
+
+        for(auto &edge :  connections) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> visited(c+1, false);
+        vector<int> nodeId(c+1);
+        //id -> {}
+        unordered_map<int, set<int>> mp;
+
+        //O(c+n)
+        for(int node = 1; node <= c; node++) {
+            if(!visited[node]) {
+                int id = node;
+                dfs(node, adj, id, nodeId, mp, visited);
+            }
+        }
+
+        vector<int> result;
+        //O(q * log(c))
+        for(auto &query : queries) {
+            int type = query[0]; //1, 2
+            int node = query[1]; //station/node
+
+            if(type == 1) {
+                int id = nodeId[node];
+                if(mp[id].count(node)) {
+                    result.push_back(node);
+                } else if(!mp[id].empty()) {
+                    result.push_back(*mp[id].begin());
+                } else {
+                    result.push_back(-1);
+                }
+            } else {
+                int id = nodeId[node];
+                mp[id].erase(node); //log(c)
+            }
+
+        }
+        return result;
+    }
+};
