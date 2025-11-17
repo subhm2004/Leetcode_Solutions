ad3eae12be91780c7b2ee5d1fdf0f63cb7a710fd
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-11-17 23:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (25.40%) - LeetSync

--- 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.cpp
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.cpp
@@ -1,9 +1,30 @@
+// class Solution {
+// public:
+//     int maximizeExpressionOfThree(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end(), greater<>());
+//         int a = nums[0], b = nums[1], c = nums[n-1];
+//         return a + b - c;
+//     }
+// };
+
 class Solution {
 public:
     int maximizeExpressionOfThree(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end(), greater<>());
-        int a = nums[0], b = nums[1], c = nums[n-1];
-        return a + b - c;
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;  
+        int min1 = INT_MAX;                 
+        
+        for (int x : nums) {
+            if (x > max1) {
+                max2 = max1;
+                max1 = x;
+            } else if (x > max2) {
+                max2 = x;
+            }
+            min1 = min(min1, x);
+        }
+        
+        return max1 + max2 - min1;
     }
 };
