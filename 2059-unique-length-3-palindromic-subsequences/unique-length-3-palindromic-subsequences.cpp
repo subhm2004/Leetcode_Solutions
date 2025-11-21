f3e620364861116719e55989a83ed892f6863081
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-11-22 00:06:16 +0530

    Time: 65 ms (88.32%) | Memory: 14.2 MB (91.69%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+ public:
+  int countPalindromicSubsequence(string s) {
+    int result = 0;
+    vector<int> first(26, -1);
+    vector<int> last(26, -1);
+
+    for (int i = 0; i < s.size(); ++i) {
+      int index = s[i] - 'a';
+      if (first[index] == -1) {
+        first[index] = i;
+      }
+      last[index] = i;
+    }
+
+    for (int i = 0; i < 26; ++i) {
+      if (first[i] != -1 && first[i] < last[i]) {
+        vector<bool> middle(26, false);
+        for (int j = first[i] + 1; j < last[i]; ++j) {
+          middle[s[j] - 'a'] = true;
+        }
+        int uniqueCount = 0;
+        for (int k = 0; k < 26; ++k) {
+          if (middle[k]) {
+            uniqueCount++;
+          }
+        }
+        result += uniqueCount;
+      }
+    }
+
+    return result;
+  }
+};
