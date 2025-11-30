31aad35c569349719422987a61a12809187e5d44
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-11-30 23:19:01 +0530

    Time: 55 ms (30.32%) | Memory: 10.2 MB (35.26%) - LeetSync

---
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int waviness(int x) {
+        string s = to_string(x);
+        int n = s.size();
+        if (n < 3) return 0;
+
+        int count = 0;
+
+        // check all middle digits
+        for (int i = 1; i + 1 < n; i++) {
+            int a = s[i-1] - '0';
+            int b = s[i] - '0';
+            int c = s[i+1] - '0';
+
+            if (b > a && b > c) count++;      // peak
+            else if (b < a && b < c) count++; // valley
+        }
+        return count;
+    }
+
+    int totalWaviness(int num1, int num2) {
+        int ans = 0;
+        for (int x = num1; x <= num2; x++) {
+            ans += waviness(x);
+        }
+        return ans;
+    }
+};
