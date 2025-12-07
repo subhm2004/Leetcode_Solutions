20235b5161094e7c385bfbad8607ab76f24ad172
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-07 16:07:15 +0530

    Time: 501 ms (64.00%) | Memory: 26.5 MB (67.49%) - LeetSync

--- 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.cpp
+++ 4085-largest-prime-from-consecutive-prime-sum/largest-prime-from-consecutive-prime-sum.cpp
@@ -5,7 +5,7 @@ public:
         if (n >= 0) isPrime[0] = false;
         if (n >= 1) isPrime[1] = false;
 
-        for (int i = 2; i * i <= n; ++i) {
+        for (int i = 2; i * i <= n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j <= n; j += i)
                     isPrime[j] = false;
@@ -22,7 +22,7 @@ public:
         long long sum = 0;
         int ans = 0;
 
-        for (int i = 2; i <= n; ++i) {
+        for (int i = 2; i <= n; i++) {
             if (isPrime[i]) {
                 sum += i;
 
