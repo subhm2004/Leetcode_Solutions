dfe94e58af45df09ba483525d9902a656661703a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-08 06:39:26 +0530

    Time: 3 ms (90.49%) | Memory: 9.1 MB (7.08%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.cpp
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int countTriples(int n) {
+        vector<int> squares(n + 1);
+        for (int i = 1; i <= n; ++i) {
+            squares[i] = i * i;
+        }
+
         int ans = 0;
 
-        for (int a = 1; a <= n; ++a) {
-            for (int b = 1; b <= n; ++b) {
-                int sum = a * a + b * b;
-                int c = sqrt(sum);
-                if (c * c == sum && c <= n) {
-                    ans++;
+        for (int c = 1; c <= n; ++c) {
+            int target = squares[c];
+            int left = 1, right = c - 1;
+
+            while (left <= right) {
+                int sum = squares[left] + squares[right];
+
+                if (sum == target) {
+                    ans += (left == right ? 1 : 2);
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
