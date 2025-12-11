c0d56026fdee590dbe589bd816a59515bc84035c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-12 02:31:08 +0530

    Time: 7 ms (68.58%) | Memory: 16.5 MB (15.95%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<long long> st;
+        long long curr_num = 0;
+        char last_operator = '+';    
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            if (isdigit(ch)) {
+                curr_num = curr_num * 10 + (ch - '0');
+            }
+
+            // If operator OR last character of string
+            if ((!isdigit(ch) && ch != ' ') || i == s.size() - 1) {
+                if (last_operator == '+') {
+                    st.push(curr_num);
+                }
+                else if (last_operator == '-') {
+                    st.push(-curr_num);
+                }
+                else if (last_operator == '*') {
+                    long long top = st.top(); 
+                    st.pop();
+                    st.push(top * curr_num);
+                }
+                else if (last_operator == '/') {
+                    long long top = st.top(); 
+                    st.pop();
+                    st.push(top / curr_num);  
+                }
+
+                last_operator = ch;   
+                curr_num = 0;       
+            }
+        }
+
+        // Final ans = sum of stack values
+        long long ans = 0;
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        return ans;
+    }
+};
