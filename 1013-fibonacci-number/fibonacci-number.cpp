edfbe3a5b0d444d97e0f4da7e6ef6ffd648b0ab9
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-13 01:18:16 +0530

    Time: 2 ms (76.21%) | Memory: 8.1 MB (20.25%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        double sqrt5 = sqrt(5);
-        double phi = (1 + sqrt5) / 2;
-        double psi = (1 - sqrt5) / 2;
-        return round((pow(phi, n) - pow(psi, n)) / sqrt5);
+        double sqrt_5 = sqrt(5);
+        double phi = (1 + sqrt_5) / 2;
+        double psi = (1 - sqrt_5) / 2;
+        return round((pow(phi, n) - pow(psi, n)) / sqrt_5);
     }
 };
