63458d4fb5a8fe0b2575cce3f03133dc5428afe4
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-17 14:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.05%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
+    // Har digit ka square sum
     int sumOfSquares(int n) {
         int sum = 0;
-        while (n) {
-            int digit = n % 10; // Last digit nikalna
-            sum += digit * digit; // Square sum me add karna
-            n /= 10; // Ek digit chhoti karna
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
         }
         return sum;
     }
 
     bool isHappy(int n) {
-        unordered_map<int, bool> visited; // Map to track visited numbers
+        int slow = n;
+        int fast = sumOfSquares(n);  // fast ko ek step aage se start
 
-        while (n != 1) {
-            if (visited[n]) // Agar pehle se present hai, toh cycle aagayi
-                return false;
-            
-            visited[n] = true; // Mark as visited
-            n = sumOfSquares(n); // Next number calculate karna
+        while (slow != fast) {
+            slow = sumOfSquares(slow);                // 1 step
+            fast = sumOfSquares(sumOfSquares(fast));  // 2 steps
         }
 
-        return true; // Agar 1 mil gaya toh happy number hai
+        return slow == 1;
     }
 };
-
-
-// class Solution {
-// public:
-//     // Har digit ka square sum calculate karne ka function
-//     int sumOfSquares(int n) {
-//         int sum = 0;
-//         while (n) {
-//             int digit = n % 10; // Last digit nikalna
-//             sum += digit * digit; // Square add karna
-//             n /= 10; // Ek digit chhoti karna
-//         }
-//         return sum;
-//     }
-
-//     bool isHappy(int n) {
-//         int slow = n, fast = n;
-
-//         do {
-//             slow = sumOfSquares(slow); // Slow ek step move karega
-//             fast = sumOfSquares(sumOfSquares(fast)); // Fast do step move karega
-//         } while (slow != fast); // Jab tak cycle detect nahi hoti
-
-//         return slow == 1; // Agar 1 mila toh happy number hai
-//     }
-// };
