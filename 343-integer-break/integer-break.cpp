93f14851336a89baaaa04bd5654b93d183b72aa6
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-17 15:27:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (25.67%) - LeetSync

---
+++ 343-integer-break/integer-break.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solveRE(int n) {
+        if (n <= 1)
+            return 0;
+        int maxProduct = 0;
+        //  splitting `n` into two parts: `i` and `n - i`
+        for (int i = 1; i < n; ++i) {
+            int product = max(i * (n - i), i * solveRE(n - i));
+            maxProduct = max(maxProduct, product);
+        }
+
+        return maxProduct;
+    }
+    int solveME(int n, vector<int>& dp) {
+        if (n <= 1)
+            return 0;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int maxProduct = 0;
+        for (int i = 1; i < n; ++i) {
+            int product = max(i * (n - i), i * solveME(n - i, dp));
+            maxProduct = max(maxProduct, product);
+        }
+
+        return dp[n] = maxProduct;
+    }
+    int integerBreak(int n) {
+        // return solveRE(n);
+
+        vector<int> dp(n + 1, -1);
+        return solveME(n, dp);
+    }
+};
