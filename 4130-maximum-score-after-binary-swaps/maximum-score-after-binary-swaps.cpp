6d4b71d8060e270bac881ea107eac3fd86f1864b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-24 03:07:55 +0530

    Time: 51 ms (65.48%) | Memory: 127.4 MB (89.54%) - LeetSync

--- 4130-maximum-score-after-binary-swaps/maximum-score-after-binary-swaps.cpp
+++ 4130-maximum-score-after-binary-swaps/maximum-score-after-binary-swaps.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     long long maximumScore(vector<int>& nums, string s) {
         int n = nums.size();
-        priority_queue<int> pq;   
+        priority_queue<int> pq;   // max-heap 
         long long score = 0;
 
         for(int i = 0; i < n; i++) {
-            pq.push(nums[i]);      
+            pq.push(nums[i]);     // index i tak jo values reachable hain, unhe heap me daal do
 
             if(s[i] == '1') {
-                score += pq.top();  
-                pq.pop();           
+                score += pq.top(); // is '1' ke liye sabse badi available value use kar li
+                pq.pop();          // ek '1' place ho gaya to hata de
             }
         }
 
         return score;
     }
 };
+/*
+Jab index i par '1' milta hai,
+tab heap me sirf wahi values hoti hain jo reachable hain (0…i)
+*/
