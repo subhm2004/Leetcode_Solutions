2eb8f68ec63e68539efcc60ad6ceeaf02225632a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2025-12-30 21:08:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (72.04%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     bool is_magic_square(vector<vector<int>>& grid, int i, int j) {
         int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2];
-        int nums[9] = {0};
+        int nums_map[9] = {0};
 
         for (int x = 0; x < 3; x++) {
             int row_sum = 0;
             int col_sum = 0;
             for (int y = 0; y < 3; y++) {
                 int val = grid[i + x][j + y];
-                if (val < 1 || val > 9 || nums[val - 1]++) {
+                if (val < 1 || val > 9 || nums_map[val - 1]++) {
                     return false;
                 }
-                row_sum += val;
-                col_sum += grid[i + y][j + x];
+                row_sum += val; // (i+x, j+y) → row traversal
+                col_sum += grid[i + y][j + x]; // (i+y, j+x) → column traversal
             }
             if (row_sum != sum || col_sum != sum) {
                 return false;
             }
         }
-        return grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2] == sum &&
-               grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j] == sum;
+        int diagonal = grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2];
+        int anti_diagonal = grid[i][j + 2] + grid[i + 1][j + 1] + grid[i + 2][j];
+
+        return diagonal == sum && anti_diagonal == sum ;
     }
 
     int numMagicSquaresInside(vector<vector<int>>& grid) {
