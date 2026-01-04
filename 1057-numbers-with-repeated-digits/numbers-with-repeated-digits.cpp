9ced2d755d23ebcdc11bb98cc494bc9725aed1a9
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-04 15:37:54 +0530

    Time: 7 ms (75.87%) | Memory: 8.8 MB (30.94%) - LeetSync

---
+++ 1057-numbers-with-repeated-digits/numbers-with-repeated-digits.cpp
@@ -0,0 +1,51 @@
+// Repeated = Total − Unique
+class Solution {
+public:
+    string s;
+    long long dp[20][2][1024][2];
+    // dp[idx][tight][mask][started]
+
+    long long solve(int idx, bool tight, int mask, bool started) {
+        // base case: saare digits process ho gaye
+        if (idx == s.size()) {
+            // agar number bana hai to valid
+            return started ? 1 : 0;
+        }
+        if (dp[idx][tight][mask][started] != -1) return dp[idx][tight][mask][started];
+
+        int limit = tight ? (s[idx] - '0') : 9;
+        long long ans = 0;
+
+        for (int d = 0; d <= limit; d++) {
+            bool updated_started = started || (d != 0);
+            bool updated_tight = tight && (d == limit);
+
+            // CASE 1: abhi tak number start nahi hua (leading zeros)
+            if (!updated_started) {
+                // leading zero hai, mask touch nahi karna
+                ans += solve(idx + 1, updated_tight, mask, false);
+            }
+            // CASE 2: number start ho chuka hai
+            else {
+                // agar digit repeat ho raha hai, skip
+                if (mask & (1 << d)) continue;
+
+                // digit ko mask me mark karke aage jao
+                ans += solve(idx + 1,updated_tight,mask | (1 << d), true);
+            }
+        }
+
+        return dp[idx][tight][mask][started] = ans;
+    }
+
+    int count_unique(int n) {
+        s = to_string(n);
+        memset(dp, -1, sizeof(dp));
+        return solve(0, true, 0, false);
+    }
+
+    int numDupDigitsAtMostN(int n) {
+        // total - unique = repeated
+        return n - count_unique(n);
+    }
+};
