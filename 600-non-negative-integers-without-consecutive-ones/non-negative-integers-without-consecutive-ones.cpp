94afe43c90886b289b9692f07f56e5b2c83d4380
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-04 22:45:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (99.63%) - LeetSync

---
+++ 600-non-negative-integers-without-consecutive-ones/non-negative-integers-without-consecutive-ones.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int dp[35][2][2];
+    string s;
+
+    int solve(int idx, bool tight, bool prev_one) {
+        if (idx >= s.size()) {
+            return 1; // valid number
+        }
+
+        if (dp[idx][tight][prev_one] != -1)
+            return dp[idx][tight][prev_one];
+
+        int limit = tight ? (s[idx] - '0') : 1;
+        int ans = 0;
+        for (int d = 0; d <= limit; d++) {
+            int updated_tight = tight && (d == limit);
+
+            // consecutive ones allowed nhi h
+            if (prev_one && d == 1)
+                continue;
+
+            ans += solve(idx + 1, updated_tight, d == 1);
+        }
+        return dp[idx][tight][prev_one] = ans;
+    }
+
+    int findIntegers(int n) {
+        // convert n to binary string s
+        s.clear();
+        while (n > 0) {
+            s.push_back((n & 1) + '0');
+            n >>= 1;
+        }
+        reverse(s.begin(), s.end());
+
+        memset(dp, -1, sizeof(dp));
+        return solve(0, true, false);
+    }
+};
