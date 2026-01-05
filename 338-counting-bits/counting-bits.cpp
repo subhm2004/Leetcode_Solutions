cfa568dc66885cf04ca101fae5ef719556f9d38e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-05 14:54:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (78.46%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -8,11 +8,13 @@ public:
         }
         return count;
     }
+
     vector<int> countBits(int n) {
         vector<int> ans(n + 1);
         for (int i = 0; i <= n; i++) {
             // ans[i] = __builtin_popcount(i);
-            ans[i] = count_set_bits(i);
+            // ans[i] = count_set_bits(i);
+            ans[i] = ans[i>>1] + (i&1);
         }
         return ans;
     }
