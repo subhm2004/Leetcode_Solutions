0fc57663f10ae1e1c9bafabd2a0a15888e33a0b8
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-05 14:52:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (34.43%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
+    int count_set_bits(int n){ // Brian Kernighan's Algorithm 
+        int count = 0;
+        while (n!= 0){
+            n = (n&(n-1)); // Remove Last Set Bit
+            count++;
+        }
+        return count;
+    }
     vector<int> countBits(int n) {
         vector<int> ans(n + 1);
         for (int i = 0; i <= n; i++) {
-            ans[i] = __builtin_popcount(i);
+            // ans[i] = __builtin_popcount(i);
+            ans[i] = count_set_bits(i);
         }
         return ans;
     }
