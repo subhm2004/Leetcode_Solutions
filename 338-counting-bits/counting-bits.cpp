283a7fc2ecceb64f1476709ed3dff329526fece2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-05 17:07:06 +0530

    Time: 2 ms (32.04%) | Memory: 10.5 MB (96.84%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -19,3 +19,38 @@ public:
         return ans;
     }
 };
+
+
+
+/*
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        
+        if(n == 0)
+            return ans;
+        
+        ans[1] = 1;
+        
+        for(int i = 2; i < n+1; i++) {
+            
+            if(i%2 == 0) {
+                ans[i] = ans[i/2];
+            } else {
+                ans[i] = ans[i/2] + 1;
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+
+
+
+*/
