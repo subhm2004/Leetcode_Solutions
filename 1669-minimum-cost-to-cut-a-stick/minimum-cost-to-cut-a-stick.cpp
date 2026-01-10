5b90de69455e4d724467ad0e0c5dd2f85372e384
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-10 16:51:38 +0530

    Time: 39 ms (30.11%) | Memory: 14.2 MB (10.28%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -7,16 +7,16 @@ public:
         // Base case: agar koi cut possible hi nahi
         if (j - i <= 1) return 0;
 
-        int mincost = INT_MAX;
+        int min_cost = INT_MAX;
 
         // i aur j ke beech wale har possible cut position try karo kyoki i or j pr to scale ki values hai or cuts ki postion start hoti h i+1 se j-1 tak 
         for (int k = i + 1; k < j; k++) {
             int cost = cuts[j] - cuts[i]; // current stick ka length
             int total_cost = /* Left scale */solveME(cuts, i, k) + /* Right scale */solveME(cuts, k, j) + cost;
-            mincost = min(mincost, total_cost);
+            min_cost = min(min_cost, total_cost);
         }
 
-        return mincost;
+        return min_cost;
     }
 
     int solveME(vector<int>& cuts, int i, int j) {
@@ -24,15 +24,15 @@ public:
 
         if (dp[i][j] != -1) return dp[i][j];
 
-        int mincost = INT_MAX;
+        int min_cost = INT_MAX;
 
         for (int k = i + 1; k < j; ++k) {
             int cost = cuts[j] - cuts[i]; 
             int total_cost = solveME(cuts, i, k) + solveME(cuts, k, j) + cost;
-            mincost = min(mincost, total_cost);
+            min_cost = min(min_cost, total_cost);
         }
 
-        return dp[i][j] = mincost;
+        return dp[i][j] = min_cost;
     }
 
     int minCost(int n, vector<int>& cuts) {
