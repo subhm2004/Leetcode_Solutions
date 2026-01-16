9ff35f257c1d06727d46b9aefabf878d145f6d00
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-16 14:58:57 +0530

    Time: 43 ms (11.98%) | Memory: 48.7 MB (11.91%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.cpp
@@ -1,3 +1,41 @@
+// DFS based serialize and deserialize (Pre Order)
+class Codec {
+public:
+    // DFS serialize
+    string serialize(TreeNode* root) {
+        if (!root) return "null,";
+        return to_string(root->val) + "," + serialize(root->left) + serialize(root->right);
+    }
+
+    // DFS deserialize helper
+    TreeNode* helper(queue<string>& q) {
+        string val = q.front(); q.pop();
+
+        if (val == "null") return nullptr;
+
+        TreeNode* node = new TreeNode(stoi(val));
+        node->left = helper(q);
+        node->right = helper(q);
+        return node;
+    }
+
+    TreeNode* deserialize(string data) {
+        queue<string> q;
+        string temp = "";
+
+        for (char c : data) {
+            if (c == ',') {
+                q.push(temp);
+                temp = "";
+            } else temp += c;
+        }
+
+        return helper(q);
+    }
+};
+/* Level Order serialize and deserialize
+
+
 class Codec {
 public:
     string serialize(TreeNode* root) {
@@ -73,3 +111,5 @@ public:
         return root;
     }
 };
+
+*/
