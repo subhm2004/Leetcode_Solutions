6c8925151002373b893e50a252a81bcf8096eef7
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-16 19:37:20 +0530

    Time: 5 ms (76.50%) | Memory: 181.4 MB (73.17%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -1,30 +1,63 @@
+// class Solution {
+// public:
+//     int ans = 0;
+
+//     void dfs(TreeNode* node, unordered_map<int, int>& freq) {
+//         if (!node)
+//             return;
+
+//         freq[node->val]++; // include current node
+
+//         // leaf node
+//         if (!node->left && !node->right) {
+//             int odd_freq_val = 0;
+//             for (auto& p : freq) {
+//                 if (p.second % 2 == 1)
+//                     odd_freq_val++;
+//             }
+//             if (odd_freq_val <= 1)
+//                 ans++;
+//         }
+
+//         dfs(node->left, freq);
+//         dfs(node->right, freq);
+
+//         freq[node->val]--; // backtrack
+//     }
+
+//     int pseudoPalindromicPaths(TreeNode* root) {
+//         unordered_map<int, int> freq;
+//         dfs(root, freq);
+//         return ans;
+//     }
+// };
+
+// Follow up aayega ki bit manipulation ka use kro mask use hoga
+// (can you get rid of passing the map)
+
 class Solution {
 public:
     int ans = 0;
-
-    void dfs(TreeNode* node, unordered_map<int,int>& freq) {
+    void dfs(TreeNode* node, int mask) {
         if (!node) return;
 
-        freq[node->val]++; // include current node
+        // flip bit for node->val
+        mask ^= (1 << node->val);
 
         // leaf node
         if (!node->left && !node->right) {
-            int odd = 0;
-            for (auto &p : freq) {
-                if (p.second % 2 == 1) odd++;
-            }
-            if (odd <= 1) ans++;
+            // check pseudo-palindromic ya fir seedha power of two check kr lo
+            if ((mask & (mask - 1)) == 0)
+                ans++;
+            // if ((__builtinpopcount(mask) == 1)
+            //     ans++;
         }
 
-        dfs(node->left, freq);
-        dfs(node->right, freq);
-
-        freq[node->val]--; // backtrack
+        dfs(node->left, mask);
+        dfs(node->right, mask);
     }
-
     int pseudoPalindromicPaths(TreeNode* root) {
-        unordered_map<int,int> freq;
-        dfs(root, freq);
+        dfs(root, 0);
         return ans;
     }
 };
