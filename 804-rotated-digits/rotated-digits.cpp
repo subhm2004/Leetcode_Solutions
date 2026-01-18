49ad871479642190eea768e859680047ff670ff3
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-18 17:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (71.45%) - LeetSync

--- 804-rotated-digits/rotated-digits.cpp
+++ 804-rotated-digits/rotated-digits.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    string s;
-    int dp[12][2][2][2]; 
-    // idx, tight, has_good, started
+    bool isGood(int n) {
+        bool changed = false;
 
-    bool bad_digit(int d) {
-        return d == 3 || d == 4 || d == 7 ;
-    }
+        while (n > 0) {
+            int d = n % 10;
 
-    bool good_digit(int d) {
-        return d == 2 || d == 5 || d == 6 || d == 9 ;
-    }
+            // invalid digits
+            if (d == 3 || d == 4 || d == 7)
+                return false;
 
-    int solve(int idx, bool tight, int has_good, bool started) {
-        if (idx == s.size()) {
-            // number should actually exist AND be good
-            return (started && has_good);
+            // digits that change after rotation
+            if (d == 2 || d == 5 || d == 6 || d == 9)
+                changed = true;
+
+            n /= 10;
         }
 
-        if (dp[idx][tight][has_good][started] != -1) return dp[idx][tight][has_good][started];
-
-        int ans = 0;
-        int limit = tight ? s[idx] - '0' : 9;
-
-        for (int d = 0; d <= limit; d++) {
-            int updated_tight = tight && (d == limit);
-            int updated_started = started || (d != 0);
-            int updated_has_good = has_good;
-
-            if (updated_started) {
-                if (bad_digit(d)) continue;
-                if (good_digit(d)) updated_has_good = 1;
-            }
-
-            ans += solve(idx + 1, updated_tight, updated_has_good, updated_started);
-        }
-        return dp[idx][tight][has_good][started] = ans;
+        return changed;
     }
 
     int rotatedDigits(int n) {
-        s = to_string(n);
-        memset(dp, -1, sizeof(dp));
-        return solve(0, true, 0, false);
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) count++;
+        }
+        return count;
     }
 };
+
