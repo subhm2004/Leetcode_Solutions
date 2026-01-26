b8e5141b91f8327518147285217553aeda39fa70
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-26 14:59:07 +0530

    Time: 20 ms (14.00%) | Memory: 36.8 MB (14.10%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        vector<vector<int>> ans;  
-        int minDiff = INT_MAX; 
-
-        // Sort kar rahe hain array ko taaki consecutive elements ke beech ka difference calculate kar sakein.
+        vector<vector<int>> ans;
+        int min_diff = INT_MAX;
         sort(arr.begin(), arr.end());
+        
+        for (int i = 1; i < arr.size(); i++) {
+            int diff = arr[i] - arr[i - 1];
 
-        for (int i = 1; i < arr.size(); ++i) {
-            int diff = arr[i] - arr[i - 1];  // consecutive elements ke beech ka difference
-
-            if (diff < minDiff) {
-                minDiff = diff;
+            if (diff < min_diff) {
+                min_diff = diff;
                 ans = {{arr[i - 1], arr[i]}};
             }
-            // Agar current difference minDiff ke barabar hai, toh uss pair ko ans mein add kar rahe hain
-            else if (diff == minDiff) {
+            
+            else if (diff == min_diff) {
                 ans.push_back({arr[i - 1], arr[i]});
             }
         }
-
-        return ans;  
+        
+        return ans;
     }
 };
