098d46634ca48930398542fecd67e66b4fbe1107
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-29 00:37:20 +0530

    Time: 3 ms (64.31%) | Memory: 8.2 MB (15.87%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,8 +1,42 @@
 class Solution {
+private:
+    const int MOD = 1e9 + 7;
+
+    // Multiply two n x n matrices
+    vector<vector<long long>> multiply(const vector<vector<long long>>& A,
+                                       const vector<vector<long long>>& B) {
+        int n = A.size();
+        vector<vector<long long>> ans(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                for (int k = 0; k < n; ++k)
+                    ans[i][j] = (ans[i][j] + A[i][k] * B[k][j]) % MOD;
+        return ans;
+    }
+
+    // Exponentiate an n x n matrix
+    vector<vector<long long>> matrixExpo(vector<vector<long long>> A,
+                                         int power) {
+        int n = A.size();
+        vector<vector<long long>> ans(n, vector<long long>(n, 0));
+        for (int i = 0; i < n; ++i)
+            ans[i][i] = 1;
+
+        while (power > 0) {
+            if (power % 2 == 1)
+                ans = multiply(ans, A);
+            A = multiply(A, A);
+            power /= 2;
+        }
+        return ans;
+    }
+
 public:
-    int fib(int N) {
-        const double sqrt_5 = sqrt(5);
-        const double phi = (1 + sqrt_5) / 2;
-        return (pow(phi, N) / sqrt_5) + 0.5;
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        vector<vector<long long>> base = {{1, 1}, {1, 0}};
+        vector<vector<long long>> result = matrixExpo(base, n - 1);
+        return result[0][0];
     }
 };
