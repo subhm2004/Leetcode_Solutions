978c048a4aa7979c29631ffcaa3f4da49b15be16
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-01-29 20:21:26 +0530

    Time: 188 ms (12.87%) | Memory: 56.7 MB (8.91%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,45 +1,62 @@
 class Solution {
 public:
-    using T = tuple<int, int, int>; // (effort, x, y)
+    int rows, cols;
+    vector<vector<int>> arr;
 
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int row = heights.size();
-        int col = heights[0].size();
+    vector<pair<int, int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-        vector<vector<int>> effort(row, vector<int>(col, INT_MAX));
+    bool is_possible(int max_effort) {
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        queue<pair<int, int>> q;
 
-        priority_queue<T, vector<T>, greater<T>> pq;
+        q.push({0, 0});
+        visited[0][0] = true;
 
-        pq.push({0, 0, 0});
-        effort[0][0] = 0;
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
 
-        vector<pair<int,int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+            if (x == rows - 1 && y == cols - 1)
+                return true;
 
-        while (!pq.empty()) {
-            auto [curr_effor, x, y] = pq.top();
-            pq.pop();
-
-            // Agar is cell ke liye already better effort mil chuka ho
-            if (curr_effor > effort[x][y]) continue;
-
-            // Destination pr aa gye hai 
-            if (x == row - 1 && y == col - 1)
-                return curr_effor;
-
-            for (auto [dx, dy] : dir) {
+            for (auto [dx, dy] : dirs) {
                 int nx = x + dx;
                 int ny = y + dy;
 
-                if (nx >= 0 && ny >= 0 && nx < row && ny < col) {
-                    int new_effort = max(curr_effor, abs(heights[x][y] - heights[nx][ny]));
+                if (nx >= 0 && ny >= 0 && nx < rows && ny < cols &&
+                    !visited[nx][ny]) {
+                        
+                    int diff = abs(arr[x][y] - arr[nx][ny]);
 
-                    if (new_effort < effort[nx][ny]) {
-                        effort[nx][ny] = new_effort;
-                        pq.push({new_effort, nx, ny});
+                    if (diff <= max_effort) {
+                        visited[nx][ny] = true;
+                        q.push({nx, ny});
                     }
                 }
             }
         }
-        return 0;
+        return false;
+    }
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        this->arr = heights;
+        rows = arr.size();
+        cols = arr[0].size();
+
+        int low = 0;
+        int high = 1e6;
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (is_possible(mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
