66ca789bc28ff11b2b9d77dde4c1f3644e80cdf2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-01 13:51:53 +0530

    Time: 1011 ms (28.57%) | Memory: 110.6 MB (52.38%) - LeetSync

---
+++ 4121-minimum-partition-score/minimum-partition-score.cpp
@@ -0,0 +1,117 @@
+class Solution {
+public:
+    vector<long long> pref;
+    int n;
+    long long INF = 1e18;
+    vector<vector<long long>> dp;
+
+    long long value(long long s) { return s * (s + 1) / 2; }
+
+    long long solveRE(int i, int parts) {
+
+        if (i == n && parts == 0)
+            return 0;
+
+        if (i == n || parts == 0)
+            return INF;
+
+        long long ans = INF;
+        /*
+        Hum current subarray ko [ i ... idx ] maan rahe hain.
+        Matlab:
+        - idx = current subarray ka last index hai
+        - next subarray hamesha idx + 1 se start hoga
+        Array me total elements = n
+        Current subarray [ i ... idx ] banane ke baad
+        bache hue elements honge:
+
+        index (idx + 1) se lekar (n - 1) tak
+        Remaining elements ki count:
+
+        (n - 1) - (idx + 1) + 1
+        = n - idx - 1
+        = n - (idx + 1)
+
+        Ab hume abhi (parts - 1) subarrays aur banane hain
+        aur har subarray non-empty hona chahiye
+
+        Isliye condition hogi:
+
+        remaining elements >= remaining subarrays
+
+        Matlab:
+
+        n - (idx + 1) >= (parts - 1)
+
+        Ab isko simplify karte hain:
+
+        n - idx - 1 >= parts - 1
+        n - idx     >= parts
+        idx         <= n - parts
+
+        Isi wajah se loop aisa likha hai:
+
+        for (int idx = i; idx <= n - parts; idx++)
+
+*/
+        for (int idx = i; idx <= n - parts; idx++) {
+        /*
+          current subarray = [ i ... idx ]
+          next call = solveME(idx+1, parts-1)
+        */
+            long long sum_arr = pref[idx + 1] - pref[i];
+            long long cost = value(sum_arr);
+            if (cost >= ans) 
+                break;
+
+            long long next = solveRE(idx + 1, parts - 1);
+
+            ans = min(ans, cost + next);
+        }
+
+        return ans;
+    }
+
+    long long solveME(int i, int parts) {
+
+        if (i == n && parts == 0)
+            return 0;
+
+        if (i == n || parts == 0)
+            return INF;
+
+        if (dp[i][parts] != -1)
+            return dp[i][parts];
+
+        long long ans = INF;
+
+        for (int idx = i; idx <= n - parts; idx++) {
+
+            long long sum_arr = pref[idx + 1] - pref[i];
+            long long cost = value(sum_arr);
+
+            if (cost >= ans) 
+                break;
+
+            long long next = solveME(idx + 1, parts - 1);
+
+            ans = min(ans, cost + next);
+        }
+
+        return dp[i][parts] = ans;
+    }
+
+
+    long long minPartitionScore(vector<int>& nums, int k) {
+        n = nums.size();
+
+        pref.assign(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i] + nums[i];
+        }
+        dp.assign(n, vector<long long>(k + 1, -1));
+
+        return solveME(0, k);
+    }
+};
