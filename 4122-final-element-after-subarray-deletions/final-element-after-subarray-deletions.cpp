db04d4ebe0dc0a7b3e434027eb6fe5e87d323bbf
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-01 21:08:40 +0530

    Time: 0 ms (100.00%) | Memory: 179.8 MB (84.27%) - LeetSync

--- 4122-final-element-after-subarray-deletions/final-element-after-subarray-deletions.cpp
+++ 4122-final-element-after-subarray-deletions/final-element-after-subarray-deletions.cpp
@@ -2,8 +2,26 @@ class Solution {
 public:
     int finalElement(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1)
-            return nums[0];
+        /*
+        important observation
+
+        Kisi bhi turn pe (jab current array ka length m ≥ 2 ho):
+
+        current player chahe to game turant khatam kar sakta hai
+        by removing a subarray of length m-1.
+
+        Aur dhyaan do:
+
+        length m-1 ka subarray sirf 2 hi tarah se ho sakta hai:
+
+        [0 … m-2]  -> last element bachta hai
+        [1 … m-1]  -> first element bachta hai
+
+        Iska matlab:
+
+        jab bhi game khatam hota hai,
+        sirf current array ka first ya last element hi final me bachta hai.
+        */
         return max(nums[0], nums[n - 1]);
     }
 };
