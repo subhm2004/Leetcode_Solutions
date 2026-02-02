65009ca749925fa582399486c80bc6e7b2575483
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-02 18:16:31 +0530

    Time: 11 ms (60.24%) | Memory: 9.5 MB (62.99%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -2,46 +2,38 @@ class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
 
-        int left = 0, right = s.size() - 1;
-        int moves = 0;
+        int n = s.size();
+        int left = 0, right = n - 1;
+        int ans = 0;
 
-        // Process string from both ends
         while (left < right) {
 
-            // If already matching, move both pointers
-            if (s[left] == s[right]) {
-                left++;
-                right--;
+            int l = left;
+            int r = right;
+
+            // find matching character for s[l] from the right side
+            while (l < r && s[l] != s[r])
+                r--;
+
+            // no matching character found (this is the middle one)
+            if (l == r) {
+                swap(s[r], s[r + 1]);
+                ans++;
                 continue;
             }
-
-            // Find matching character for s[left] from the right side
-            int match_pos = right;
-            while (match_pos > left && s[match_pos] != s[left]) {
-                match_pos--;
+            else {
+                // bring s[r] to position 'right'
+                while (r < right) {
+                    swap(s[r], s[r + 1]);
+                    ans++;
+                    r++;
+                }
             }
 
-            // If no matching character found on the right
-            // then this character must go to the middle
-            if (match_pos == left) {
-                swap(s[left], s[left + 1]);
-                moves++;
-                continue;
-            }
-
-            // Bring the matching character to position 'right'
-            // using adjacent swaps
-            while (match_pos < right) {
-                swap(s[match_pos], s[match_pos + 1]);
-                moves++;
-                match_pos++;
-            }
-
-            // Current pair is fixed
             left++;
             right--;
         }
 
-        return moves;
+        return ans;
     }
 };
