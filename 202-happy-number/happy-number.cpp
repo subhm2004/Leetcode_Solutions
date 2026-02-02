ca852d5625d5995bd92de007f3d0587a95aad5cf
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-03 04:27:32 +0530

    Time: 1 ms (27.71%) | Memory: 8.5 MB (19.63%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -1,25 +1,52 @@
 class Solution {
 public:
-    // Har digit ka square sum
     int sumOfSquares(int n) {
         int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
+        while (n) {
+            int digit = n % 10; 
+            sum += digit * digit;  
+            n /= 10;  
         }
         return sum;
     }
 
     bool isHappy(int n) {
-        int slow = n;
-        int fast = sumOfSquares(n);  // fast ko ek step aage se start
+        unordered_map<int, bool> visited;
 
-        while (slow != fast) {
-            slow = sumOfSquares(slow);                // 1 step
-            fast = sumOfSquares(sumOfSquares(fast));  // 2 steps
+        while (n != 1) {
+            if (visited[n]) 
+                return false;
+            
+            visited[n] = true;
+            n = sumOfSquares(n); 
         }
 
-        return slow == 1;
+        return true;  
     }
 };
+
+
+// class Solution {
+// public:
+//     // Har digit ka square sum calculate karne ka function
+//     int sumOfSquares(int n) {
+//         int sum = 0;
+//         while (n) {
+//             int digit = n % 10; // Last digit nikalna
+//             sum += digit * digit; // Square add karna
+//             n /= 10; // Ek digit chhoti karna
+//         }
+//         return sum;
+//     }
+
+//     bool isHappy(int n) {
+//         int slow = n, fast = n;
+
+//         do {
+//             slow = sumOfSquares(slow); // Slow ek step move karega
+//             fast = sumOfSquares(sumOfSquares(fast)); // Fast do step move karega
+//         } while (slow != fast); // Jab tak cycle detect nahi hoti
+
+//         return slow == 1; // Agar 1 mila toh happy number hai
+//     }
+// };
