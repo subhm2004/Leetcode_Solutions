cb9e7a63dd55ef06c088c10bee054864dac9c4ec
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-02 18:02:33 +0530

    Time: 7 ms (86.61%) | Memory: 9.5 MB (82.68%) - LeetSync

--- 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
+++ 1356-minimum-number-of-moves-to-make-palindrome/minimum-number-of-moves-to-make-palindrome.cpp
@@ -1,34 +1,43 @@
 class Solution {
 public:
     int minMovesToMakePalindrome(string s) {
-        int n = s.size();
-        int left = 0, right = n - 1;
+
+        int left = 0, right = s.size() - 1;
         int moves = 0;
 
+        // Process string from both ends
         while (left < right) {
+
+            // If already matching, move both pointers
             if (s[left] == s[right]) {
                 left++;
                 right--;
                 continue;
             }
 
+            // Find matching character for s[left] from the right side
             int match_pos = right;
             while (match_pos > left && s[match_pos] != s[left]) {
                 match_pos--;
             }
 
+            // If no matching character found on the right
+            // then this character must go to the middle
             if (match_pos == left) {
                 swap(s[left], s[left + 1]);
                 moves++;
                 continue;
             }
 
+            // Bring the matching character to position 'right'
+            // using adjacent swaps
             while (match_pos < right) {
                 swap(s[match_pos], s[match_pos + 1]);
                 moves++;
                 match_pos++;
             }
 
+            // Current pair is fixed
             left++;
             right--;
         }
@@ -36,4 +45,3 @@ public:
         return moves;
     }
 };
-
