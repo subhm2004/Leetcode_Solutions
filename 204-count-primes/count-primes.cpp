c25d4521b22486ab2c174ea218638e5d6e90b285
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-04 03:05:56 +0530

    Time: 189 ms (48.08%) | Memory: 12.7 MB (39.54%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -21,7 +21,7 @@ public:
         int count = 0;
         for (int i = 2; i < n; ++i) {
             if (isPrime[i])
-                ++count;
+                count++;
         }
         return count;
     }
