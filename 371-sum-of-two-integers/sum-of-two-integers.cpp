504ae59cd4a2eb234371151753b9827a2fc2fa3c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-05 00:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (1.76%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
-    int solve(int a, int b) {
+    int getSum(int a, int b) {
         if (b == 0)
             return a;
-        return solve(a ^ b, (a & b) << 1);
+        return getSum(a ^ b, (a & b) << 1);
     }
-    int getSum(int a, int b) { return solve(a, b); }
 };
