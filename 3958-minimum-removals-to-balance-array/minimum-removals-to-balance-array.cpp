5e2c273c8a30306f8324ad3a9b5d501bc9079d8f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-06 15:29:03 +0530

    Time: 48 ms (21.22%) | Memory: 105 MB (52.91%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.cpp
@@ -1,22 +1,52 @@
+// // Sorting + Sliding Window ( minimium removal or deletion = total length - maximum balanced)
+// class Solution { 
+// public:
+//     int minRemoval(vector<int>& nums, int k) {
+
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+
+//         int left = 0;
+//         int max_len = 1;
+
+//         for (int right = 0; right < n; right++) {
+
+//             while (nums[right] > 1LL * nums[left] * k) {
+//                 left++;
+//             }
+
+//             max_len = max(max_len, right - left + 1);
+//         }
+
+//         return n - max_len;
+//     }
+// };
+
+// Binary Seach + Sorting 
 class Solution {
 public:
-    int minRemoval(vector<int>& nums, int k) {
+    int minRemoval(vector<int>& a, int k) {
+        int n = a.size();
+        sort(a.begin(), a.end());
+        int maxi = 1;
 
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            long long limit = 1LL * a[i] * k;
+            int l = i;
+            int r = n - 1;
+            int j = i;
 
-        int left = 0;
-        int max_len = 1;
-
-        for (int right = 0; right < n; right++) {
-
-            while (nums[right] > 1LL * nums[left] * k) {
-                left++;
+            while (l <= r) {
+                int mid = (l + r) >> 1;
+                if (a[mid] <= limit) {
+                    j = mid;
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
             }
-
-            max_len = max(max_len, right - left + 1);
+            maxi = max(maxi, j - i + 1);
         }
-
-        return n - max_len;
+        return n - maxi;
     }
 };
