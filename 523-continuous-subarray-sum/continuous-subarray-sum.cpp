6297a95fb30a27859642ed85012b8e6b902f3921
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-13 15:41:27 +0530

    Time: 83 ms (27.53%) | Memory: 146.2 MB (82.20%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,36 +1,13 @@
-// class Solution {                   TLE 94/101
-// public:
-//     bool checkSubarraySum(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         for (int i = 0; i < n; ++i) {
-//             int sum = 0;
-//             for (int j = i; j < n; ++j) {
-//                 sum += nums[j];
-//                 if (j - i + 1 >= 2) {
-//                     if (k == 0) {
-//                         if (sum == 0)
-//                             return true;
-//                     } else {
-//                         if (sum % k == 0)
-//                             return true;
-//                     }
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// };
-
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int sum = 0;
 
         // Yeh map store karega: remainder -> uska pehla index
-        unordered_map<int, int> remainder_Index;
+        unordered_map<int, int> remainder_idx;
 
         // Jab remainder 0 ho, toh iska matlab subarray start se hi valid ho sakta hai
-        remainder_Index[0] = -1;
+        remainder_idx[0] = -1;
 
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
@@ -40,15 +17,15 @@ public:
                 sum %= k;
 
             // Agar yeh remainder pehle se map mein hai
-            if (remainder_Index.count(sum)) {
-                int prevIndex = remainder_Index[sum];
+            if (remainder_idx.count(sum)) {
+                int prev_idx = remainder_idx[sum];
 
                 // Check karo ki length kam se kam 2 ho
-                if (i - prevIndex > 1)
+                if (i - prev_idx > 1)
                     return true;
             } else {
                 // Agar yeh remainder pehli baar aaya hai, toh store karo
-                remainder_Index[sum] = i;
+                remainder_idx[sum] = i;
             }
         }
 
