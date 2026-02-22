4da119c6de89266f6b9bc6d50cfe1953463e9c46
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-22 19:52:52 +0530

    Time: 616 ms (5.29%) | Memory: 236.7 MB (62.91%) - LeetSync

---
+++ 3737-paint-house-iv/paint-house-iv.cpp
@@ -0,0 +1,64 @@
+using ll = long long;
+class Solution {
+public:
+    const ll INF = 1e18;
+
+    ll dp[100005][4][4]; // dp[idx][prevL][prevR]
+    int n;
+    vector<vector<int>> cost;
+
+    // idx = current pair index
+    // prevL = previous left color
+    // prevR = previous right color
+    ll solve(int idx, int prevL, int prevR) {
+
+        // sare pair processed ho gye hai
+        if (idx == n / 2)
+            return 0;
+
+        if (dp[idx][prevL][prevR] != -1)
+            return dp[idx][prevL][prevR];
+
+        ll ans = INF;
+
+        int L = idx;
+        int R = n - 1 - idx;
+
+        // try krte h sare color for left house
+        for (int c1 = 1; c1 <= 3; c1++) {
+
+            // adjacent condition (left side)
+            if (c1 == prevL)
+                continue;
+
+            // try krte h sare color for right house
+            for (int c2 = 1; c2 <= 3; c2++) {
+
+                // adjacent condition (right side)
+                if (c2 == prevR)
+                    continue;
+
+                // equidistant condition (same color nhi hona chahiye)
+                if (c1 == c2)
+                    continue;
+
+                ll curr_cost =
+                    cost[L][c1 - 1] + cost[R][c2 - 1] + solve(idx + 1, c1, c2);
+
+                ans = min(ans, curr_cost);
+            }
+        }
+
+        return dp[idx][prevL][prevR] = ans;
+    }
+
+    long long minCost(int n, vector<vector<int>>& cost) {
+
+        this->n = n;
+        this->cost = cost;
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, 0, 0);
+    }
+};
