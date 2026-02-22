d318929c824477f8cd205d685ae3966fad702864
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-22 12:44:49 +0530

    Time: 38 ms (72.48%) | Memory: 14.5 MB (52.71%) - LeetSync

--- 1343-dice-roll-simulation/dice-roll-simulation.cpp
+++ 1343-dice-roll-simulation/dice-roll-simulation.cpp
@@ -2,38 +2,42 @@ class Solution {
 public:
     static const int MOD = 1e9 + 7;
 
-    long long dp[5001][7][16];
+    long long dp[5001][7][16]; // dp[rolls_left][last_face][streak]
+
+    // Har step par hume 3 cheezein pata honi chahiye:
+    // Kitne rolls abhi baaki hain :- rolls_left
+    // Last kaunsa face aaya tha :- last_face
+    // Wo face kitni baar continuous chal raha hai :- streak
+
     vector<int> rollMax;
 
-    long long solve(int rollsLeft, int lastFace, int streak) {
+    long long solve(int rolls_left, int last_face, int streak) {
 
         // base case
-        if (rollsLeft == 0) return 1;
+        // Agar rolls khatam ho gaye mtlb ek valid sequence mil gaya.
+        if (rolls_left == 0)
+            return 1;
 
-        // memoization check
-        if (dp[rollsLeft][lastFace][streak] != -1)
-            return dp[rollsLeft][lastFace][streak];
+        if (dp[rolls_left][last_face][streak] != -1)
+            return dp[rolls_left][last_face][streak];
 
         long long ways = 0;
 
-        // try every face
+        // try karte hai sare face ko
         for (int face = 1; face <= 6; face++) {
 
-            if (face == lastFace) {
+            if (face == last_face) {
                 // same face → check streak limit
                 if (streak < rollMax[face - 1]) {
-                    ways = (ways +
-                            solve(rollsLeft - 1, face, streak + 1)) % MOD;
+                    ways = (ways + solve(rolls_left - 1, face, streak + 1)) % MOD;
                 }
-            }
-            else {
+            } else {
                 // new face → streak = 1
-                ways = (ways +
-                        solve(rollsLeft - 1, face, 1)) % MOD;
+                ways = (ways + solve(rolls_left - 1, face, 1)) % MOD;
             }
         }
 
-        return dp[rollsLeft][lastFace][streak] = ways;
+        return dp[rolls_left][last_face][streak] = ways;
     }
 
     int dieSimulator(int n, vector<int>& rollMax) {
