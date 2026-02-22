0b62a8cd877d30b88e660ea72aff26d1d08a4a59
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-22 12:54:01 +0530

    Time: 269 ms (46.40%) | Memory: 23.8 MB (48.00%) - LeetSync

--- 2404-number-of-distinct-roll-sequences/number-of-distinct-roll-sequences.cpp
+++ 2404-number-of-distinct-roll-sequences/number-of-distinct-roll-sequences.cpp
@@ -3,25 +3,28 @@ public:
     const int MOD = 1e9 + 7;
     int dp[10001][7][7][7];
 
-
     int gcd(int a, int b) {
-        if (b == 0) return a;
+        if (b == 0)
+            return a;
         return gcd(b, a % b);
     }
 
-     int solveRE(int i, int prev, int prev2, int prev3, int n) {
+    int solveRE(int i, int prev, int prev2, int prev3, int n) {
         // Base case: agar sequence ki length n tak pahuch gayi, toh yeh valid hai
         if (i == n) return 1;
 
         int total = 0;
 
         // Har position ke liye 1 se 6 tak ke dice face try karo
-        for (int face = 1; face <= 6; ++face) {
+        for (int face = 1; face <= 6; face++) {
             // Constraint 1: same number pichle 2 steps mein nahi aana chahiye
-            if (face == prev || face == prev2) continue;
+            if (face == prev || face == prev2)
+                continue;
 
-            // Constraint 2: current face aur previous face ka GCD 1 hona chahiye
-            if (prev != 0 && gcd(face, prev) != 1) continue;
+            // Constraint 2: current face aur previous face ka GCD 1 hona
+            // chahiye
+            if (prev != 0 && gcd(face, prev) != 1)
+                continue;
 
             // Agar face valid hai, toh next index ke liye recursion call karo
             total = (total + solveRE(i + 1, face, prev, prev2, n)) % MOD;
@@ -31,7 +34,8 @@ public:
     }
 
     int solveME(int i, int prev, int prev2, int prev3, int n) {
-        if (i == n) return 1;
+        if (i == n)
+            return 1;
 
         if (dp[i][prev][prev2][prev3] != -1)
             return dp[i][prev][prev2][prev3];
@@ -39,17 +43,18 @@ public:
         int total = 0;
 
         for (int face = 1; face <= 6; ++face) {
-            if (face == prev || face == prev2) continue;
-            if (prev != 0 && gcd(face, prev) != 1) continue;
-            
+            if (face == prev || face == prev2)
+                continue;
+            if (prev != 0 && gcd(face, prev) != 1)
+                continue;
+
             total = (total + solveME(i + 1, face, prev, prev2, n)) % MOD;
         }
 
         return dp[i][prev][prev2][prev3] = total;
     }
 
-
-     int distinctSequences(int n) {
+    int distinctSequences(int n) {
         // return solveRE(0, 0, 0, 0, n);
         memset(dp, -1, sizeof(dp));
         return solveME(0, 0, 0, 0, n);
