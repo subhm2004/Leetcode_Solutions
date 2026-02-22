70afda95de760c805d0846b1f55b4b2279b2aa33
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-22 17:35:52 +0530

    Time: 1 ms (60.04%) | Memory: 9.7 MB (32.96%) - LeetSync

---
+++ 4226-check-digitorial-permutation/check-digitorial-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isDigitorialPermutation(int n) {
+        vector<int> digitorial = {1, 2, 145, 40585};
+
+        string target = to_string(n);
+        sort(target.begin(), target.end());
+
+        for (int x : digitorial)
+        {
+            string curr = to_string(x);
+
+            // same number of digits required
+            if (curr.size() != target.size()) continue;
+
+            sort(curr.begin(), curr.end());
+
+            if (curr == target)
+                return true;
+        }
+
+        return false;
+    }
+};
