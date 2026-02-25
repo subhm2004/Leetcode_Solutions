ee04200c0ca19d7c2a964811546efd6fc423bf37
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-25 18:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.11%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,24 +1,31 @@
 class kadan {
 public:
-    int maxSubarraySum(vector<int>& nums) {
-        int n = nums.size();
+    int max_Subarray_Sum(vector<int>& nums) {
         int maxi = INT_MIN;
         int sum = 0;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < nums.size(); i++) {
             sum += nums[i];
             maxi = max(maxi, sum);
             if (sum < 0) sum = 0;
         }
-
         return maxi;
     }
-};
 
-class Solution : public kadan {
+    int min_Subarray_Sum(vector<int>& nums) {
+        int mini = INT_MAX;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            mini = min(mini, sum);
+            if (sum > 0) sum = 0;
+        }
+        return mini;
+    }
+};
+class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Directly calling inherited method
-        return maxSubarraySum(nums);
+        kadan kd;
+        return kd.max_Subarray_Sum(nums);
     }
 };
