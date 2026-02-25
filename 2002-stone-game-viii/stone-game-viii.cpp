67fdb20c86be8c211ddf71a4754c4c0582f89dab
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-25 19:03:09 +0530

    Time: 126 ms (11.51%) | Memory: 98.4 MB (25.18%) - LeetSync

---
+++ 2002-stone-game-viii/stone-game-viii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> prefix;
+    int dp[100005];
+    int n;
+
+    int solve(int i) {
+
+        if (i == n)
+            return prefix[n];
+
+        if (dp[i] != -1)
+            return dp[i];
+        
+        // two choices
+
+        int include = prefix[i] - solve(i + 1);
+        int exclude = solve(i + 1);
+        return dp[i] = max(include, exclude);
+    }
+
+    int stoneGameVIII(vector<int>& stones) {
+
+        n = stones.size();
+
+        // 1-based prefixix sum
+        prefix.assign(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+            prefix[i] = prefix[i - 1] + stones[i - 1];
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(2); // first move minimum 2 stones
+    }
+};
