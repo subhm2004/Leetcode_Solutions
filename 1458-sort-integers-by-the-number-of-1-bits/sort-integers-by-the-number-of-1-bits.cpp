e5a9c625c38ca9bf775f6d27b29a1d2a70b60df1
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-25 13:27:50 +0530

    Time: 7 ms (19.88%) | Memory: 14.3 MB (58.88%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,34 +1,19 @@
 class Solution {
  public:
+    int count_set_Bits(int n) { // Brian Kernighan’s Algorithm
+    int count = 0;
+    while (n) {
+        n = n & (n - 1);
+        count++;
+    }
+    return count;
+}
   vector<int> sortByBits(vector<int>& arr) {
-    sort(arr.begin(), arr.end(), [](int a, int b) {
-      int bitsA = __builtin_popcount(a); // Count 1s in binary representation of 'a'
-      int bitsB = __builtin_popcount(b); // Count 1s in binary representation of 'b'
+    sort(arr.begin(), arr.end(), [this](int a, int b) { 
+    int bitsA = count_set_Bits(a);  
+    int bitsB = count_set_Bits(b);
       return bitsA == bitsB ? a < b : bitsA < bitsB;
     });
     return arr;
   }
 };
-
-// class Solution {
-//  public:
-//   vector<int> sortByBits(vector<int>& arr) {
-//     auto countBits = [](int n) {
-//       int count = 0;
-//       while (n) {
-//         count += (n & 1); // Check if the last bit is 1
-//         n >>= 1;          // Right shift to check the next bit
-//       }
-//       return count;
-//     };
-
-//     sort(arr.begin(), arr.end(), [&](int a, int b) {
-//       int bitsA = countBits(a);
-//       int bitsB = countBits(b);
-//       return bitsA == bitsB ? a < b : bitsA < bitsB;
-//     });
-
-//     return arr;
-//   }
-// };
-
