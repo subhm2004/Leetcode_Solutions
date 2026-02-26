dd4a083e22da54b5ab20612fec50c7dd6a249c3b
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-27 01:43:27 +0530

    Time: 19 ms (56.93%) | Memory: 81.2 MB (80.60%) - LeetSync

---
+++ 1896-maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int dp[305][305];
+    vector<int> nums, mult;
+    int n, m;
+
+    int solve(int i, int l) {
+
+        // all multipliers used
+        if (i == m)
+            return 0;
+
+        if (dp[i][l] != -1)
+            return dp[i][l];
+
+        int r = n - 1 - (i - l);
+
+        // choose left
+        int take_left = mult[i] * nums[l] + solve(i + 1, l + 1);
+
+        // choose right
+        int take_right = mult[i] * nums[r] + solve(i + 1, l);
+
+        return dp[i][l] = max(take_left, take_right);
+    }
+
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        this-> nums = nums;
+        mult = multipliers;
+
+        n = nums.size();
+        m = mult.size();
+
+        memset(dp, -1, sizeof(dp));
+
+        return solve(0, 0);
+    }
+};
+
