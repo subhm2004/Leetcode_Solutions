7d48b41e19952296ffe203a1297cb5ebf9f3dfa5
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-02-28 21:40:26 +0530

    Time: 115 ms (13.50%) | Memory: 22.3 MB (9.82%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[10005];
+    vector<int> arr;   
+
+    int solveME(int i) {
+        int n = arr.size();
+
+        if (i >= n - 1)
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int min_jumps = INT_MAX;
+        int max_reach_limit = min(i + arr[i], n - 1);
+
+        for (int next = i + 1; next <= max_reach_limit; next++) {
+            int jumps = solveME(next);
+
+            if (jumps != INT_MAX)
+                min_jumps = min(min_jumps, jumps + 1);
+        }
+
+        return dp[i] = min_jumps;
+    }
+
+    int jump(vector<int>& nums) {
+        arr = nums;                  
+        memset(dp, -1, sizeof(dp));   
+        return solveME(0);
+    }
+};
