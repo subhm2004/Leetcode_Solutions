49e720c60734305fd87e4569c9fda5be1d8903bb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-05 15:09:19 +0530

    Time: 3 ms (53.48%) | Memory: 26.4 MB (16.34%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,46 +1,19 @@
-class Solution {    // Moore Voting Algorithm 
+class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int candidate_1 = 0, candidate_2 = 1;
-        int count_1 = 0, count_2 = 0;
         int n = nums.size();
 
-        // First pass: find potential candidates
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == candidate_1) {
-                count_1++;
-            } else if (nums[i] == candidate_2) {
-                count_2++;
-            } else if (count_1 == 0) {
-                candidate_1 = nums[i];
-                count_1 = 1;
-                continue;
-            } else if (count_2 == 0) {
-                candidate_2 = nums[i];
-                count_2 = 1;
-                continue;
-            } else {
-                count_1--;
-                count_2--;
-            }
-        }
-
-        // Second pass: verify candidates
-        count_1 = 0;
-        count_2 = 0;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == candidate_1) {
-                count_1++;
-            } else if (nums[i] == candidate_2) {
-                count_2++;
-            }
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
         }
 
         vector<int> ans;
-        if (count_1 > n / 3) ans.push_back(candidate_1);
-        if (count_2 > n / 3) ans.push_back(candidate_2);
-
+        for (auto& pair : freq) {
+            if (pair.second > n / 3) {
+                ans.push_back(pair.first);
+            }
+        }
         return ans;
     }
 };
-
