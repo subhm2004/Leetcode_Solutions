5f1ee98c4c88e3e64ed6ce35446b9cf77df43dc2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-07 00:42:06 +0530

    Time: 3 ms (29.71%) | Memory: 28.1 MB (86.60%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,27 +1,7 @@
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         sort(nums.begin(), nums.end());
-//         return nums[nums.size() / 2];
-//     }
-// };
 class Solution {
-public:  // moor's voting algorithm 
+public:
     int majorityElement(vector<int>& nums) {
-        int major = nums[0];
-        int count = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (count == 0) {
-                 major = nums[i];
-
-                count = 1;
-                continue;
-            }
-            if (nums[i] == major)
-                count++;
-            else
-                count--;
-        }
-        return major;
+        sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
     }
 };
