5e9b63758f0a311e34af406d78f16e8e3828c47c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-06 23:45:26 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (53.63%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -6,11 +6,14 @@ public:
         for(int x : nums) {
             total_sum += x;
         }
+        // total_sum = accumulate(nums.begin(),nums.end(),0);
 
         int left_sum = 0;
 
         for(int i = 0; i < nums.size(); i++) {
-            
+            // total sum = left_sum + pivot idx element(nums[i]) + right_sum
+            // to ab rearrange krke aata hai 
+            // right_sum = total sum - left_sum - nums[i]
             int right_sum = total_sum - left_sum - nums[i];
 
             if(left_sum == right_sum) {
