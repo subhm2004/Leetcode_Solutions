fd5ea9ca406996d4f6b232532c6e6c641e93f63f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-07 01:50:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (51.51%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
-               c=='A' || c=='E' || c=='I' || c=='O' || c=='U';
+    bool is_vowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+    bool is_consonant(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            return !is_vowel(c);
+        }
+        return false; // non-alphabet characters
     }
 
     string reverseVowels(string s) {
-        int i = 0, j = s.size() - 1;
+        int i = 0;
+        int j = s.size() - 1;
 
         while (i < j) {
             // move i till vowel
-            while (i < j && !isVowel(s[i])) i++;
+            while (i < j && !is_vowel(s[i]))
+                i++;
 
             // move j till vowel
-            while (i < j && !isVowel(s[j])) j--;
+            while (i < j && !is_vowel(s[j]))
+                j--;
 
             // swap vowels
             swap(s[i], s[j]);
