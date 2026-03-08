e402f1b5b0268a00a3a1795e911c8d8780e44bae
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-08 14:32:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (86.66%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,17 @@
+// Cantor's diagonalization argument (diagonal flip method)
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+        for (int i = 0; i < nums.size(); ++i)
+            ans += nums[i][i] == '0' ? '1' : '0';
+
+        return ans;
+    }
+};
+/*
+Agar hum kisi binary string ke collection ko ek 2D matrix ke form me likhein,
+toh diagonal elements ko flip karne se ek naya string banta hai jo har existing
+string se kam se kam ek position par different hota hai—isiliye woh list me nahi
+hoga.
+*/
