8a7a4444efb380e47aec9961bbeb3299e6e36dbb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-08 14:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (28.67%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -26,3 +26,31 @@ public:
         return -1;
     }
 };
+
+
+/*
+class Solution {
+public:
+    int pivotIndex(vector<int>& arr) {
+        int n = arr.size();
+
+        // prefix_sum[i] = sum of elements from index 0 to i-1
+        vector<int> prefix_sum(n + 1, 0);
+        for (int i = 0; i < n; i++)
+            prefix_sum[i + 1] = prefix_sum[i] + arr[i];
+
+        int total_sum = prefix_sum[n];
+
+        for (int i = 0; i < n; i++) {
+            int left_sum  = prefix_sum[i];              // sum of [0, i-1]
+            int right_sum = total_sum - prefix_sum[i + 1]; // sum of [i+1, n-1]
+
+            if (left_sum == right_sum)
+                return i;
+        }
+
+        return -1;
+    }
+};
+
+*/
