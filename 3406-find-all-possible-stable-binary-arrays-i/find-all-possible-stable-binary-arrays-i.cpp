0a800b148390b1f26b428caa59c0c7e747114afb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-09 19:23:36 +0530

    Time: 236 ms (61.79%) | Memory: 8.7 MB (84.55%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -3,23 +3,6 @@ public:
     int MOD = 1e9 + 7;
     int dp[201][201][2];  
 
-    int solveRE(int one, int zero, bool flag, int limit) {
-        if (one == 0 && zero == 0) return 1;
-
-        int ans = 0;
-        if (flag) { // Previous was 0, now need to pick 1's
-            for (int i = 1; i <= min(one, limit); i++) {
-                ans = (ans + solveRE(one - i, zero, false, limit)) % MOD;
-            }
-        } else { // Previous was 1, now need to pick 0's
-            for (int i = 1; i <= min(zero, limit); i++) {
-                ans = (ans + solveRE(one, zero - i, true, limit)) % MOD;
-            }
-        }
-
-        return ans;
-    }
-
     int solveME(int one, int zero, bool flag, int limit) {
         if (one == 0 && zero == 0) return 1;
 
