f18a7df1936b428117e570591426bceab5fe5921
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-12 17:54:12 +0530

    Time: 63 ms (28.71%) | Memory: 55.4 MB (25.69%) - LeetSync

--- 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     unordered_map<int, list<int>> adjList;
 
+    // Graph banana - har path ke liye dono directions me edge add kro
     void buildGraph(vector<vector<int>>& paths) {
         for (auto& p : paths) {
-            int u = p[0];
-            int v = p[1];
+            int u = p[0] - 1; // 0-based index
+            int v = p[1] - 1; // 0-based index
 
             adjList[u].push_back(v);
             adjList[v].push_back(u);
@@ -14,17 +15,17 @@ public:
 
     void dfs(int node, vector<int>& flower, vector<bool>& visited) {
 
+        // Is node ko visited mark kro
         visited[node] = true;
 
+        // Dekho kaun se flowers already neighbours ne le rakhe hain
         bool used[5] = {false};
-
-        // neighbours ke flowers mark kr diye
         for (auto nbr : adjList[node]) {
             if (flower[nbr] != 0)
-                used[flower[nbr]] = true;
+                used[flower[nbr]] = true; // Neighbour ka flower mark kro
         }
 
-        // available flower assign kr diye
+        // Pehla available flower assign kro jo kisi neighbour ne nahi liya
         for (int f = 1; f <= 4; f++) {
             if (!used[f]) {
                 flower[node] = f;
@@ -32,7 +33,7 @@ public:
             }
         }
 
-        // DFS traversal kr diya
+        // Abhi jo unvisited neighbours hain unpe DFS chalao
         for (auto nbr : adjList[node]) {
             if (!visited[nbr]) {
                 dfs(nbr, flower, visited);
@@ -44,15 +45,17 @@ public:
 
         buildGraph(paths);
 
-        vector<int> flower(n + 1, 0);
-        vector<bool> visited(n + 1, false);
+        // 0-based: size n, sab 0 se start (matlab koi flower nahi)
+        vector<int> flower(n, 0);
+        vector<bool> visited(n, false);
 
-        for (int i = 1; i <= n; i++) {
+        // Har disconnected component ke liye DFS chalao
+        for (int i = 0; i < n; i++) {
             if (!visited[i])
                 dfs(i, flower, visited);
         }
 
-        flower.erase(flower.begin());
+        // Direct return kro - ab erase ki zaroorat nahi kyunki 0-based hai
         return flower;
     }
 };
