0445304b67a170411bbf197b92e1ce326a1d566a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-12 18:51:37 +0530

    Time: 7 ms (47.85%) | Memory: 21.3 MB (8.47%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool topoSortBfs(int n, unordered_map<int, list<int>>& adjList) {
+        vector<int> result;
+        queue<int> q;
+        unordered_map<int, int> indegree;
+
+        for (auto& pair : adjList) {
+            int node = pair.first;
+            for (auto& neighbor : pair.second) {
+                indegree[neighbor]++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while (!q.empty()) {
+            int currentNode = q.front();
+            q.pop();
+
+            result.push_back(currentNode);
+
+            for (auto& neighbor : adjList[currentNode]) {
+                indegree[neighbor]--;
+                if (indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        return result.size() == n;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, list<int>> adjList;
+
+        for (auto& prerequisite : prerequisites) {
+            int u = prerequisite[0];
+            int v = prerequisite[1];
+            adjList[v].push_back(u);
+        }
+
+        return topoSortBfs(numCourses, adjList);
+    }
+};
