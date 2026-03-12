41a8006f2f43ef896456742c1a3ac928c950cfef
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-12 16:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (45.75%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -9,3 +9,20 @@ public:
         return num ^ mask;
     }
 };
+
+/**
+ * num = 5 = "101" (binary)
+ *
+ * STEP 1: Significant bits nikalo
+ *   __builtin_clz(5) = 29 (leading zeros)
+ *   bits = 32 - 29 = 3
+ *
+ * STEP 2: Mask banao (utni 1s jitni bits)
+ *   (1LL << 3) - 1 = 8 - 1 = 7 = "111"
+ *
+ * STEP 3: XOR karo (har bit flip)
+ *   101 (5)
+ * ^ 111 (7)
+ * -------
+ *   010 = 2 
+ */
