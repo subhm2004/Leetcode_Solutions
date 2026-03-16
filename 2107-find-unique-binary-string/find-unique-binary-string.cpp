865260a0321732145510986dc23cb63fcdd8368f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-16 14:14:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (98.86%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -4,7 +4,7 @@ public:
     string findDifferentBinaryString(vector<string>& nums) {
         string ans;
         for (int i = 0; i < nums.size(); ++i)
-            ans += nums[i][i] == '0' ? '1' : '0';
+            ans += (nums[i][i] == '0') ? '1' : '0';
 
         return ans;
     }
