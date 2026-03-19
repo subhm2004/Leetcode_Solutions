5618613005fc5b6f898803af95b3f94f37d9e77d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-19 12:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.45%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,56 +1,21 @@
-class Solution {     // Counting method 
+class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        
-        int count_0 = 0;
-        int count_1 = 0;
-        int count_2 = 0;
-        
-        for(int &num : nums) {
-            if(num == 0)
-                count_0++;
-            else if(num == 1)
-                count_1++;
-            else
-                count_2++;
-        }
-        
-        for(int i = 0; i<n; i++) {
-            if(count_0 > 0) {
-                nums[i] = 0;
-                count_0--;
-            } else if(count_1 > 0) {
-                nums[i] = 1;
-                count_1--;
-            } else if(count_2 > 0) {
-                nums[i] = 2;
-                count_2--;
+        int i = 0;
+        int j = 0;
+        int k = nums.size() - 1;
+
+        while (j <= k) {
+            if (nums[j] == 2) {
+                swap(nums[j], nums[k--]);
+            }
+            else if (nums[j] == 0) {
+                swap(nums[i++],nums[j++]);
+            
+            }
+            else{
+                j++;
             }
         }
     }
 };
-
-// class Solution {       // Dutch national flag algorithm ( swap method )
-// public:
-//     void sortColors(vector<int>& nums) {
-//         int n = nums.size();
-        
-//         int i = 0;   //denotes for 0
-//         int j = 0;   //denotes for 1
-//         int k = n-1; //denotes for 2
-        
-//         while ( j <= k) {
-//             if(nums[j] == 1) {
-//                 j++;
-//             } else if(nums[j] == 2) {
-//                 swap(nums[j], nums[k]);
-//                 k--;
-//             } else { //nums[j] == 0
-//                 swap(nums[j], nums[i]);
-//                 i++;
-//                 j++;
-//             }
-//         }
-//     }
-// };
