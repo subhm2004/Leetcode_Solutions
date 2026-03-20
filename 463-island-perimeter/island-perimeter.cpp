817ac184acf7f114252831151363aaebc7ca8ec8
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-20 08:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 102.4 MB (37.20%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,55 +1,43 @@
 class Solution {
- public:
-  int bfs(vector<vector<int>>& grid, int startRow, int startCol) {
-    int rows = grid.size();
-    int cols = grid[0].size();
-    queue<pair<int, int>> q;
+public:
+    int m;
+    int n;
+    int peri;
 
-    // Direction vector for moving up, down, left, right
-    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    void dfs(vector<vector<int>>& grid, int i, int j) {
 
-    int perimeter = 0;
-
-    // Start BFS from the given cell
-    q.push({startRow, startCol});
-    grid[startRow][startCol] = 2;  // Mark as visited
-
-    while (!q.empty()) {
-      auto [i, j] = q.front();
-      q.pop();
-
-      for (const auto& dir : directions) {
-        int ni = i + dir.first;
-        int nj = j + dir.second;
-
-        // Check the neighbor
-        if (ni < 0 || ni >= rows || nj < 0 || nj >= cols || grid[ni][nj] == 0) {
-          // Increment perimeter if out of bounds or water
-          ++perimeter;
-        } else if (grid[ni][nj] == 1) {
-          // Add unvisited land cell to the queue
-          q.push({ni, nj});
-          grid[ni][nj] = 2;  // Mark as visited
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            peri++;
+            return;
         }
-      }
+
+        if (grid[i][j] == -1) {
+            return;
+        }
+
+        grid[i][j] = -1; // mark visited
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 
-    return perimeter;
-  }
+    int islandPerimeter(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        peri = 0;
 
-  int islandPerimeter(vector<vector<int>>& grid) {
-    int rows = grid.size();
-    int cols = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
 
-    for (int i = 0; i < rows; ++i) {
-      for (int j = 0; j < cols; ++j) {
-        if (grid[i][j] == 1) {
-          // Start BFS when we find the first land cell
-          return bfs(grid, i, j);
+                if (grid[i][j] == 1) {
+                    dfs(grid, i, j);
+                    return peri;
+                }
+            }
         }
-      }
-    }
 
-    return 0;  // No land found
-  }
+        return -1;
+    }
 };
