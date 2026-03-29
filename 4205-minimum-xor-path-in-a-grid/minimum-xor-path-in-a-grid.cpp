258363d85c90781cfd587bb7e94abb55c0342923
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-29 17:37:52 +0530

    Time: 540 ms (25.00%) | Memory: 365.9 MB (41.67%) - LeetSync

---
+++ 4205-minimum-xor-path-in-a-grid/minimum-xor-path-in-a-grid.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int m, n;
+    vector<vector<int>> grid;
+
+    vector<vector<vector<int>>> dp;
+
+    int solve(int i, int j, int curr_xor) {
+        curr_xor ^= grid[i][j];
+
+        if (i == m - 1 && j == n - 1) {
+            return curr_xor;
+        }
+
+        if (dp[i][j][curr_xor] != -1)
+            return dp[i][j][curr_xor];
+
+        int ans = INT_MAX;
+
+        if (i + 1 < m) {
+            ans = min(ans, solve(i + 1, j, curr_xor));
+        }
+
+        if (j + 1 < n) {
+            ans = min(ans, solve(i, j + 1, curr_xor));
+        }
+
+        return dp[i][j][curr_xor] = ans;
+    }
+
+    int minCost(vector<vector<int>>& grid) {
+
+        this->grid = grid;
+        m = grid.size();
+        n = grid[0].size();
+
+        dp.assign(m, vector<vector<int>>(n, vector<int>(1024, -1)));
+
+        return solve(0, 0, 0);
+    }
+};
