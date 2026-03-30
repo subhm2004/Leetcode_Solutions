1aaa2db57a04696c8177f7e4e6398e2d9278b74a
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-30 13:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (24.36%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        // even group match  AND  odd group match
-        return ((s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0])) &&
-            ((s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1]));
+        int n = s1.size();
+        string s1_even = "", s1_odd = "";
+        string s2_even = "", s2_odd = "";
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                s1_even += s1[i];
+                s2_even += s2[i];
+            } else {
+                s1_odd += s1[i];
+                s2_odd += s2[i];
+            }
+        }
+
+        sort(s1_even.begin(), s1_even.end());
+        sort(s2_even.begin(), s2_even.end());
+        sort(s1_odd.begin(), s1_odd.end());
+        sort(s2_odd.begin(), s2_odd.end());
+
+        return s1_even == s2_even && s1_odd == s2_odd;
     }
 };
