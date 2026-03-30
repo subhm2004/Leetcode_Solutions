025540b2c6c1e4afab87a024804cf5d7517aac28
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-30 13:36:51 +0530

    Time: 54 ms (95.77%) | Memory: 67.7 MB (71.27%) - LeetSync

---
+++ 4097-minimum-removals-to-achieve-target-xor/minimum-removals-to-achieve-target-xor.cpp
@@ -0,0 +1,40 @@
+// minimum removals = n - (max subset size whose xor is equal to target)
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(int i, int curr_xor, vector<int>& nums, int target) {
+        if (i == nums.size()) {
+            return (curr_xor == target) ? 0 : INT_MIN/2; // -INF
+        }
+
+        if (dp[i][curr_xor] != -1)
+            return dp[i][curr_xor];
+
+        // include
+        int include = 1 + solve(i + 1, curr_xor ^ nums[i], nums, target);
+
+        // exclude
+        int exclude = solve(i + 1, curr_xor, nums, target);
+
+        return dp[i][curr_xor] = max(include, exclude);
+    }
+
+    int minRemovals(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int maxVal = *max_element(nums.begin(), nums.end());
+        maxVal = max({maxVal, target, 1}); // Fix 1: 0 aur target dono handle
+
+        int bits = log2(maxVal) + 1;
+        int MAXX = 1 << bits;
+        dp.assign(n + 1, vector<int>(MAXX, -1));
+
+        int max_subset = solve(0, 0, nums, target); // max subset size whose xor equal to target
+
+        if (max_subset < 0)
+            return -1;
+
+        return n - max_subset;
+    }
+};
