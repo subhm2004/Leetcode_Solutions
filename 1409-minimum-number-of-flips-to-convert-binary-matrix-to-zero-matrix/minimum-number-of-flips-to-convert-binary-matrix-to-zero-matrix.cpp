266aae0ce81f818e5a030851af0414ff157b42bb
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-31 04:08:24 +0530

    Time: 23 ms (10.45%) | Memory: 15.3 MB (11.94%) - LeetSync

---
+++ 1409-minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix/minimum-number-of-flips-to-convert-binary-matrix-to-zero-matrix.cpp
@@ -0,0 +1,60 @@
+using Matrix = vector<vector<int>>;
+class Solution {
+public:
+    bool is_zero_matrix(Matrix& mat) {
+        for (auto& row : mat) {
+            for (int val : row) {
+                if (val == 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    int bfs(Matrix start, int m, int n) {
+        queue<pair<Matrix, int>> q;
+        set<Matrix> visited;
+
+        q.push({start, 0});
+        visited.insert(start);
+
+        vector<pair<int, int>> directions = {
+            {0, 0}, {1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        while (!q.empty()) {
+            auto [curr, steps] = q.front();
+            q.pop();
+
+            if (is_zero_matrix(curr))
+                return steps;
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+
+                    Matrix new_mat = curr;
+
+                    for (auto& d : directions) {
+                        int ni = i + d.first;
+                        int nj = j + d.second;
+
+                        if (ni >= 0 && ni < m && nj >= 0 && nj < n) {
+                            new_mat[ni][nj] ^= 1;
+                        }
+                    }
+
+                    if (!visited.count(new_mat)) {
+                        visited.insert(new_mat);
+                        q.push({new_mat, steps + 1});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    int minFlips(Matrix& mat) {
+        int m = mat.size(), n = mat[0].size();
+        return bfs(mat, m, n);
+    }
+};
