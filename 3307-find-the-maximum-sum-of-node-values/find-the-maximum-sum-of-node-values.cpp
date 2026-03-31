6c3dbbe702f26a28358561dd0f2394ece5fdfe2c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-03-31 06:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 126.7 MB (51.16%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -3,30 +3,30 @@ public:
     typedef long long ll;
 
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        ll totalSum = 0;
-        int beneficialCount = 0;
-        int smallestLoss = INT_MAX;
+        ll total_sum = 0;
+        int count = 0;
+        int smallest_loss = INT_MAX;
 
         for (ll num : nums) {
-            int xoredValue = num ^ k;
+            int xored_val = num ^ k;
 
             // Agar XOR karne se value badh rahi hai toh use kar lo
-            if (xoredValue > num) {
-                beneficialCount++;
-                totalSum += xoredValue;
+            if (xored_val > num) {
+                count++;
+                total_sum += xored_val;
             } else {
-                totalSum += num;
+                total_sum += num;
             }
 
             // Nuksaan track karo jab XOR beneficial na ho
-            smallestLoss = min((ll)smallestLoss, abs(num - xoredValue));
+            smallest_loss = min((ll)smallest_loss, abs(num - xored_val));
         }
 
-        // Agar beneficial XORs ka count even hai toh sab sahi
-        if (beneficialCount % 2 == 0)
-            return totalSum;
+        // Agar XORs ka count even hai toh sab sahi
+        if (count % 2 == 0)
+            return total_sum;
 
         // Warna, ek minimum nukasan waale ko hata do
-        return totalSum - smallestLoss;
+        return total_sum - smallest_loss;
     }
 };
