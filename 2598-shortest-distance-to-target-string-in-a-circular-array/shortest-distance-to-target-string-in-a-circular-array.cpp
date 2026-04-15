0c0f34a90da9dd791a08ae7038db304256374d46
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-04-15 20:51:43 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (37.22%) - LeetSync

---
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int start_idx) {
+        int n = words.size();
+
+        // Dono directions mein target search karte h (clockwise aur counter-clockwise)
+        for (int i = 0; i < n; ++i) {
+            // closewise move: (start_idx + i) % n
+            int right = (start_idx + i) % n;
+            
+            // anti-closewise move: (start_idx - i + n) % n
+            int left = (start_idx - i + n) % n;
+
+            // Agar target kisi direction mein mil jaaye, to i steps return karo
+            if (words[right] == target || words[left] == target)
+                return i;
+        }
+
+        // Agar target nahi milta, to -1 return kr dege
+        return -1;
+    }
+};
