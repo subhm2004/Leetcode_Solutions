128971b7df54ceecaa0eff02c237f965be66bc05
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-04-18 11:49:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (86.21%) - LeetSync

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int reversed_number(int n) {
-        string s = to_string(n);
-        reverse(s.begin(),s.end());
-        return stoi(s);
+        int rev = 0;
+        while(n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+        return rev;
     }
 
     int mirrorDistance(int n) {
