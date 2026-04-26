3a68439169f626d97d9d07f9290ae4a36b4eb634
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-04-26 21:27:35 +0530

    Time: 2 ms (9.09%) | Memory: 125.5 MB (0.00%) - LeetSync

---
+++ 4292-compare-sums-of-bitonic-parts/compare-sums-of-bitonic-parts.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int compareBitonicSums(vector<int>& nums) {
+        int n = nums.size();
+        int peak = 0;
+
+        // peak nikalte hai 
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                peak = i;
+                break;
+            }
+        }
+
+        long long ascending_sum = 0, descending_sum = 0;
+
+        for (int i = 0; i <= peak; i++) {
+            ascending_sum += nums[i];
+        }
+
+        for (int i = peak; i < n; i++) {
+            descending_sum += nums[i];
+        }
+
+        if (ascending_sum > descending_sum) return 0;
+        if (descending_sum > ascending_sum) return 1;
+        return -1;
+    }
+};
