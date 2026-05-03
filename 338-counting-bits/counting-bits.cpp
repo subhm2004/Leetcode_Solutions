e6f15c67e8d974eed8ed99f7c795ebad77d43fbc
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-05-03 16:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (79.11%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -13,8 +13,7 @@ public:
         vector<int> ans(n + 1);
         for (int i = 0; i <= n; i++) {
             // ans[i] = __builtin_popcount(i);
-            // ans[i] = count_set_bits(i);
-            ans[i] = ans[i>>1] + (i&1);
+            ans[i] = count_set_bits(i);
         }
         return ans;
     }
@@ -22,35 +21,4 @@ public:
 
 
 
-/*
 
-class Solution {
-public:
-    vector<int> countBits(int n) {
-        vector<int> ans(n+1);
-        
-        if(n == 0)
-            return ans;
-        
-        ans[1] = 1;
-        
-        for(int i = 2; i < n+1; i++) {
-            
-            if(i%2 == 0) {
-                ans[i] = ans[i/2];
-            } else {
-                ans[i] = ans[i/2] + 1;
-            }
-            
-        }
-        
-        return ans;
-        
-    }
-};
-
-
-
-
-
-*/
