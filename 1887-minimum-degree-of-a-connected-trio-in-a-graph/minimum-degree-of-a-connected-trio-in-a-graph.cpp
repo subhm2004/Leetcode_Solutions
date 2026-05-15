abde2971ec631c9aabfefa89c1b918b34d7c0703
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-05-15 13:32:00 +0530

    Time: 347 ms (39.35%) | Memory: 44.5 MB (85.66%) - LeetSync

--- 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.cpp
@@ -3,7 +3,7 @@ public:
     int minTrioDegree(int n, vector<vector<int>>& edges) {
         // adjacency matrix
         vector<vector<bool>> adj(n + 1, vector<bool>(n + 1, false));
-        unordered_map<int,int>degree;
+        unordered_map<int, int> degree;
 
         for (auto& edge : edges) {
             int u = edge[0], v = edge[1];
@@ -12,23 +12,22 @@ public:
             degree[v]++;
         }
 
-        int minDeg = INT_MAX;
+        int ans = INT_MAX;
 
-        // Only iterate over edges, then check third node
-        // O(E * N) 
-        for (auto& edge : edges) {
-            int u = edge[0], v = edge[1];
-
-            for (int k = 1; k <= n; k++) {
-                if (adj[u][k] && adj[v][k]) {
-                    // Valid trio (u, v, k) found
-                    // -6 because each internal edge is counted twice in degrees
-                    int trioDeg = degree[u] + degree[v] + degree[k] - 6;
-                    minDeg = min(minDeg, trioDeg);
+        for (int i = 1; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (!adj[i][j])
+                    continue;
+                for (int k = j + 1; k <= n; k++) {
+                    if (adj[i][k] && adj[j][k]) {
+                        int trio_degree = degree[i] + degree[j] + degree[k] - 6;
+                        ans = min(ans, trio_degree);
+                    }
                 }
             }
         }
-
-        return minDeg == INT_MAX ? -1 : minDeg;
+        if (ans == INT_MAX)
+            return -1;
+        return ans;
     }
 };
