e669cab6f057a758c5f848d4097c763779bcbe2c
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-05-16 22:45:33 +0530

    Time: 102 ms (45.40%) | Memory: 138.6 MB (73.65%) - LeetSync

---
+++ 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    const int MOD = 1e9 + 7;
+
+    int numberOfGoodPartitions(vector<int>& nums) {
+
+        int n = nums.size();
+
+        unordered_map<int,int> last;
+
+        for(int i = 0; i < n; i++) {
+            last[nums[i]] = i;
+        }
+
+        int partitions = 0;
+
+        int i = 0;
+        int j = -1;
+
+        while(i < n) {
+
+            // naya partition start
+            if(i > j) {
+                partitions++;
+            }
+
+            j = max(j, last[nums[i]]);
+
+            i++;
+        }
+
+        long long ans = 1;
+
+        // M partitions ke liye M-1 cuts lagte hai or har cut pr ye hota h mari ya na maro to total ho gya hai 2^M-1
+        for(int i = 1; i < partitions; i++) {
+            ans = (ans * 2) % MOD;
+        }
+
+        return ans;
+    }
+};
