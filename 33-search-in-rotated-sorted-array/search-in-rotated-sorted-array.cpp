842e14bbd185e100f75b07191ba6f56a5bc4ffed
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-05-23 15:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (33.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,25 +1,61 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
+
+    int binarySearch(vector<int>& nums, int l, int r, int target) {
 
         while (l <= r) {
+
             int mid = l + (r - l) / 2;
+
             if (nums[mid] == target)
                 return mid;
-            if (nums[l] <= nums[mid]) {
-                if (nums[l] <= target && target < nums[mid])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            } else {
-                if (nums[mid] < target && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
+
+            if (nums[mid] < target)
+                l = mid + 1;
+            else
+                r = mid - 1;
         }
+
         return -1;
+    }
+
+    int findPivot(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int l = 0;
+        int r = n - 1;
+
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+
+            // pivot found
+            if (mid < n - 1 && nums[mid] > nums[mid + 1])
+                return mid + 1;
+
+            // left part sorted
+            if (nums[l] <= nums[mid])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return 0;
+    }
+
+    int search(vector<int>& nums, int target) {
+
+        int n = nums.size();
+
+        int pivot = findPivot(nums);
+
+        // target right sorted half me hai
+        if (target >= nums[pivot] && target <= nums[n - 1]) {
+            return binarySearch(nums, pivot, n - 1, target);
+        }
+
+        // target left sorted half me hai
+        return binarySearch(nums, 0, pivot - 1, target);
     }
 };
