9ec33f7e1dd122710e8b9d52bd8e15897e3441a1
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-05-23 19:48:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (53.59%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
+
     bool check(vector<int>& nums) {
+
         int n = nums.size();
 
-        int peak = 0;
+        int pivot = 0;
+
         for (int i = 0; i < n; i++) {
+
             if (nums[i] > nums[(i + 1) % n]) {
-                peak++;
+                pivot++;
             }
+            if(pivot > 1) return false;
         }
 
-        return peak <= 1;
+        return true;
     }
 };
-
-
-// class Solution {     agar mujeh k times roate krna hota array to 
-// public:
-//     void rotate(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         k = k % n; // Handle cases where k > n
-
-//         // Reverse entire array
-//         reverse(nums.begin(), nums.end());
-
-//         // Reverse first k elements
-//         reverse(nums.begin(), nums.begin() + k);
-
-//         // Reverse remaining n-k elements
-//         reverse(nums.begin() + k, nums.end());
-//     }
-// };
