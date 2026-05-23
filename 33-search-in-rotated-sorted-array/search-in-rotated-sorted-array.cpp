282f9d318f55a6a1a8f56930d3a976161caebec7
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-05-23 15:21:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.92%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
 
-    int binary_Search(vector<int>& nums, int l, int r, int target) {
+    int search(vector<int>& nums, int target) {
+
+        int l = 0;
+        int r = nums.size() - 1;
 
         while (l <= r) {
 
@@ -10,52 +13,27 @@ public:
             if (nums[mid] == target)
                 return mid;
 
-            if (nums[mid] < target)
-                l = mid + 1;
-            else
-                r = mid - 1;
+            // left half sorted
+            if (nums[l] <= nums[mid]) {
+
+                // target left half me hai
+                if (nums[l] <= target && target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+
+            // right half sorted
+            else {
+
+                // target right half me hai
+                if (nums[mid] < target && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
         }
 
         return -1;
-    }
-
-    int find_Pivot(vector<int>& nums) {
-
-        int n = nums.size();
-
-        int l = 0;
-        int r = n - 1;
-
-        while (l <= r) {
-
-            int mid = l + (r - l) / 2;
-
-            // pivot found
-            if (mid < n - 1 && nums[mid] > nums[mid + 1])
-                return mid + 1;
-
-            // left part sorted
-            if (nums[l] <= nums[mid])
-                l = mid + 1;
-            else
-                r = mid - 1;
-        }
-
-        return 0;
-    }
-
-    int search(vector<int>& nums, int target) {
-
-        int n = nums.size();
-
-        int pivot = find_Pivot(nums);
-
-        // target right sorted half me hai
-        if (target >= nums[pivot] && target <= nums[n - 1]) {
-            return binary_Search(nums, pivot, n - 1, target);
-        }
-
-        // target left sorted half me hai
-        return binary_Search(nums, 0, pivot - 1, target);
     }
 };
