9c9f31c8dbe067b878d952512a73eabee18571f4
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-11 00:02:15 +0530

    Time: 0 ms (100.00%) | Memory: 103.6 MB (68.60%) - LeetSync

--- 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -3,7 +3,7 @@ public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int maxi = *max_element(nums.begin(), nums.end());
         int mini = *min_element(nums.begin(), nums.end());
-        long long diff = (long long)maxi - mini;
+        long long diff = 1LL * (maxi - mini);
         return diff * k;
     }
 };
