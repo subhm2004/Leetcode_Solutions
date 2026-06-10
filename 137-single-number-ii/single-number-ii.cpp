8611fa5c2eeb14cbd51617729b0274730419e526
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-10 22:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (86.36%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,19 +1,16 @@
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> count;
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return nums[0];
 
-        for (const int num : nums) {
-            count[num]++;
+        int ones = 0; // holds number appearing only 1 times
+        int twos = 0; // holds number appearing 2 or more than 2 times
+
+        for(int i = 0; i < nums.size(); i++){
+            ones = (ones ^ nums[i]) & ~twos;
+            twos = (twos ^ nums[i]) & ~ones;
         }
-
-        for (const auto& pair : count) {
-            if (pair.second == 1) {
-                return pair.first;
-            }
-        }
-
-        return -1;
+        return ones;
     }
 };
