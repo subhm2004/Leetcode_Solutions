009fa9ef7be2a502302a2438299fde975ac51cd8
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-21 15:13:46 +0530

    Time: 2 ms (50.00%) | Memory: 11.1 MB (50.00%) - LeetSync

--- 4342-create-grid-with-exactly-one-path/create-grid-with-exactly-one-path.cpp
+++ 4342-create-grid-with-exactly-one-path/create-grid-with-exactly-one-path.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
+
+    // recursion se path bana rahe hain
+    void dfs(int i, int j, int m, int n, vector<string>& grid) {
+
+        // current cell ko free kar do
+        grid[i][j] = '.';
+
+        // destination aa gaya
+        if (i == m - 1 && j == n - 1)
+            return;
+
+        // agar right ja sakte hain to right jao
+        if (j + 1 < n)
+            dfs(i, j + 1, m, n, grid);
+
+        // warna down jao
+        else
+            dfs(i + 1, j, m, n, grid);
+    }
+
     vector<string> createGrid(int m, int n) {
+
+        // initially sab blocked
         vector<string> grid(m, string(n, '#'));
 
-        if (m == 1) {
-            grid[0] = string(n, '.');
-            return grid;
-        }
-
-        if (n == 1) {
-            for (int i = 0; i < m; i++) grid[i][0] = '.';
-            return grid;
-        }
-
-        for (int j = 0; j < n; j++) grid[0][j] = '.';
-        for (int i = 0; i < m; i++) grid[i][n - 1] = '.';
+        // (0,0) se unique path banao
+        dfs(0, 0, m, n, grid);
 
         return grid;
     }
