17ff1cba300f9d9573dc2d0bf7e55808f0f528ae
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-23 19:37:15 +0530

    Time: 51 ms (99.66%) | Memory: 34.5 MB (9.64%) - LeetSync

--- 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.cpp
+++ 4318-valid-subarrays-with-matching-sum-digits-i/valid-subarrays-with-matching-sum-digits-i.cpp
@@ -1,33 +1,48 @@
 class Solution {
 public:
-    int first_digit(long long num) {
-
-        while (num >= 10) {
-            num /= 10;
-        }
-
-        return num;
-    }
-
-    int last_digit(long long num) { return num % 10; }
-
     long long countValidSubarrays(vector<int>& nums, int x) {
 
         int n = nums.size();
 
-        long long ans = 0;
+        vector<long long> pref(n + 1, 0);
 
         for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i] + nums[i];
+        }
 
-            long long sum = 0;
+        long long maxSum = pref[n];
+        long long ans = 0;
 
-            for (int j = i; j < n; j++) {
+        for (int j = 1; j <= n; j++) {
 
-                sum += nums[j];
+            long long curPref = pref[j];
 
-                if (last_digit(sum) == x && first_digit(sum) == x) {
-                    ans++;
+            long long p = 1;
+
+            while (p <= maxSum) {
+
+                long long L = 1LL * x * p;
+                long long R = 1LL * (x + 1) * p - 1;
+
+                if (L > maxSum)
+                    break;
+
+                long long left = curPref - R;
+                long long right = curPref - L;
+
+                auto it1 = lower_bound(pref.begin(), pref.begin() + j, left);
+                auto it2 = upper_bound(pref.begin(), pref.begin() + j, right);
+
+                for (auto it = it1; it != it2; ++it) {
+
+                    long long sum = curPref - *it;
+
+                    if (sum % 10 == x) {
+                        ans++;
+                    }
                 }
+
+                p *= 10;
             }
         }
 
