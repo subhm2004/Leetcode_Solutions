ba5e76792e718458ff64fbef5862ddee10f0ae83
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-25 15:16:33 +0530

    Time: 23 ms (87.15%) | Memory: 50 MB (11.55%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,22 +1,77 @@
+class FenwickTree {
+public:
+    int n;
+    vector<int> bit; // 1-indexed fenwick array
+
+    FenwickTree(int n) {
+        this->n = n;
+        bit.resize(n + 1, 0);
+    }
+
+    void update(int i, int x) {
+        for (; i <= n; i += (i & -i))
+            bit[i] += x;
+    }
+
+    int prefix_sum(int i) {
+        int total = 0;
+        for (; i > 0; i -= (i & -i))
+            total += bit[i];
+        return total;
+    }
+    int range_sum(int l, int r) { return prefix_sum(r) - prefix_sum(l - 1); }
+};
+class Coordinate_Compression {
+public:
+    unordered_map<int, int> compress;
+    vector<int> rev_map;
+
+    Coordinate_Compression(const vector<int>& arr) {
+        set<int> st(arr.begin(), arr.end());
+        int id = 0;
+        for (int x : st) {
+            compress[x] = id;
+            rev_map.push_back(x);
+            id++;
+        }
+    }
+
+    vector<int> get_compressed(const vector<int>& arr) const {
+        vector<int> res(arr.size());
+        for (int i = 0; i < (int)arr.size(); i++)
+            res[i] = compress.at(arr[i]);
+        return res;
+    }
+
+    int get_original(int val) const { return rev_map[val]; }
+
+    int size() const { return (int)rev_map.size(); }
+};
+
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
         int n = nums.size();
-        int ans = 0;
 
-        for(int i = 0; i < n; i++) {
-            int count = 0;
+        vector<int> prefix(n + 1, 0);
 
-            for(int j = i; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target)
+                prefix[i + 1] = prefix[i] + 1;
+            else
+                prefix[i + 1] = prefix[i] - 1;
+        }
 
-                if(nums[j] == target)
-                    count++;
+        Coordinate_Compression cc(prefix);
+        vector<int> compressed = cc.get_compressed(prefix);
 
-                int len = j - i + 1;
+        FenwickTree bit(cc.size());
 
-                if(count > len / 2)
-                    ans++;
-            }
+        long long ans = 0;
+
+        for (int x : compressed) {
+            ans += bit.prefix_sum(x);
+            bit.update(x + 1, 1);
         }
 
         return ans;
