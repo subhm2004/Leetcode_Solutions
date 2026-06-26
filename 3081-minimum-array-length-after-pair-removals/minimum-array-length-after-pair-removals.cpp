f1d3ef5e8d65f386d604d521dee83a621cd62f59
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-26 17:24:39 +0530

    Time: 0 ms (100.00%) | Memory: 153.1 MB (44.84%) - LeetSync

---
+++ 3081-minimum-array-length-after-pair-removals/minimum-array-length-after-pair-removals.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minLengthAfterRemovals(vector<int>& nums) {
+        int n = nums.size();
+
+        int i = 0;
+        int j = (n + 1) / 2;
+
+        int pairs = 0;
+
+        while (i < n / 2 && j < n) {
+
+            if (nums[i] < nums[j]) {
+                pairs++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+
+        return n - 2 * pairs;
+    }
+};
