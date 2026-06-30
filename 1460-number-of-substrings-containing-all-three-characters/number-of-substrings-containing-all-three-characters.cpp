cd7ec78aedbd251d4470e3f0306886266f02ea1e
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-06-30 18:11:07 +0530

    Time: 11 ms (43.83%) | Memory: 10.9 MB (95.61%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
+        int n = s.size();
         int ans = 0;
-        vector<int> freq(3, 0); // 'a', 'b', 'c' ka count store karne ke liye
-        int left = 0; // Window ka left boundary
 
-        for (int right = 0; right < s.length(); ++right) {
-            ++freq[s[right] - 'a']; // Current character ka count badhao
+        vector<int> freq(3, 0);
 
-            // Jab tak 'a', 'b', 'c' sab ek baar aa chuke hain, window ko shrink karo
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            freq[s[right] - 'a']++;
+
             while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
-                --freq[s[left] - 'a']; // Leftmost character ko hatao
-                ++left; // Window ka left boundary badhao
+
+                freq[s[left] - 'a']--;
+
+                left++;
             }
 
-            // Har valid window se jitne substrings ban sakte hain, unka count add karo
             ans += left;
         }
 
