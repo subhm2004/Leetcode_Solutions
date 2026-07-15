1c8c54e187d36a70c7dd074821460f8eef9e3299
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-07-15 12:31:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -5,13 +5,13 @@ public:
             Sum of first n odd numbers  = n^2
             Sum of first n even numbers = n(n + 1)
 
-            gcd(sumOdd, sumEven)
+            gcd(odd_sum, even_sum)
             = gcd(n^2, n(n + 1))
             = n * gcd(n, n + 1)
 
             Since gcd(n, n + 1) = 1 (consecutive numbers are always coprime),
 
-            gcd(sumOdd, sumEven) = n
+            gcd(odd_sum, even_sum) = n
         */
         return n;
     }
