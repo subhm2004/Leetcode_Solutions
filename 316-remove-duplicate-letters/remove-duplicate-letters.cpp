6e663085df2a3d02dc97a537008dc08f2cb241c2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-07-19 17:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (41.15%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        unordered_map<char, int> last_idx;
+        unordered_map<char, bool> visited;
+
+        // Har character ki last occurrence store karo
+        for (int i = 0; i < s.size(); i++)
+            last_idx[s[i]] = i;
+
+        string ans = "";
+
+        for (int i = 0; i < s.size(); i++) {
+
+            char ch = s[i];
+
+            if (visited[ch])
+                continue;
+
+            while (!ans.empty() && ans.back() > ch && last_idx[ans.back()] > i) {
+
+                visited[ans.back()] = false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            visited[ch] = true;
+        }
+
+        return ans;
+    }
+};
