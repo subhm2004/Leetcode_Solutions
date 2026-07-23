e4abc6c62679ace833880a2c514b984f37ef707f
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-07-23 18:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 199 MB (28.13%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-
-    
     int number_of_bits(int x) { return x == 0 ? 0 : (int)log2(x) + 1; }
 
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
         if (n < 3)
             return n;
-        int no_of_bits = number_of_bits(n);
-        return pow(2, no_of_bits);
+        int bits = number_of_bits(n);
+        // return pow(2, bits);
+        return 1 << bits;
     }
 };
