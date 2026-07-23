a539c54374a239a4cf8352eea41104409ad4764d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-07-23 12:55:00 +0530

    Time: 0 ms (100.00%) | Memory: 199.3 MB (9.38%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    // int number_of_bits(int x) {
-    //     int count = 0;
-    //     while (x) {
-    //         x >>= 1; // divide x by 2 (bitwise) left shift 
+
     
-    //         count++; // count how many times we can divide until x becomes 0
-    //     }
-    //     return count;
-    // }
-    
-    int number_of_bits(int x) { return x == 0 ? 0 : (int)std::log2(x) + 1; }
+    int number_of_bits(int x) { return x == 0 ? 0 : (int)log2(x) + 1; }
 
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
         if (n < 3)
             return n;
-        int numOfBits = number_of_bits(n);
-        return pow(2, numOfBits);
+        int no_of_bits = number_of_bits(n);
+        return pow(2, no_of_bits);
     }
 };
