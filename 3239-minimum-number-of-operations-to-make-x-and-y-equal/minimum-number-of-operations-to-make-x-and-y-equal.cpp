417dd0f85634970e65f4e957f6a6dad69d54f3a2
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-08-08 00:32:31 +0530

    Time: 7 ms (78.98%) | Memory: 13.6 MB (79.41%) - LeetSync

--- 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.cpp
+++ 3239-minimum-number-of-operations-to-make-x-and-y-equal/minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -1,53 +1,43 @@
 class Solution {
 public:
-    const int MAX_LIMIT = 1e5;
+    const int LIMIT = 1e4 + 5;
 
     int bfs(int x, int y) {
-
-        vector<bool> visited(MAX_LIMIT + 1, 0);
+        vector<bool> visited(LIMIT, false);
 
         queue<pair<int, int>> q;
         q.push({x, 0});
         visited[x] = true;
 
         while (!q.empty()) {
-
             auto [curr, steps] = q.front();
             q.pop();
 
             if (curr == y)
                 return steps;
 
-            // x - 1
+            // x-1
             if (curr - 1 >= 1 && !visited[curr - 1]) {
                 visited[curr - 1] = true;
                 q.push({curr - 1, steps + 1});
             }
-
-            // x + 1
-            if (curr + 1 <= MAX_LIMIT && !visited[curr + 1]) {
+            // X + 1
+            if (curr + 1 < LIMIT && !visited[curr + 1]) {
                 visited[curr + 1] = true;
                 q.push({curr + 1, steps + 1});
             }
 
-            // Divide by 5
             if (curr % 5 == 0 && !visited[curr / 5]) {
                 visited[curr / 5] = true;
                 q.push({curr / 5, steps + 1});
             }
 
-            // Divide by 11
             if (curr % 11 == 0 && !visited[curr / 11]) {
                 visited[curr / 11] = true;
                 q.push({curr / 11, steps + 1});
             }
         }
-
         return -1;
     }
-
-    int minimumOperationsToMakeEqual(int x, int y) {
-
-        return bfs(x, y);
-    }
+    int minimumOperationsToMakeEqual(int x, int y) { return bfs(x, y); }
 };
