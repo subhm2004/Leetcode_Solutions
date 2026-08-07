7805c674ed415ede1e0e064e722da1e188b27a19
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-08-07 23:41:50 +0530

    Time: 151 ms (70.19%) | Memory: 210 MB (76.60%) - LeetSync

--- 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
+++ 4283-multi-source-flood-fill/multi-source-flood-fill.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
-    void multi_source_BFS(vector<vector<int>>& grid, vector<vector<int>>& dist,
-                        int n, int m) {
-
+    void multi_source_bfs(vector<vector<int>>& grid, vector<vector<int>>& dist,
+                          int n, int m) {
         queue<pair<int, int>> q;
 
-        // Sabhi colored cells ko source banao
+        // sare source ko queue me daal do
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
                 if (grid[i][j] != 0) {
                     dist[i][j] = 0;
                     q.push({i, j});
@@ -19,51 +17,44 @@ public:
         }
 
         while (!q.empty()) {
-
             auto [r, c] = q.front();
             q.pop();
 
-            for (auto& dir : directions) {
+            for (auto& d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
 
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-
+                // boundary conditions
                 if (nr < 0 || nr >= n || nc < 0 || nc >= m)
                     continue;
 
-                // First time reach
-                if (dist[nr][nc] == INT_MAX) {
-
+                // first time aya hai cell pr
+                if (dist[nr][nc] == 0x3f3f3f3f) {
                     dist[nr][nc] = dist[r][c] + 1;
                     grid[nr][nc] = grid[r][c];
 
                     q.push({nr, nc});
                 }
 
-                // Same time arrival
+                // same time pr aa gya
                 else if (dist[nr][nc] == dist[r][c] + 1) {
-
                     grid[nr][nc] = max(grid[nr][nc], grid[r][c]);
                 }
             }
         }
     }
-
     vector<vector<int>> colorGrid(int n, int m, vector<vector<int>>& sources) {
 
         vector<vector<int>> grid(n, vector<int>(m, 0));
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
 
         for (auto& src : sources) {
-
-            int r = src[0];
-            int c = src[1];
-            int color = src[2];
+            int r = src[0], c = src[1], color = src[2];
 
             grid[r][c] = color;
         }
+        vector<vector<int>> dist(n, vector<int>(m, 0x3f3f3f3f));
 
-        multi_source_BFS(grid, dist, n, m);
+        multi_source_bfs(grid, dist, n, m);
 
         return grid;
     }
