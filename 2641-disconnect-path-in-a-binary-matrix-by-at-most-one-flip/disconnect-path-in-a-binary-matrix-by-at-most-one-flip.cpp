8231bf108bfd3f964179b60f5adc54136844f61d
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-08-08 23:34:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.19%) - LeetSync

--- 2641-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
+++ 2641-disconnect-path-in-a-binary-matrix-by-at-most-one-flip/disconnect-path-in-a-binary-matrix-by-at-most-one-flip.cpp
@@ -1,69 +1,29 @@
 class Solution {
 public:
-
     int m, n;
 
-    // DFS ka kaam:
-    // (i,j) se destination tak koi path hai ya nahi
-    // Agar path milta hai to us path ke saare cells ko 0 bhi kar deta hai
-    bool dfs(vector<vector<int>>& grid, int i, int j) {
-
-        // Grid ke bahar chale gaye
-        // Ya current cell blocked hai
-        if (i >= m || j >= n || grid[i][j] == 0)
+    bool dfs(vector<vector<int>>& arr, int i, int j) {
+        if (i >= m || j >= n || arr[i][j] == 0)
             return false;
-
-        // Destination mil gaya
         if (i == m - 1 && j == n - 1)
             return true;
 
-        // Current cell ko erase kar do
-        // Taaki ye path dobara use na ho
-        grid[i][j] = 0;
+        // mark as visited path uda diya gayab kr dya
+        arr[i][j] = 0;
 
-        // Pehle Down try karo
-        // Agar nahi mila to Right try karo
-        return dfs(grid, i + 1, j) || dfs(grid, i, j + 1);
+        return dfs(arr, i + 1, j) || dfs(arr, i, j + 1);
     }
 
-    bool isPossibleToCutPath(vector<vector<int>>& grid) {
+    bool isPossibleToCutPath(vector<vector<int>>& arr) {
+        m = arr.size();
+        n = arr[0].size();
 
-        // Grid ka size
-        m = grid.size();
-        n = grid[0].size();
-
-        // -------------------------
-        // STEP-1
-        // Pehle dekh lo path exist bhi karta hai ya nahi
-        // -------------------------
-
-        if (!dfs(grid, 0, 0))
+        // agar already path exist nhi karta
+        if (!dfs(arr, 0, 0))
             return true;
 
-        // Agar yahan tak aaye hain
-        // Matlab ek path mil gaya tha
-        // Aur DFS us path ko erase bhi kar chuka hai
+        arr[0][0] = 1, arr[m - 1][n - 1] = 1;
 
-        // Source aur Destination ko erase nahi karna
-        // Isliye unko wapas 1 bana do
-
-        grid[0][0] = 1;
-        grid[m - 1][n - 1] = 1;
-
-        // -------------------------
-        // STEP-2
-        // Ab dubara path dhoondo
-        // -------------------------
-
-        // Agar second path nahi mila
-        // To iska matlab sirf ek hi path tha
-        // Aur usko erase karne se graph disconnect ho gaya
-        // Answer = true
-
-        // Agar second path mil gaya
-        // To matlab do independent paths the
-        // Answer = false
-
-        return !dfs(grid, 0, 0);
+        return !dfs(arr, 0, 0);
     }
 };
