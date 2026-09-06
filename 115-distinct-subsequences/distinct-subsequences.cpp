e9e430e7feaf484b424c91fb2af1274c9d56a611
Shubham Malik <146654596+subhm2004@users.noreply.github.com>
2026-09-06 14:29:15 +0530

    Time: 27 ms (71.17%) | Memory: 12.9 MB (80.54%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-    int solveRE(string& s, string& t, int i, int j) {
+
+    int dp[1001][1001];
+
+    int solveME(string& s, string& t, int i, int j) {
+
+        // Agar t pura match ho gaya to ek valid subsequence mil gayi
         if (j == t.size())
             return 1;
+
+        // Agar s khatam ho gaya lekin t abhi baaki hai to match possible nahi hai
         if (i == s.size())
             return 0;
-        int ans = 0;
-        if (s[i] == t[j]) {
-            ans += solveRE(s, t, i + 1, j + 1);
-        }
-        ans += solveRE(s, t, i + 1, j);
-        return ans;
-    }
-    int solveME(string& s, string& t, int i, int j, vector<vector<int>>& dp) {
-        if (j == t.size())
-            return 1;
-        if (i == s.size())
-            return 0;
+
         if (dp[i][j] != -1)
             return dp[i][j];
+
         int ans = 0;
+
+        // Agar current characters match karte hain
         if (s[i] == t[j]) {
-            ans += solveME(s, t, i + 1, j + 1, dp);
+
+            // Current character ko choose kar rahe hain
+            ans += solveME(s, t, i + 1, j + 1);
         }
-        ans += solveME(s, t, i + 1, j, dp);
+
+        // Current character ko skip kar rahe hain
+        ans += solveME(s, t, i + 1, j);
+
         return dp[i][j] = ans;
     }
+
     int numDistinct(string s, string t) {
-        // return solveRE(s, t, 0, 0);
-        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, -1));
-        return solveME(s, t, 0, 0, dp);
+
+        memset(dp, -1, sizeof(dp));
+
+        return solveME(s, t, 0, 0);
     }
 };
